ffb586df5ff3a3a9d0f922c03c900d5996da0f11
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-09-16 23:55:33 -0400

    Create the backend structure of the spring boot project with very minimal maven dependencies (starter-web, starter-actuator and the rest of the default dependencies).

---
+++ service/switchboardapp/src/main/java/com/switchboard/switchboardapp/SwitchboardappApplication.java
@@ -0,0 +1,13 @@
+package com.switchboard.switchboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchboardappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchboardappApplication.class, args);
+	}
+
+}
---
+++ service/switchboardapp/src/test/java/com/switchboard/switchboardapp/SwitchboardappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchboard.switchboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchboardappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
