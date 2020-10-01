c34eb38b842a9be61d7d3603d208619f2027ef4b
Jun Loke <rebecca.j.loke@gmail.com>
2020-10-01 15:05:13 -0400

    Add Material UI core and Implement an App bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+  AppBar,
+  Toolbar
+} from "@material-ui/core"
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar></Toolbar>
+        </AppBar>
     </div>
   );
 }
