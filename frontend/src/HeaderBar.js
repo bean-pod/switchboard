e5c5063a351edc0358bdce696d926191d9e40e95
gdzooks <issa4044@gmail.com>
2020-10-03 12:36:11 -0400

    Update header to match UI mockups.

--- frontend/src/HeaderBar.js
+++ frontend/src/HeaderBar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {
   AppBar,
-  Toolbar
+  Toolbar,
+  IconButton,
+  Badge
 } from "@material-ui/core"
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function HeaderBar() {
   return (
     <div className="HeaderBar">
       <AppBar position="static">
-        <Toolbar></Toolbar>
+        <Toolbar class="darkGrey paddedText headerTitle flexContents">
+          Switchboard
+          <span class="alignRight">
+            <IconButton color="inherit"><NotificationsIcon /></IconButton>
+            <IconButton color="inherit"><AccountCircle /></IconButton>
+          </span>     
+        </Toolbar>
         </AppBar>
     </div>
   );
