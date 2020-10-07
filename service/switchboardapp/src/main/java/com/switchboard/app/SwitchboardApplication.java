84aa674eaae6603a1c55297d0209a79dc002d882
Mohamed Hefny <mohhef@rocketmail.com>
2020-10-07 00:15:31 -0400

    Test github actions

--- service/switchboardapp/src/main/java/com/switchboard/app/SwitchboardApplication.java
+++ service/switchboardapp/src/main/java/com/switchboard/app/SwitchboardApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SwitchboardApplication {
 
     public static void main(String[] args) {
-        System.out.println("test github actions");
         SpringApplication.run(SwitchboardApplication.class, args);
     }
 
