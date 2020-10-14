67fe25fab836e503dea26ebc1697f15c894d8b69
Jun Loke <rebecca.j.loke@gmail.com>
2020-10-13 20:51:48 -0400

    Move head cell generation to its own file

--- frontend/src/DeviceList.js
+++ frontend/src/DeviceList.js
@@ -34,6 +34,7 @@ import {
 
 import PropTypes from "prop-types";
 import GenerateData from "./SampleData";
+import generateHeadCells from "./headCells";
 // imports for material ui & etc
 
 // temporary row
@@ -206,31 +207,7 @@ function DevicesTable() {
                         <TableHead>
                             <TableRow>
                                 <TableCell style={{ width: 1, padding: 0, paddingLeft: 5 }}></TableCell>
-                                <TableCell>
-                                    <Typography variant="caption">Name</Typography>
-                                    <TextField variant="outlined" id="nameSearch" size="small" />
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="caption">MAC Address</Typography>
-                                    <TextField variant="outlined" id="macSearch" size="small" />
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="caption">Status</Typography>
-                                    <Select variant="outlined" id="statusSelect" fullWidth="true" >
-                                        <MenuItem dense="true" value={"Online"}> <Typography variant="caption">Online</Typography></MenuItem>
-                                        <MenuItem dense="true" value={"Pending"}> <Typography variant="caption">Pending</Typography></MenuItem>
-                                        <MenuItem dense="true" value={"Error"}>   <Typography variant="caption">Error</Typography></MenuItem>
-                                        <MenuItem dense="true" value={"Offline"}> <Typography variant="caption">Offline</Typography></MenuItem>
-                                    </Select>
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="caption">IP Address</Typography>
-                                    <TextField variant="outlined" multiline="false" margin="dense" size="small" id="ipSearch" />
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="caption">Port</Typography>
-                                    <TextField variant="outlined" id="portSearch" size="small" />
-                                </TableCell>
+                                {generateHeadCells()}
                                 <TableCell align="center"><Typography variant="caption">Actions</Typography></TableCell>
                             </TableRow>
                         </TableHead>
---
+++ frontend/src/headCells.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    TableCell,
+    TableSortLabel,
+    Select,
+    Typography,
+    TextField,
+    MenuItem
+} from "@material-ui/core"
+
+class HeadCell{
+    constructor(id, label, isNum, padding,){
+        this.id = id;
+        this.label = label;
+        this.isNumeric = isNum;
+        this.searchID = id +"Search";
+        this.padding = padding;
+    }
+}
+function getHeadCellData(){
+    
+    return [
+         new HeadCell("name", "Name", false, false),
+          new HeadCell("mac", "Mac Address", true, true),
+          new HeadCell("status", "Status", false, true),
+          new HeadCell("ip", "IP Address", true, true),
+          new HeadCell("port", "Port", true, true)
+     ] ;
+
+  };
+
+export default function generateHeadCells(){
+    var headcells = getHeadCellData();
+
+    var html = headcells.map((headCell)=> (
+                <TableCell
+                 key= {headCell.id}
+                 padding={headCell.padding ?  'default': 'none'}
+                 >
+                     <TableSortLabel>
+                        <Typography variant="caption"> {headCell.label}</Typography>
+                     </TableSortLabel>
+                    {headCell.id == "status"? (
+                        <Select variant="outlined" id={headCell.searchID} fullWidth="true" >
+                        <MenuItem dense="true" value={"Online"}> <Typography variant="caption">Online</Typography></MenuItem>
+                        <MenuItem dense="true" value={"Pending"}> <Typography variant="caption">Pending</Typography></MenuItem>
+                        <MenuItem dense="true" value={"Error"}>   <Typography variant="caption">Error</Typography></MenuItem>
+                        <MenuItem dense="true" value={"Offline"}> <Typography variant="caption">Offline</Typography></MenuItem>
+                    </Select>
+                    ):
+                    (
+                        <TextField variant="outlined" id={headCell.searchID} size="small" />
+                    )}
+                </TableCell>
+            ));
+
+    return html;
+}
+
+
