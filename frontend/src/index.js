434f7bc62d8ee493161b709292e143d2f2a10166
gdzooks <issa4044@gmail.com>
2020-10-26 20:12:58 -0400

    Update breadcrumb position to match mockups.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import DeviceList from './devicelist/DeviceList';
 
 import {
   Breadcrumbs,
+  Container,
   Typography,
   Link,
   Box,
@@ -14,14 +15,16 @@ import {
 ReactDOM.render(
   <React.StrictMode>
     <HeaderBar />
-    <Box padding="2em 0em 0em 10em">
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link color="inherit" href="/" >
-          Home
+    <Container>
+      <Box padding="2em 0em 0em 0em">
+        <Breadcrumbs aria-label="breadcrumb">
+          <Link color="inherit" href="/" >
+            Home
         </Link>
-        <Typography component={'span'} color="textPrimary">Devices</Typography>
-      </Breadcrumbs>
-    </Box>
+          <Typography component={'span'} color="textPrimary">Devices</Typography>
+        </Breadcrumbs>
+      </Box>
+    </Container>
     <Box padding="1em">
       <DeviceList />
     </Box>
