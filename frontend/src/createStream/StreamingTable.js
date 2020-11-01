9527978db73f4bf20c75223955c728a33ddd570b
Jun Loke <rebecca.j.loke@gmail.com>
2020-10-31 22:56:54 -0400

    Change sizing/styling for Streaming table

--- frontend/src/createStream/StreamingTable.js
+++ frontend/src/createStream/StreamingTable.js
@@ -14,18 +14,23 @@ export default class StreamingTable extends React.Component{
 
     render(){
         return(
-            <React.Fragment>            
+            <React.Fragment>  
+                     
                 <Grid container spacing={2} alignContent={"center"} alignItems={"center"}  justify = {'center'}>
-                    <Grid item xs = {4}>
+                    <Grid item xs = {2}>
+                        <div class="streamingTable">
                         <SelectDevicesTable/>
+                        </div>     
                     </Grid>
-                    <Grid item xs = {3} justify = {'center'}>
+                    <Grid item xs = {2} justify = {'center'}alignContent={"center"} alignItems={"center"}display="flex">
                         <Button class="green buttonText">
                             <SwapHoriz /> Stream
                         </Button>
                     </Grid>
-                    <Grid item xs = {4}>
-                    <SelectDevicesTable/>
+                    <Grid item xs = {2}>
+                        <div class="streamingTable">
+                        <SelectDevicesTable/>
+                        </div>
                     </Grid>
                 </Grid>
            
