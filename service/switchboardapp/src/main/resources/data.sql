7ad9f5aa9f87e369e8398fd3ef1136d659230b72
Mohamed Hefny <mohhef@rocketmail.com>
2020-11-01 22:32:34 +0400

    fix merge conflicts with master

--- service/switchboardapp/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-/* This data is only loaded when using h2 in memory database,to enable h2: uncomment the line "#spring.datasource.url=jdbc:h2:mem:testdb"
-   in application.properties
- */
-
--- insert into device(serial_number, display_name, Status) values (1,'Device1','Running');
--- insert into device(serial_number, display_name, Status) values (2,'Device2','Running');
