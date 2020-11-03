2d804784a276b267647ea36b97be94ff885da643
Mohamed Hefny <mohhef@rocketmail.com>
2020-11-03 20:32:12 +0400

    make the channel entity the owner side for easier crud operations

--- src/main/java/org/beanpod/switchboard/entity/ChannelEntity.java
+++ src/main/java/org/beanpod/switchboard/entity/ChannelEntity.java
@@ -6,8 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter @Setter
 @AllArgsConstructor
@@ -18,4 +17,18 @@ public class ChannelEntity {
     @Id
     String name;
     Integer port;
+
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE})
+    @JoinColumn(name = "decoder_serial")
+    DecoderEntity decoderEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE})
+    @JoinColumn(name = "encoder_serial")
+    EncoderEntity encoderEntity;
 }
