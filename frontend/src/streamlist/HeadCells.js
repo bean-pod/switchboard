9693004f12f0e2c739e2dcde3653d119c663ce12
gdzooks <issa4044@gmail.com>
2020-11-03 11:44:21 -0500

    Create HeadCells for Streams List table.
    Following the Device List table's format

--- frontend/src/streamlist/HeadCells.js
+++ frontend/src/streamlist/HeadCells.js
@@ -1,1 +1,48 @@
 import React from 'react';
+import {
+    TableCell,
+    TableSortLabel,
+    Select,
+    Typography,
+    TextField,
+    MenuItem
+} from "@material-ui/core"
+import HeadCell from '../model/HeadCell'
+
+export default class StreamListHeadCells extends React.Component {
+    constructor(props) {
+        super(props)
+        this.headcells = this.getHeadCellData();
+    }
+
+    getHeadCellData() {
+        return [
+            new HeadCell("id", "ID", true, true),
+            new HeadCell("date", "Date", true, true),
+            new HeadCell("sendername", "Sender", false, true),
+            new HeadCell("receivername", "Receiver", false, true),
+            new HeadCell("status", "Status", false, true),
+            new HeadCell("type", "Type", false, true),
+            new HeadCell("timeelapsed", "Time Elapsed", true, true),
+        ];
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.headcells.map((headCell) =>(
+                    <TableCell
+                        class="tableHeadCell lightestGrey"
+                        key={headCell.id}
+                        padding={headCell.padding ? 'default' : 'none'}>
+                            <TableSortLabel>
+                                <Typography component={'span'} variant="caption">
+                                    {headCell.label}
+                                </Typography>
+                            </TableSortLabel>
+                    </TableCell>
+                ))}
+            </React.Fragment>
+        );
+    }
+}
