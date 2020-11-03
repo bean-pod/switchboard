15338799342b45140ba6a90533ca8a675533a4c3
gdzooks <issa4044@gmail.com>
2020-11-03 11:46:19 -0500

    Create empty stream list table.

--- frontend/src/streamlist/StreamsTable.js
+++ frontend/src/streamlist/StreamsTable.js
@@ -1,1 +1,37 @@
 import React from 'react';
+import {
+    Box,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableContainer,
+    TableBody,
+    Typography
+} from "@material-ui/core"
+import HeadCells from './HeadCells'
+
+export default function StreamsTable(props) {
+    return (
+        <React.Fragment>
+            <Box>
+                <TableContainer style={{ maxHeight: 450 }}>
+                    <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell class="tableHeadCell lightestGrey" style={{ width: 1, padding: 0, paddingLeft: 5 }}></TableCell>
+                                <HeadCells />
+                                <TableCell class="tableHeadCell lightestGrey" align="center">
+                                    <Typography variant="caption">Actions</Typography>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+        </React.Fragment>
+    );
+}
