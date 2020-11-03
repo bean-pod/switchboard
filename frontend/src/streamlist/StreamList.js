013719e59b5bd67231ef6015d39e47da904758e6
gdzooks <issa4044@gmail.com>
2020-11-03 11:45:38 -0500

    Add Stream List page with title.

--- frontend/src/streamlist/StreamList.js
+++ frontend/src/streamlist/StreamList.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import {
+    Box,
+    Container,
+    makeStyles  
+} from "@material-ui/core";
+
+export default function StreamList() {
+
+    return (
+        <Container>
+            <Box class="headerArea">
+                <div class="title">
+                    Current Streams
+                </div>
+            </Box>
+
+        </Container>
+    );
+}
