a4c6aad2e8088862e8ee487225e525034da7f061
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-02 21:42:59 -0500

    Enable dynamic breadcrumbs!

--- frontend/src/Breadcrumb.js
+++ frontend/src/Breadcrumb.js
@@ -1,43 +1,30 @@
 import React from 'react';
 import {
     Breadcrumbs,
-    Typography
+    Typography,
+    Link
 } from "@material-ui/core"
-import parse from 'html-react-parser';
 
-function Breadcrumb() {
-
-    var element = "<Breadcrumbs aria-label=\"breadcrumb\">";
-    var currentBreadcrumb = "/"
-
-    for (var i = 0; i < arguments.length; i++) {
-        if (i == (arguments.length - 1)) {
-            element.concat("<Typography color=\"textPrimary\">", arguments[i], "</Typography>")
-        }
-        currentBreadcrumb.concat("/", arguments[i]);
-
-        element += "<Link color=\"inherit\" href=\""
-            + currentBreadcrumb + "\" onClick={handleClick}>"
-            + arguments[i] + "</Link>);"
-    }
-
-    element.concat("</Breadcrumbs>")
-    return parse(element)
-}
-
-class Breadcrumb extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default class DynamicBreadcrumb extends React.Component {
 
     render() {
         return (
             <Breadcrumbs aria-label="breadcrumb">
                 {this.props.breadcrumbs.map((crumb) => {
+                    
+                    if (crumb==="Home")
+                     return (
+                        <React.Fragment>
+                            <Link color="inherit" href="" >
+                                <Typography color="textPrimary"> {crumb}</Typography>
+                            </Link>
+                        </React.Fragment>
+                    
+                    );
                     return (
                         <React.Fragment>
-                            <Typography color="textPrimary"> {crumb}"</Typography>
-                    <Link color="inherit" href={"\ " + crumb} onClick={handleClick}> {crumb}</Link>
+                            <Link color="inherit" href={"\ " + crumb} > <Typography color="textPrimary"> {crumb}</Typography>
+                    </Link>
                         </React.Fragment>
                     
                     );
@@ -46,4 +33,3 @@ class Breadcrumb extends React.Component {
         );
     }
 }
-export default Breadcrumb("home", "device");
