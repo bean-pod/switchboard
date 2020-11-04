9f47ee3dfa462cf5de8aeffbf3dc74ba071b78a5
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-04 12:46:10 -0500

    cleanup Warnings

--- frontend/src/createStream/StreamingTable.js
+++ frontend/src/createStream/StreamingTable.js
@@ -44,15 +44,15 @@ export default class StreamingTable extends React.Component {
 
                 <Grid container spacing={2} alignContent={"center"} alignItems={"center"} justify={'center'}>
                     <Grid item xs={3}>
-                        <div class="streamingTable">
+                        <div className="streamingTable">
                             <SelectDevicesTable name="Sender Devices" dataSource={this.state.senders} />
                         </div>
                     </Grid>
-                    <Grid item xs={2} id="TableStartStreaming" alignContent={"center"} alignItems={"center"} display="flex">
+                    <Grid container item xs={2} id="TableStartStreaming" justify={'center'} alignContent={"center"} alignItems={"center"} display="flex">
                         <StreamButton />
                     </Grid>
                     <Grid item xs={3}>
-                        <div class="streamingTable">
+                        <div className="streamingTable">
                             <SelectDevicesTable name="Receiver Devices" dataSource={this.state.receivers} />
                         </div>
                     </Grid>
