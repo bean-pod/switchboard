733f33df4404a583959da5205a1e1dcc94184fcb
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-04 22:25:38 -0500

    Cleanup test

--- frontend/src/devicelist/__tests__/DeviceListSortBy.test.js
+++ frontend/src/devicelist/__tests__/DeviceListSortBy.test.js
@@ -6,33 +6,32 @@ Enzyme.configure({ adapter: new Adapter() });
 
 import DeviceListSortSelector from "../DeviceListSortSelector"
 
-describe("<DeviceListSortSelector/>",()=>{
+describe("<DeviceListSortSelector/>", () => {
     let wrapper;
-    const setState=jest.fn();
-    const useStateSpy = jest.spyOn(React, "useState")
-    
+
     beforeEach(() => {
-        wrapper = Enzyme.shallow(<DeviceListSortSelector/>)
+        wrapper = Enzyme.shallow(<DeviceListSortSelector />)
     });
-    
+
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    describe("Change State",()=>{
-        it("Should call setState",()=>{
+    describe("Change State", () => {
+        it("Should call setState", () => {
             const mockEvent = {
-                target:{
+                target: {
                     name: "sortBy",
                     value: "Test"
                 }
             };
-            const expected={
-                sort:"Test"
+            const expected = {
+                sort: "Test"
             };
-    
+
             wrapper.instance().handleSortChange(mockEvent);
             expect(wrapper.state()).toEqual(expected);
+
         })
     })
 })
