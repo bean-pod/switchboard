4a6c7e2e37511db6d81fbf12e0622210df4449ae
gdzooks <issa4044@gmail.com>
2020-11-06 00:31:44 -0500

    Remove unused imports

--- frontend/src/streamlist/HeadCells.js
+++ frontend/src/streamlist/HeadCells.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from 'react'
 import {
     TableCell,
     TableSortLabel,
-    Select,
     Typography,
-    TextField,
-    MenuItem
 } from "@material-ui/core"
 import HeadCell from '../model/HeadCell'
 
