1259c9a8011d64ef637c676edb06f6c70162d013
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-07 13:36:15 -0500

    Create Input and Output ChannelInfo classes in accordance with new contract

---
+++ frontend/src/model/OutputChannelInfo.js
@@ -0,0 +1,8 @@
+export default class OutputChannelInfo {
+    constructor(id, name, port, encoder) {
+        this.id = id;
+        this.name = name;
+        this.port = port;
+        this.encoder = encoder;
+    }
+}
