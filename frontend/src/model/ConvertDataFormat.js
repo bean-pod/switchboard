a1fe027d37545c4e26d92833856ff00ec9ac844c
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-08 22:37:03 -0500

    Move conversion functions to ConvertDataFormat file

---
+++ frontend/src/model/ConvertDataFormat.js
@@ -0,0 +1,43 @@
+import DeviceInfo from "./DeviceInfo";
+
+export function convertToLocal(databaseDevice) {
+    return (
+        new DeviceInfo(
+            databaseDevice.serialNumber,
+            databaseDevice.lastCommunication,
+            databaseDevice.device.ipAddress,
+            databaseDevice.device.displayName,
+            databaseDevice.device.status,
+            databaseDevice.inputs,
+            "lol")
+    );
+}
+
+export function convertToAxios(deviceInfo) {
+    if (deviceInfo.hasOwnProperty("outputs")) {
+        return {
+            "serialNumber": deviceInfo.serialNumber,
+            "lastCommunication": deviceInfo.lastCommunication,
+            "device": {
+                "serialNumber": deviceInfo.serialNumber,
+                "ipAddress": deviceInfo.ip,
+                "displayName": deviceInfo.name,
+                "status": deviceInfo.status
+            },
+            "outputs": deviceInfo.channels
+        }
+    }
+    else {
+        return {
+            "serialNumber": deviceInfo.serialNumber,
+            "lastCommunication": deviceInfo.lastCommunication,
+            "device": {
+                "serialNumber": deviceInfo.serialNumber,
+                "ipAddress": deviceInfo.ip,
+                "displayName": deviceInfo.name,
+                "status": deviceInfo.status
+            },
+            "inputs": deviceInfo.channels
+        }
+    }
+}
--- frontend/src/model/DeviceInfo.js
+++ frontend/src/model/DeviceInfo.js
@@ -2,36 +2,10 @@ export default class DeviceInfo {
     constructor(serial, lastCommunication, ipAddress, displayName, status, channels, extras) {
         this.serialNumber = serial;
         this.lastCommunication = lastCommunication;
-        this.ip= ipAddress;
-        this.name= displayName;
-        this.status=status;
+        this.ip = ipAddress;
+        this.name = displayName;
+        this.status = status;
         this.channels = channels;
         this.extras = extras;
-    }
-
-    // function for converting from format to format
-    toCoderFormat() {
-        return ({
-            serialNumber: this.serialNumber,
-            device: {
-                serialNumber: this.serialNumber,
-                ipAddress: this.ipAddress,
-                displayName: this.displayName,
-                status: this.status
-            }
-        });
-    }
-
-    toDeviceFormat(coder){
-        return (
-            new DeviceInfo(
-                coder.serialNumber,
-                null,
-                coder.ipAddress,
-                coder.displayName,
-                coder.status,
-                null,
-                null)
-        );
     }
 }
