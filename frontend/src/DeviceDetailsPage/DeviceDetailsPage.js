b6ef23c2c8ef4b03f491500970a607895578b8bf
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-23 22:01:33 -0500

    Create DeviceDetailsPage.js

---
+++ frontend/src/DeviceDetailsPage/DeviceDetailsPage.js
@@ -0,0 +1,37 @@
+import { Title } from "@material-ui/icons";
+import React from "react";
+
+export default class DeviceDetailsPage extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            isLoading = true,
+            isData = false,
+            device = props.device
+        }
+    }
+
+    useEffect(){
+
+    }
+    render(){
+        return(
+            <Container>
+            <DynamicBreadcrumb
+                breadcrumbs={[["Home", ""], ["My Devices", "Devices"], ["Device", "Streaming"]]} />
+            <Box padding="1em">
+                <Box className="flexContents headerArea">
+                    <div className="title">Streaming</div>
+                </Box>
+
+                <div id="StreamingTable">
+
+                    <StreamingTable device={this.props.device} />
+                </div>
+            </Box>                
+        </Container>
+        );
+    }
+}
