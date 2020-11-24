5786d270a34fc9666d901afbae94516679c3141e
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-23 22:18:26 -0500

    Run linter

--- frontend/src/DeviceDetailsPage/DeviceDetailsPage.js
+++ frontend/src/DeviceDetailsPage/DeviceDetailsPage.js
@@ -1,37 +1,39 @@
 import { Title } from "@material-ui/icons";
 import React from "react";
 
-export default class DeviceDetailsPage extends React.Component{
+export default class DeviceDetailsPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.state = {
+      isLoading: true,
+      isData: false,
+      device: props.device
+    };
+  }
 
-        this.state={
-            isLoading = true,
-            isData = false,
-            device = props.device
-        }
-    }
+  useEffect() {}
 
-    useEffect(){
+  render() {
+    return (
+      <Container>
+        <DynamicBreadcrumb
+          breadcrumbs={[
+            ["Home", ""],
+            ["My Devices", "Devices"],
+            ["Device", "Streaming"]
+          ]}
+        />
+        <Box padding="1em">
+          <Box className="flexContents headerArea">
+            <div className="title">Streaming</div>
+          </Box>
 
-    }
-    render(){
-        return(
-            <Container>
-            <DynamicBreadcrumb
-                breadcrumbs={[["Home", ""], ["My Devices", "Devices"], ["Device", "Streaming"]]} />
-            <Box padding="1em">
-                <Box className="flexContents headerArea">
-                    <div className="title">Streaming</div>
-                </Box>
-
-                <div id="StreamingTable">
-
-                    <StreamingTable device={this.props.device} />
-                </div>
-            </Box>                
-        </Container>
-        );
-    }
+          <div id="StreamingTable">
+            <StreamingTable device={this.props.device} />
+          </div>
+        </Box>
+      </Container>
+    );
+  }
 }
