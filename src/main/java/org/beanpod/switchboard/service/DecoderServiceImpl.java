ba5c8971e636619e0b186da949316e7532601e10
Felix Lapierre <felix.lapierre1@gmail.com>
2020-11-25 14:56:59 -0500

    Update time of last communication when getting an encoder/decoder's streams

--- src/main/java/org/beanpod/switchboard/service/DecoderServiceImpl.java
+++ src/main/java/org/beanpod/switchboard/service/DecoderServiceImpl.java
@@ -1,19 +1,35 @@
 package org.beanpod.switchboard.service;
 
 import lombok.RequiredArgsConstructor;
+import org.beanpod.switchboard.dao.DecoderDaoImpl;
 import org.beanpod.switchboard.dao.StreamDaoImpl;
+import org.beanpod.switchboard.dto.DecoderDto;
 import org.beanpod.switchboard.dto.StreamDto;
+import org.beanpod.switchboard.exceptions.ExceptionType;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class DecoderServiceImpl implements DecoderService {
     private final StreamDaoImpl streamDao;
+    private final DecoderDaoImpl decoderDao;
 
     @Override
     public List<StreamDto> getDecoderStreams(String decoderSerialNumber) {
+        Optional<DecoderDto> decoderOptional = decoderDao.findDecoder(decoderSerialNumber);
+        if(decoderOptional.isEmpty()) {
+            throw new ExceptionType.DeviceNotFoundException(decoderSerialNumber);
+        }
+        DecoderDto decoder = decoderOptional.get();
+
+        decoder.setLastCommunication(Date.from(Instant.now()));
+        decoderDao.save(decoder);
+
         return streamDao.getDecoderStreams(decoderSerialNumber);
     }
 }
--- src/main/java/org/beanpod/switchboard/service/EncoderServiceImpl.java
+++ src/main/java/org/beanpod/switchboard/service/EncoderServiceImpl.java
@@ -1,19 +1,35 @@
 package org.beanpod.switchboard.service;
 
 import lombok.RequiredArgsConstructor;
+import org.beanpod.switchboard.dao.EncoderDaoImpl;
 import org.beanpod.switchboard.dao.StreamDaoImpl;
+import org.beanpod.switchboard.dto.EncoderDto;
 import org.beanpod.switchboard.dto.StreamDto;
+import org.beanpod.switchboard.exceptions.ExceptionType;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class EncoderServiceImpl implements EncoderService {
+    private final EncoderDaoImpl encoderDao;
     private final StreamDaoImpl streamDao;
 
     @Override
     public List<StreamDto> getEncoderStreams(String encoderSerialNumber) {
+        Optional<EncoderDto> encoderOptional = encoderDao.findEncoder(encoderSerialNumber);
+        if(encoderOptional.isEmpty()) {
+            throw new ExceptionType.DeviceNotFoundException(encoderSerialNumber);
+        }
+        EncoderDto encoderDto = encoderOptional.get();
+
+        encoderDto.setLastCommunication(Date.from(Instant.now()));
+        encoderDao.save(encoderDto);
+
         return streamDao.getEncoderStreams(encoderSerialNumber);
     }
 }
