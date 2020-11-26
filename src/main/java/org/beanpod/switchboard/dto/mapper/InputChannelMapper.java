1dcbebe27c0e447cc598ab510ee3c6c659885196
Felix Lapierre <32281260+felixlapierre@users.noreply.github.com>
2020-11-26 18:56:05 -0500

    Adjusted mapper classes to properly populate fields of channels (#165)

--- src/main/java/org/beanpod/switchboard/dto/mapper/InputChannelMapper.java
+++ src/main/java/org/beanpod/switchboard/dto/mapper/InputChannelMapper.java
@@ -5,16 +5,21 @@ import org.beanpod.switchboard.dto.InputChannelDTO;
 import org.beanpod.switchboard.entity.InputChannelEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.openapitools.model.InputChannelModel;
 
 @Mapper(
     componentModel = "spring",
     uses = {DecoderMapper.class, ChannelMapper.class})
 public interface InputChannelMapper {
-  @Mapping(target = "decoder", ignore = true)
+  @Mapping(target = "decoder", qualifiedByName = "toDecoderDTOShallow")
   InputChannelDTO toInputChannelDTO(InputChannelEntity inputChannelEntity);
 
   @Mapping(target = "decoder", ignore = true)
   Set<InputChannelDTO> toInputChannelDTOs(Set<InputChannelEntity> inputChannelEntities);
 
   InputChannelEntity toInputChannelEntity(InputChannelDTO inputChannelDTO);
+
+  @Mapping(target = "name", source = "channel.name")
+  @Mapping(target = "port", source = "channel.port")
+  InputChannelModel inputChannelDTOToInputChannelModel(InputChannelDTO inputChannelDTO);
 }
