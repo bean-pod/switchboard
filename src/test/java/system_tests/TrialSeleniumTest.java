c4ff0122cf971a4db3a9a8c546cbdbd6e34f6749
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-11-26 09:37:08 -0500

    Select Online status for the sender

--- src/test/java/system_tests/TrialSeleniumTest.java
+++ src/test/java/system_tests/TrialSeleniumTest.java
@@ -33,8 +33,19 @@ public class TrialSeleniumTest {
         driver.findElement(By.id("serialSearch")).sendKeys("1:10:111:999");
         driver.findElement(By.id("ipSearch")).sendKeys("123:456");
 
-
-
+        //select Online status
+        {
+            WebElement element = driver.findElement(By.id("statusSearch"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).clickAndHold().perform();
+        }
+        {
+            WebElement element = driver.findElement(By.cssSelector("#menu- > div:nth-child(1)"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(element).release().perform();
+        }
+        driver.findElement(By.cssSelector("body")).click();
+        driver.findElement(By.cssSelector(".MuiListItem-dense:nth-child(1)")).click();
     }
 
 }
