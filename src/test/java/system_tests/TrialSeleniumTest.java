f5b6ede5fb2ac5e9567cbca2541e89cc298e1c76
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-11-26 10:47:54 -0500

    Check if the test device was added

--- src/test/java/system_tests/TrialSeleniumTest.java
+++ src/test/java/system_tests/TrialSeleniumTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class TrialSeleniumTest {
     private static WebDriver driver;
@@ -29,7 +30,7 @@ public class TrialSeleniumTest {
         driver.manage().window().setSize(new Dimension(782, 818));
 
         //populate Name, Serial Name and IP Address fields
-        driver.findElement(By.id("nameSearch")).sendKeys("Sender 10");
+        driver.findElement(By.id("nameSearch")).sendKeys("Sender Test10");
         driver.findElement(By.id("serialSearch")).sendKeys("1:10:111:999");
         driver.findElement(By.id("ipSearch")).sendKeys("123:456");
 
@@ -40,6 +41,7 @@ public class TrialSeleniumTest {
             builder.moveToElement(element).clickAndHold().perform();
         }
         {
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
             WebElement element = driver.findElement(By.cssSelector("#menu- > div:nth-child(1)"));
             Actions builder = new Actions(driver);
             builder.moveToElement(element).release().perform();
@@ -48,6 +50,16 @@ public class TrialSeleniumTest {
         driver.findElement(By.cssSelector(".MuiListItem-dense:nth-child(1)")).click();
 
         driver.findElement(By.id("DeviceListAddDevBtn")).click();
+
+        //Check if the device was added
+        driver.navigate().refresh();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        List<WebElement> devicesRows = driver.findElements(By.className("singleDeviceRow"));
+        System.out.println(devicesRows.size());
+        for(WebElement element : devicesRows){
+            System.out.println(element.getText().contains("Sender Test10"));
+        }
 //        WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"menu-\"]/div[3]/ul"));
 //        Select statuses = new Select(dropdown);
 //        driver.findElement(new By.ByXPath(("//*[@id=\"menu-\"]/div[3]/ul/li[1]"))).click();
