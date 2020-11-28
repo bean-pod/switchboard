928bdd0458ebc4e07b5f0ec09c6be0a2425d42b3
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-27 20:51:34 -0500

    Cleanup DeviceDetailsPage
    Destructure props, fix typo from name change,  add props validation

--- frontend/src/DeviceDetailsPage/DeviceDetailsPage.jsx
+++ frontend/src/DeviceDetailsPage/DeviceDetailsPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Box, Container, Grid } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { Button, Container, Grid } from "@material-ui/core";
 
 import DeviceDetailsConciseTable from "./DeviceDetailsConciseTable";
-import DynamicBreadcrumb from "../General/DynamicBreadcrumb";
+import DynamicBreadcrumb from "../general/DynamicBreadcrumb";
 import DeviceDetailsTabTable from "./DeviceDetailsTabTable";
+import DeviceInfo from "../model/DeviceInfo";
 
 export default class DeviceDetailsPage extends React.Component {
   constructor(props) {
@@ -12,32 +14,46 @@ export default class DeviceDetailsPage extends React.Component {
   }
 
   render() {
+    const { device } = this.props;
     return (
       <Container>
         <DynamicBreadcrumb
           breadcrumbs={[
             ["Home", ""],
             ["My Devices", "Devices"],
-            [this.props.device.name, this.props.device.id]
+            [device.name, device.id]
           ]}
         />
         <Grid container spacing={1}>
           <Grid item xs={12} className="flexContents headerArea">
-            <div className="title">{this.props.device.name}</div>
+            <div className="title">{device.name}</div>
           </Grid>
           <Grid item xs={12} />
-          <Grid item xs={5}>
-            <DeviceDetailsConciseTable device={this.props.device} />
+          <Grid container xs={5} spacing={2}>
+            <Grid item xs={12}>
+              <DeviceDetailsConciseTable device={device} />
+            </Grid>
+            <Grid item xs={2}>
+              <Button variant="contained" color="primary">
+                Edit
+              </Button>
+            </Grid>
+            <Grid item xs={2}>
+              <Button variant="outlined" color="red">
+                Delete
+              </Button>
+            </Grid>
           </Grid>
           <Grid item xs={1} />
           <Grid item xs={6}>
-            <DeviceDetailsTabTable
-              tabs={this.tabs}
-              device={this.props.device}
-            />
+            <DeviceDetailsTabTable tabs={this.tabs} device={device} />
           </Grid>
         </Grid>
       </Container>
     );
   }
 }
+
+DeviceDetailsPage.propTypes = {
+  device: PropTypes.instanceOf(DeviceInfo).isRequired
+};
