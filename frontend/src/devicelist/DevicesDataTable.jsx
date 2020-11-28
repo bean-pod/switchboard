b0b476eee9bcb4e13893d17fef550c6f6ac791ae
gdzooks <issa4044@gmail.com>
2020-11-28 06:47:19 -0500

    Fix broken import

--- frontend/src/devicelist/DevicesDataTable.jsx
+++ frontend/src/devicelist/DevicesDataTable.jsx
@@ -14,7 +14,7 @@ import {
 import MaterialTable, { MTableToolbar } from "material-table";
 
 import ChannelDetailsTable from "./ChannelDetailsTable";
-import StatusIndicator from "../StatusIndicator";
+import StatusIndicator from "../General/StatusIndicator";
 import ActionMenu from "./ActionMenu";
 
 export default class DevicesTable extends React.Component {
