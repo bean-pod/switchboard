c29742f4078adb1fe6de4f61117cd476197882b2
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-28 01:41:12 -0500

    Make ConciseTable a functional Component

--- frontend/src/DeviceDetailsPage/DeviceDetailsConciseTable.jsx
+++ frontend/src/DeviceDetailsPage/DeviceDetailsConciseTable.jsx
@@ -5,25 +5,24 @@ import PropTypes from "prop-types";
 import DeviceDetailsConciseRow from "./DeviceDetailsConciseRow";
 import DeviceInfo from "../model/DeviceInfo";
 
-export default class DeviceDetailsConciseTable extends React.Component {
-  render() {
-    return (
-      <>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableBody>
-              {DeviceInfo.getProperties().map((property) => (
-                <DeviceDetailsConciseRow
-                  name={property}
-                  value={this.props.device[property]}
-                />
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </>
-    );
-  }
+export default function DeviceDetailsConciseTable(props) {
+  const { device } = props;
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableBody>
+            {DeviceInfo.getProperties().map((property) => (
+              <DeviceDetailsConciseRow
+                name={property}
+                value={device[property]}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
 
 DeviceDetailsConciseTable.propTypes = {
