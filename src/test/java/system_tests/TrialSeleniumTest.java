fc5b368df88c66e7d4cef71d258201cffeb1fd85
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2020-11-28 10:12:46 -0500

    Clean up the code

--- src/test/java/system_tests/TrialSeleniumTest.java
+++ src/test/java/system_tests/TrialSeleniumTest.java
@@ -1,10 +1,5 @@
 package system_tests;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,11 +8,9 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static system_tests.HttpHandler.postRequest;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -50,31 +43,7 @@ public class TrialSeleniumTest {
     void testAddDecoder(){
         driver.get("http://localhost:3000/Devices");
         driver.manage().window().setSize(new Dimension(782, 818));
-
-//        //populate Name, Serial Name and IP Address fields
-//        driver.findElement(By.id("nameSearch")).sendKeys("Sender Test10");
-//        driver.findElement(By.id("serialSearch")).sendKeys("1:10:111:999");
-//        driver.findElement(By.id("ipSearch")).sendKeys("123:456");
-//
-//        //select Online status
-//        {
-//            WebElement element = driver.findElement(By.id("statusSearch"));
-//            Actions builder = new Actions(driver);
-//            builder.moveToElement(element).clickAndHold().perform();
-//        }
-//        {
-//            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-//            WebElement element = driver.findElement(By.cssSelector("#menu- > div:nth-child(1)"));
-//            Actions builder = new Actions(driver);
-//            builder.moveToElement(element).release().perform();
-//        }
-//        driver.findElement(By.cssSelector("body")).click();
-//        driver.findElement(By.cssSelector(".MuiListItem-dense:nth-child(1)")).click();
-//
-//        driver.findElement(By.id("DeviceListAddDevBtn")).click();
-//
-//        //Check if the device was added
-//        driver.navigate().refresh();
+        
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         List<WebElement> devicesRows = driver.findElements(By.className("singleDeviceRow"));
