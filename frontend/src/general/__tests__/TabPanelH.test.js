3c2c7a16948ad5c02415a8dda673047174c5271d
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-28 17:58:39 -0500

    CLeanup

--- frontend/src/general/__tests__/TabPanelH.test.js
+++ frontend/src/general/__tests__/TabPanelH.test.js
@@ -10,47 +10,47 @@ Enzyme.configure({ adapter: new Adapter() });
 jest.spyOn(global.console, "error");
 
 describe("Horizontal Tab Panel TabPanelH", () => {
-    let wrapper;
+  let wrapper;
 
-    afterEach(() => {
-        jest.clearAllMocks();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("Throws an error when", () => {
+    const goodValues = {
+      index: 1,
+      value: 9
+    };
+    it("Not called with props", () => {
+      wrapper = Enzyme.shallow(<TabPanelH />);
+      expect(console.error).toHaveBeenCalled();
     });
-
-    describe("Throws an error when", () => {
-        const goodValues = {
-            index: 1,
-            value: 9
-        };
-        it("Not called with props", () => {
-            wrapper = Enzyme.shallow(<TabPanelH />);
-            expect(console.error).toHaveBeenCalled();
-        });
-        it("called with an index that is not a number", () => {
-            const badIndex = "BadIndex";
-            wrapper = Enzyme.shallow(
-                <TabPanelH index={badIndex} value={goodValues.value} />
-            );
-            expect(console.error).toHaveBeenCalled();
-        });
-        it("Does not render children if the index and value are different", () => {
-            wrapper = Enzyme.shallow(
-                <TabPanelH index={1} value={5}>
-                    <div id="unique" />
-                </TabPanelH>
-            );
-
-            expect(wrapper.children).toHaveLength(1);
-            expect(wrapper.find(Box)).toHaveLength(0);
-        });
+    it("called with an index that is not a number", () => {
+      const badIndex = "BadIndex";
+      wrapper = Enzyme.shallow(
+        <TabPanelH index={badIndex} value={goodValues.value} />
+      );
+      expect(console.error).toHaveBeenCalled();
     });
-    it("Renders any children passed only if the index and value are the same", () => {
-        wrapper = Enzyme.shallow(
-            <TabPanelH index={1} value={1}>
-                <div id="unique" />
-            </TabPanelH>
-        );
+    it("Does not render children if the index and value are different", () => {
+      wrapper = Enzyme.shallow(
+        <TabPanelH index={1} value={5}>
+          <div id="unique" />
+        </TabPanelH>
+      );
 
-        expect(wrapper.children).toHaveLength(1);
-        expect(wrapper.find(Box)).toHaveLength(1);
+      expect(wrapper.children).toHaveLength(1);
+      expect(wrapper.find(Box)).toHaveLength(0);
     });
+  });
+  it("Renders any children passed only if the index and value are the same", () => {
+    wrapper = Enzyme.shallow(
+      <TabPanelH index={1} value={1}>
+        <div id="unique" />
+      </TabPanelH>
+    );
+
+    expect(wrapper.children).toHaveLength(1);
+    expect(wrapper.find(Box)).toHaveLength(1);
+  });
 });
