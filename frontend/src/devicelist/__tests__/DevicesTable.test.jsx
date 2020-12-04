fddec70183c950e70bee0bcf40c637842d48d25d
Jun Loke <rebecca.j.loke@gmail.com>
2020-12-04 18:12:52 -0500

    linter

--- frontend/src/devicelist/__tests__/DevicesTable.test.jsx
+++ frontend/src/devicelist/__tests__/DevicesTable.test.jsx
@@ -9,53 +9,61 @@ import {
   jest,
   it
 } from "@jest/globals";
-import DevicesTable from "../DevicesTable";
 import { Box, TableContainer } from "@material-ui/core";
 import MaterialTable from "material-table";
+import DevicesTable from "../DevicesTable";
 
 Enzyme.configure({ adapter: new Adapter() });
 jest.spyOn(global.console, "error");
 
-describe("<DevicesTable/> component", ()=>{
-    let wrapper;
+describe("<DevicesTable/> component", () => {
+  let wrapper;
 
-    afterEach(() => {
-        jest.clearAllMocks();
-      });
-      
-    describe("has the correct components", ()=>{
-        const dummyTitle = "TEST TITLE";
-        const dummyDevices = [];
-        beforeEach(()=>{
-            wrapper = Enzyme.shallow(<DevicesTable title = {dummyTitle} devices = {dummyDevices}/>);
-        });
-        it("contains one (1) Box component", ()=>{
-            expect(wrapper.find(Box)).toHaveLength(1);
-        });
-        it("contains one (1) TableContainer component", () => {
-            expect(wrapper.find(TableContainer)).toHaveLength(1);
-        });
-        it("contains one (1) MaterialTable component", () => {
-            expect(wrapper.find(MaterialTable)).toHaveLength(1);
-        });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
+  describe("has the correct components", () => {
+    const dummyTitle = "TEST TITLE";
+    const dummyDevices = [];
+    beforeEach(() => {
+      wrapper = Enzyme.shallow(
+        <DevicesTable title={dummyTitle} devices={dummyDevices} />
+      );
     });
-    describe("Props validation", ()=>{
-        const validTitle = "TEST TITLE";
-        const validDevices = [];
-        it("Does not log error if inputs are valid", ()=>{
-            wrapper = Enzyme.shallow(<DevicesTable title = {validTitle} devices = {validDevices}/>);
-            expect(console.error).not.toHaveBeenCalled();
-        })
-        it("logs an error if title is a string", ()=>{
-            const notAString=47384;
-            wrapper = Enzyme.shallow(<DevicesTable title = {notAString} devices = {validDevices}/>);
-            expect(console.error).toHaveBeenCalled();
-        });
-        it("logs an error if devices is not an array of DeviceInfo objects", ()=>{
-            const notAnArrayOfDeviceInfo=47384;
-            wrapper = Enzyme.shallow(<DevicesTable title = {validTitle} devices = {notAnArrayOfDeviceInfo}/>);
-            expect(console.error).toHaveBeenCalled();
-        });
-    })
-})
+    it("contains one (1) Box component", () => {
+      expect(wrapper.find(Box)).toHaveLength(1);
+    });
+    it("contains one (1) TableContainer component", () => {
+      expect(wrapper.find(TableContainer)).toHaveLength(1);
+    });
+    it("contains one (1) MaterialTable component", () => {
+      expect(wrapper.find(MaterialTable)).toHaveLength(1);
+    });
+  });
+  describe("Props validation", () => {
+    /*  eslint-disable no-console */
+    const validTitle = "TEST TITLE";
+    const validDevices = [];
+    it("Does not log error if inputs are valid", () => {
+      wrapper = Enzyme.shallow(
+        <DevicesTable title={validTitle} devices={validDevices} />
+      );
+      expect(console.error).not.toHaveBeenCalled();
+    });
+    it("logs an error if title is a string", () => {
+      const notAString = 47384;
+      wrapper = Enzyme.shallow(
+        <DevicesTable title={notAString} devices={validDevices} />
+      );
+      expect(console.error).toHaveBeenCalled();
+    });
+    it("logs an error if devices is not an array of DeviceInfo objects", () => {
+      const notAnArrayOfDeviceInfo = 47384;
+      wrapper = Enzyme.shallow(
+        <DevicesTable title={validTitle} devices={notAnArrayOfDeviceInfo} />
+      );
+      expect(console.error).toHaveBeenCalled();
+    });
+  });
+});
