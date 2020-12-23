0cb14379462a63610c623dfc6a625a2c81a4115f
Mohamed Hefny <mohhef@rocketmail.com>
2020-12-24 00:16:02 +0400

    Refactor code by removing configclass

--- src/main/java/org/beanpod/switchboard/config/AopConfig.java
+++
@@ -1,8 +0,0 @@
-package org.beanpod.switchboard.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy
-public class AopConfig {}
