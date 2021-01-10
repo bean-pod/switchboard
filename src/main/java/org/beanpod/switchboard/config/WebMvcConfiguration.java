8ae614b71fba4cb95cf797a2e8ee1c3d285e5432
Felix Lapierre <felix.lapierre1@gmail.com>
2021-01-09 21:38:05 -0500

    Removed allowedOrigins(*) to address security hotspot

--- src/main/java/org/beanpod/switchboard/config/WebMvcConfiguration.java
+++ src/main/java/org/beanpod/switchboard/config/WebMvcConfiguration.java
@@ -12,10 +12,7 @@ public class WebMvcConfiguration {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
       }
     };
   }
