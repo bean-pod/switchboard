0cb00a4e69fb0fa079d40faf1650edb38addf5bf
Mohamed Hefny <mohhef@rocketmail.com>
2021-01-10 16:25:39 -0500

    Deal with hard headed sonar by using optional class

--- src/main/java/org/beanpod/switchboard/aop/StreamAspect.java
+++ src/main/java/org/beanpod/switchboard/aop/StreamAspect.java
@@ -1,10 +1,15 @@
 package org.beanpod.switchboard.aop;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.beanpod.switchboard.exceptions.ExceptionType;
+import org.beanpod.switchboard.exceptions.ExceptionType.UnknownException;
 import org.beanpod.switchboard.service.LogService;
+import org.openapitools.model.DecoderModel;
+import org.openapitools.model.EncoderModel;
+import org.openapitools.model.InputChannelModel;
+import org.openapitools.model.OutputChannelModel;
 import org.openapitools.model.StreamModel;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +21,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class StreamAspect {
 
+  String stream = "stream";
   private final LogService logService;
 
   @AfterReturning(
@@ -23,19 +29,34 @@ public class StreamAspect {
       returning = "result")
   public void createStream(Object result) {
     ResponseEntity<StreamModel> response = (ResponseEntity<StreamModel>) result;
-    String decoderSerial = response.getBody().getInputChannel().getDecoder().getSerialNumber();
-    String encoderSerial = response.getBody().getOutputChannel().getEncoder().getSerialNumber();
-    Long outputId = response.getBody().getOutputChannel().getId();
-    Long inputId = response.getBody().getInputChannel().getId();
-    if (decoderSerial != null && encoderSerial != null && outputId != null && inputId != null) {
-      String message =
+
+    String decoderSerial = Optional.of(response.getBody())
+        .map(StreamModel::getInputChannel)
+        .map(InputChannelModel::getDecoder)
+        .map(DecoderModel::getSerialNumber)
+        .orElseThrow(() -> new UnknownException(stream));
+
+    String encoderSerial = Optional.of(response.getBody())
+        .map(StreamModel::getOutputChannel)
+        .map(OutputChannelModel::getEncoder)
+        .map(EncoderModel::getSerialNumber)
+        .orElseThrow(() -> new UnknownException(stream));
+
+    Long outputId = Optional.of(response.getBody())
+        .map(StreamModel::getOutputChannel)
+        .map(OutputChannelModel::getId)
+        .orElseThrow(() -> new UnknownException(stream));
+
+    Long inputId = Optional.of(response.getBody())
+        .map(StreamModel::getInputChannel)
+        .map(InputChannelModel::getId)
+        .orElseThrow(() -> new UnknownException(stream));
+     String message =
           String.format(
               "A stream started from output channel %d of decoder %s"
                   + " to input channel %d of encoder %s",
               outputId, decoderSerial, inputId, encoderSerial);
       logService.createLog(message, "info");
-    } else {
-      throw new ExceptionType.UnknownException("stream");
-    }
+
   }
 }
