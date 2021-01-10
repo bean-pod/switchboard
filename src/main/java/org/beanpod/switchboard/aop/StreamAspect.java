c56e4f21ff39c69b58a951001bae09319b86fde0
Mohamed Hefny <mohhef@rocketmail.com>
2021-01-10 15:40:58 -0500

    refactor code to handle null exceptions

--- src/main/java/org/beanpod/switchboard/aop/StreamAspect.java
+++ src/main/java/org/beanpod/switchboard/aop/StreamAspect.java
@@ -3,6 +3,7 @@ package org.beanpod.switchboard.aop;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.beanpod.switchboard.exceptions.ExceptionType;
 import org.beanpod.switchboard.service.LogService;
 import org.openapitools.model.StreamModel;
 import org.springframework.context.annotation.Configuration;
@@ -22,15 +23,21 @@ public class StreamAspect {
       returning = "result")
   public void createStream(Object result) {
     ResponseEntity<StreamModel> response = (ResponseEntity<StreamModel>) result;
-    String decoderSerial = response.getBody().getInputChannel().getDecoder().getSerialNumber();
-    String encoderSerial = response.getBody().getOutputChannel().getEncoder().getSerialNumber();
-    Long outputId = response.getBody().getOutputChannel().getId();
-    Long inputId = response.getBody().getInputChannel().getId();
-    String message =
-        String.format(
-            "A stream started from output channel %d of decoder %s"
-                + " to input channel %d of encoder %s",
-            outputId, decoderSerial, inputId, encoderSerial);
-    logService.createLog(message, "info");
+    if (response.getBody() != null) {
+      String decoderSerial = response.getBody().getInputChannel().getDecoder().getSerialNumber();
+      String encoderSerial = response.getBody().getOutputChannel().getEncoder().getSerialNumber();
+      Long outputId = response.getBody().getOutputChannel().getId();
+      Long inputId = response.getBody().getInputChannel().getId();
+      String message =
+          String.format(
+              "A stream started from output channel %d of decoder %s"
+                  + " to input channel %d of encoder %s",
+              outputId, decoderSerial, inputId, encoderSerial);
+      logService.createLog(message, "info");
+      }
+    else{
+      throw new ExceptionType.UnknownException("stream");
+    }
+
   }
 }
