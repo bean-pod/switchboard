3ac21794a2d4421a69420c02adc32f806ce0ce17
Sharon Ho <sharon.cy.ho@gmail.com>
2021-01-18 19:46:57 -0500

    Fix test related to layout change

--- frontend/src/DeviceDetailsPage/__tests__/DeviceDetailsPage.test.jsx
+++ frontend/src/DeviceDetailsPage/__tests__/DeviceDetailsPage.test.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Button, Container, Grid } from "@material-ui/core";
+import { Box, Button, Container, Grid } from "@material-ui/core";
+import EditIcon from '@material-ui/icons/Edit';
 
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { afterEach, describe, expect, jest, it } from "@jest/globals";
 
 import DeviceInfo from "../../model/DeviceInfo";
-import DeviceDetailsConciseTable from "../DeviceDetailsConciseTable";
 import DynamicBreadcrumb from "../../general/DynamicBreadcrumb";
 import DeviceDetailsTabTable from "../DeviceDetailsTabTable";
 import DeviceDetailsPage from "../DeviceDetailsPage";
+import DeviceDetailsConciseTable from "../DeviceDetailsConciseTable";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -42,8 +43,12 @@ describe("DeviceDetailsPage", () => {
     wrapper = Enzyme.shallow(<DeviceDetailsPage location={dummyLocation} />);
     expect(wrapper.find(Container)).toHaveLength(1);
     expect(wrapper.find(DynamicBreadcrumb)).toHaveLength(1);
+    expect(wrapper.find(Box)).toHaveLength(5);
     expect(wrapper.find(Button)).toHaveLength(1);
+    expect(wrapper.find(EditIcon)).toHaveLength(1);
     expect(wrapper.find("DeleteDeviceButton")).toHaveLength(1);
+    expect(wrapper.find(Grid)).toHaveLength(3);
+    expect(wrapper.find(DeviceDetailsConciseTable)).toHaveLength(1);
     expect(wrapper.find(DeviceDetailsTabTable)).toHaveLength(1);
   });
 });
