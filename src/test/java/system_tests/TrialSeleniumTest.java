55739dc3c833aa5859d4f91f6bbbc3623619cfd0
Joshua Butler <butler.joshua355@gmail.com>
2021-01-20 19:06:42 -0500

    Refactored code, and added an add decoder test

--- src/test/java/system_tests/TrialSeleniumTest.java
+++ src/test/java/system_tests/TrialSeleniumTest.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.beanpod.switchboard.entity.DeviceEntity;
-import org.beanpod.switchboard.fixture.DeviceFixture;
-import org.junit.jupiter.api.AfterEach;
+import org.beanpod.switchboard.entity.DecoderEntity;
+import org.beanpod.switchboard.entity.EncoderEntity;
+import org.beanpod.switchboard.fixture.DecoderFixture;
+import org.beanpod.switchboard.fixture.EncoderFixture;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -21,53 +23,110 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TrialSeleniumTest {
 
   private static WebDriver driver;
-  private static DeviceEntity deviceEntity;
+  private static HashMap<String, String> testSenderDeviceParams;
+  private static HashMap<String, String> testSenderEncoderParams;
+  private static HashMap<String, String> testReceiverDeviceParams;
+  private static HashMap<String, String> testReceiverDecoderParams;
 
   @BeforeAll
-  static void setUp() throws IOException {
+  static void setUp() {
+    // create a test sender
+    EncoderEntity testSender = EncoderFixture.getEncoderEntity1();
+
+    testSenderDeviceParams = new HashMap<>();
+    testSenderDeviceParams.put("serialNumber", testSender.getDevice().getSerialNumber());
+    testSenderDeviceParams.put("displayName", testSender.getDevice().getDisplayName());
+    testSenderDeviceParams.put("status", testSender.getDevice().getStatus());
+    testSenderDeviceParams.put("privateIpAddress", testSender.getDevice().getPrivateIpAddress());
+    testSenderDeviceParams.put("publicIpAddress", testSender.getDevice().getPublicIpAddress());
+
+    testSenderEncoderParams = new HashMap<>();
+    testSenderEncoderParams.put("serialNumber", testSender.getSerialNumber());
+    testSenderEncoderParams.put("lastCommunication", testSender.getLastCommunication().toString());
+
+    // create a test receiver
+    DecoderEntity testReceiver = DecoderFixture.getDecoderEntity2();
+
+    testReceiverDeviceParams = new HashMap<>();
+    testReceiverDeviceParams.put("serialNumber", testReceiver.getDevice().getSerialNumber());
+    testReceiverDeviceParams.put("displayName", testReceiver.getDevice().getDisplayName());
+    testReceiverDeviceParams.put("status", testReceiver.getDevice().getStatus());
+    testReceiverDeviceParams.put("privateIpAddress", testReceiver.getDevice().getPrivateIpAddress());
+    testReceiverDeviceParams.put("publicIpAddress", testReceiver.getDevice().getPublicIpAddress());
+
+    testReceiverDecoderParams = new HashMap<>();
+    testReceiverDecoderParams.put("serialNumber", testSender.getSerialNumber());
+    testReceiverDecoderParams.put("lastCommunication", testSender.getLastCommunication().toString());
+
+    // Set up Selenium Chrome Driver
     // TODO set it up for mac/linux:
     // https://stackoverflow.com/questions/228477/how-do-i-programmatically-determine-operating-system-in-java
     System.setProperty(
         "webdriver.chrome.driver", "src\\test\\java\\system_tests\\chromedriver.exe");
     driver = new ChromeDriver();
-
-    // create a test device
-    deviceEntity = DeviceFixture.getDevice1();
-    HashMap<String, String> deviceParam = new HashMap<>();
-    deviceParam.put("serialNumber", deviceEntity.getSerialNumber());
-    deviceParam.put("displayName", deviceEntity.getDisplayName());
-    deviceParam.put("status", deviceEntity.getStatus());
-    deviceParam.put("privateIpAddress", deviceEntity.getPrivateIpAddress());
-    deviceParam.put("publicIpAddress", deviceEntity.getPublicIpAddress());
-
-    // create a test encoder
-    HashMap<String, String> encoderParam = new HashMap<>();
-    encoderParam.put("serialNumber", deviceEntity.getSerialNumber());
-    encoderParam.put("lastCommunication", "2020-11-28 09:40:00");
-
-    postRequest("http://localhost:8080/device", deviceParam);
-    postRequest("http://localhost:8080/encoder", encoderParam);
+    driver.get("http://localhost:3000/");
+    driver.manage().window().setSize(new Dimension(782, 818));
+    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
   }
 
   @Test
-  void testAddEncoder() {
-    driver.get("http://localhost:3000/Devices");
-    driver.manage().window().setSize(new Dimension(782, 818));
+  void testAddEncoder() throws IOException {
+    // Mock sender self-registration
+    postRequest("http://localhost:8080/device", testSenderDeviceParams);
+    postRequest("http://localhost:8080/encoder", testSenderEncoderParams);
 
-    driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    // Go to List of Senders
+    driver.navigate().to("http://localhost:3000/Devices");
 
+    // Check for encoder
     WebElement encodersTable = driver.findElement(By.tagName("table")); // find encoders table
     List<WebElement> devicesRows =
         encodersTable.findElements(By.tagName("tr")); // find all tr elements inside found table
-
     boolean assertValue =
-        devicesRows.stream().anyMatch(row -> row.getText().contains(deviceEntity.getDisplayName()));
+        devicesRows.stream().anyMatch(row -> row.getText().contains(testSenderDeviceParams.get("displayName")));
 
     assertTrue(assertValue);
   }
 
-  @AfterEach
-  void tearDown() {
+  @Test
+  void testAddDecoder() throws IOException {
+    // mock receiver self-registration
+    postRequest("http://localhost:8080/device", testReceiverDeviceParams);
+    postRequest("http://localhost:8080/decoder", testReceiverDecoderParams);
+
+    // Go to List of Receivers
+    driver.findElement(By.xpath("//button[@id='vertical-tab-1']/span")).click();
+
+    // Check for decoder
+    WebElement decodersTable = driver.findElement(By.tagName("table")); // find encoders table
+    List<WebElement> devicesRows =
+        decodersTable.findElements(By.tagName("tr")); // find all tr elements inside found table
+    boolean assertValue =
+        devicesRows.stream().anyMatch(row -> row.getText().contains(testReceiverDeviceParams.get("displayName")));
+
+    assertTrue(assertValue);
+  }
+
+  // TODO
+  @Test
+  void testCreateStream() {
+
+  }
+
+  // TODO
+  @Test
+  void testViewStream() {
+
+  }
+
+  // TODO
+  @Test
+  void testDeleteStream() {
+
+  }
+
+  @AfterAll
+  static void tearDown() {
     driver.quit();
   }
 }
