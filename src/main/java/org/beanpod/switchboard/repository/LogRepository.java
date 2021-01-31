69409468eb0d05cd6946c4a6c37959c8bc5686f2
Felix Lapierre <felix.lapierre1@gmail.com>
2021-01-30 19:24:37 -0500

    Google Java Format

--- src/main/java/org/beanpod/switchboard/repository/LogRepository.java
+++ src/main/java/org/beanpod/switchboard/repository/LogRepository.java
@@ -1,11 +1,10 @@
 package org.beanpod.switchboard.repository;
 
+import java.util.List;
 import org.beanpod.switchboard.entity.LogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface LogRepository extends JpaRepository<LogEntity, Long> {
