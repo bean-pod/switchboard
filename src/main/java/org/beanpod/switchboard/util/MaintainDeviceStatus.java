92d65cda46c30abed82fd8ad8e4076675ad5c10a
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-01 20:32:48 -0500

    Implement createLog and getDateToBeCompared methods which are helpers for the maintainStatusField method

--- src/main/java/org/beanpod/switchboard/util/MaintainDeviceStatus.java
+++ src/main/java/org/beanpod/switchboard/util/MaintainDeviceStatus.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.beanpod.switchboard.dao.DeviceDaoImpl;
 import org.beanpod.switchboard.dto.mapper.DeviceMapper;
 import org.beanpod.switchboard.entity.EncoderEntity;
+import org.beanpod.switchboard.service.LogService;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -16,26 +17,39 @@ public class MaintainDeviceStatus {
     private static DateUtil date = new DateUtil();
     private final DeviceDaoImpl service;
     private final DeviceMapper deviceMapper;
+    private final LogService logService;
+
     //E should be of type DecoderEntity or EncoderEntity
-    public <E> void maintainStatusField(List<EncoderEntity> devices){
+    public void maintainStatusField(List<EncoderEntity> devices){
+        Date dateToBeCompared = getDateToBeCompared();
+
+        for(int i=0;i<devices.size();i++){
+            //if status field equals online and lastCommunication is more than 10minutes old
+            if(((devices.get(i)).getDevice().getStatus()).equalsIgnoreCase("online")
+                    && dateToBeCompared.after(devices.get(i).getLastCommunication())){
+                //update last_communication field to offline
+                devices.get(i).getDevice().setStatus("offline");
+                service.save(deviceMapper.toDeviceDto(devices.get(i).getDevice()));
+
+                //create a log
+                createLog("offline", devices.get(i).getSerialNumber());
+            }
+        }
+    }
+
+
+    private void createLog(String status, String serialNumber){
+        String message = "A device has been updated with a status of " + status;
+        logService.createLog(message, "info", serialNumber);
+    }
+
+    private Date getDateToBeCompared(){
         //subtract 10minutes from the current date
         Date dateToBeCompared = date.getCurrentDate();
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         dateToBeCompared.setTime(System.currentTimeMillis()-600000);
 
-        for(int i=0;i<devices.size();i++){
-
-            //if lastCommunication is more than 10minutes old
-            if((devices.get(i).getDevice().getStatus()).equalsIgnoreCase("online")
-                    && dateToBeCompared.after(devices.get(i).getLastCommunication())){
-                System.out.print("--------------------> b4");
-                devices.get(i).getDevice().setStatus("offline");
-                service.save(deviceMapper.toDeviceDto(devices.get(i).getDevice()));
-                System.out.print("--------------------> in");
-            }
-        }
-
+        return dateToBeCompared;
     }
-
 
 }
