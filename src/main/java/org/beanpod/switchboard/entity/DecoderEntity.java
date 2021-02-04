47a18c3bbb9afc37bb408ca1a503942044dbc35d
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-04 02:11:11 -0500

    Make DecoderEntity and DecoderEntity classes implement DummyInterface

--- src/main/java/org/beanpod/switchboard/entity/DecoderEntity.java
+++ src/main/java/org/beanpod/switchboard/entity/DecoderEntity.java
@@ -2,25 +2,12 @@ package org.beanpod.switchboard.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapsId;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity(name = "Decoder")
 @Getter
@@ -29,7 +16,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(value = {"hibernateLazyIntializer", "handler"})
-public class DecoderEntity {
+public class DecoderEntity implements dummyInterface{
 
   @Id
   @NotNull
