36e744cc3bf10dd91713c99366c5ff0d8c48d5e0
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-04 02:10:02 -0500

    Implement a dummy interface to avoid duplicating maintainStatusField method for EncoderEntity and DecoderEntity objects

---
+++ src/main/java/org/beanpod/switchboard/entity/dummyInterface.java
@@ -0,0 +1,9 @@
+package org.beanpod.switchboard.entity;
+
+import java.util.Date;
+
+public interface dummyInterface {
+    public DeviceEntity getDevice();
+    public Date getLastCommunication();
+    public String getSerialNumber();
+}
