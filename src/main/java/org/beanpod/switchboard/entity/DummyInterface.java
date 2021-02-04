7959ad29045a7f15f284393af6935dc42c3f5a14
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-04 02:15:23 -0500

    Rename interface name to match naming conventions

--- src/main/java/org/beanpod/switchboard/entity/DummyInterface.java
+++ src/main/java/org/beanpod/switchboard/entity/DummyInterface.java
@@ -2,7 +2,7 @@ package org.beanpod.switchboard.entity;
 
 import java.util.Date;
 
-public interface dummyInterface {
+public interface DummyInterface {
     public DeviceEntity getDevice();
     public Date getLastCommunication();
     public String getSerialNumber();
