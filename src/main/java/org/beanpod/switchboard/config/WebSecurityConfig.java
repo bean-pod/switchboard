45ff24fc524ee8a9756233ed0a966c317b1d0368
Mohamed Hefny <mohhef@rocketmail.com>
2021-02-07 17:58:51 -0500

    Add confirmation token functionality and config file

--- src/main/java/org/beanpod/switchboard/config/WebSecurityConfig.java
+++ src/main/java/org/beanpod/switchboard/config/WebSecurityConfig.java
@@ -3,9 +3,10 @@ package org.beanpod.switchboard.config;
 import lombok.AllArgsConstructor;
 import org.beanpod.switchboard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,31 +16,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserService userService;
 
-  private final BCryptPasswordEncoder bcryptPasswordEncoder;
 
-  // TODO remove this method when sign in functionality is implemented
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/*/**");
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+    return new BCryptPasswordEncoder();
   }
 
-  // TODO uncomment this block when sign in functionality is enabled, this secures the whole website
-  //  @Override
-  //  protected void configure(HttpSecurity http) throws Exception {
-  //
-  //    http.authorizeRequests()
-  //        .antMatchers("/sign-up/**", "/sign-in/**")
-  //        .permitAll()
-  //        .anyRequest()
-  //        .authenticated()
-  //        .and()
-  //        .formLogin()
-  //        .loginPage("/sign-in")
-  //        .permitAll();
-  //  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http
+        .authorizeRequests()
+        .antMatchers("/sign-up/**", "/sign-in/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/sign-in")
+        .permitAll();
+  }
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userService).passwordEncoder(bcryptPasswordEncoder);
+    auth.userDetailsService(userService)
+        .passwordEncoder(bCryptPasswordEncoder());
   }
+
+
 }
