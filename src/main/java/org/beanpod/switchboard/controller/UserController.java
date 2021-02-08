e4e7b5ab73d8ca8a76c3eb84003fa8dec5ba75ae
Mohamed Hefny <mohhef@rocketmail.com>
2021-02-08 00:29:53 -0500

    override the user controller in open api generated code

--- src/main/java/org/beanpod/switchboard/controller/UserController.java
+++ src/main/java/org/beanpod/switchboard/controller/UserController.java
@@ -2,32 +2,29 @@ package org.beanpod.switchboard.controller;
 
 import lombok.AllArgsConstructor;
 import org.beanpod.switchboard.dao.UserDaoImpl;
-import org.beanpod.switchboard.entity.UserEntity;
+import org.openapitools.api.UserApi;
 import org.openapitools.model.UserModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 @AllArgsConstructor
-public class UserController  {
+public class UserController implements UserApi {
 
   private final UserDaoImpl userDao;
 
   @GetMapping("/sign-up")
-  public String signUpPage(UserEntity user) {
+  public String signUpPage(UserModel user) {
 
     return "sign-up";
   }
 
-  @RequestMapping(value="/user/sign-up", method= RequestMethod.POST)
-  public String signUp(@ModelAttribute("userEntity")UserModel user) {
+  @Override
+  public ResponseEntity<String> signUp(UserModel user) {
     userDao.save(user);
-    return "redirect:/sign-up";
-//    return ResponseEntity.ok("User "+ user.getEmail() + " has been successfully saved");
+    return ResponseEntity.ok("User "+ user.getEmail() + " has been successfully saved");
   }
 
 
