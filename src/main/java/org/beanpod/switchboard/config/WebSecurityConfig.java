4feb4057e36128c05779d067285943b6ca3c5088
Mohamed Hefny <mohhef@rocketmail.com>
2021-02-08 00:26:05 -0500

    code for thymeleaf testing

--- src/main/java/org/beanpod/switchboard/config/WebSecurityConfig.java
+++ src/main/java/org/beanpod/switchboard/config/WebSecurityConfig.java
@@ -5,7 +5,7 @@ import org.beanpod.switchboard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -17,19 +17,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+  //TODO remove this method when sign in functionality is implemented
   @Override
-  protected void configure(HttpSecurity http) throws Exception {
-
-    http.authorizeRequests()
-        .antMatchers("/sign-up/**", "/sign-in/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .formLogin()
-        .loginPage("/sign-in")
-        .permitAll();
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/*/**");
   }
+
+  //TODO uncomment this block when sign in functionality is enabled, this secures the whole website
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//
+//    http.authorizeRequests()
+//        .antMatchers("/sign-up/**", "/sign-in/**")
+//        .permitAll()
+//        .anyRequest()
+//        .authenticated()
+//        .and()
+//        .formLogin()
+//        .loginPage("/sign-in")
+//        .permitAll();
+//  }
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
