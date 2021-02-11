a1b215c2d7645255cdc5913e4036f1ef5a1ea922
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-11 08:23:18 -0500

    Extract the logging functionality to an Aspect class

---
+++ src/main/java/org/beanpod/switchboard/aop/MaintainDeviceStatusAspect.java
@@ -0,0 +1,44 @@
+package org.beanpod.switchboard.aop;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.beanpod.switchboard.entity.DeviceEntity;
+import org.beanpod.switchboard.service.LogService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class MaintainDeviceStatusAspect {
+  private final LogService logService;
+
+  @AfterReturning(
+      value =
+          "execution(* org.beanpod.switchboard.util.MaintainDeviceStatus.maintainStatusField(org.beanpod.switchboard.dto.StreamDto))",
+      returning = "updatedDevices")
+  public void createLogStream(List<DeviceEntity> updatedDevices) {
+    createLogHelper(updatedDevices);
+  }
+
+  @AfterReturning(
+      value =
+          "execution(* org.beanpod.switchboard.util.MaintainDeviceStatus.maintainStatusField(java.util.List))",
+      returning = "updatedDevices")
+  public void createLogEncoderAndDecoder(List<DeviceEntity> updatedDevices) {
+    createLogHelper(updatedDevices);
+  }
+
+  // helper method to create logs
+  private void createLogHelper(List<DeviceEntity> updatedDevices) {
+    if (!updatedDevices.isEmpty()) {
+      for (DeviceEntity device : updatedDevices) {
+        String message = "A device has been updated with a status of " + device.getStatus();
+        logService.createLog(message, "info", device.getSerialNumber());
+      }
+    }
+  }
+}
