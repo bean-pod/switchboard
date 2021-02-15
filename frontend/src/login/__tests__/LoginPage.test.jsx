ba249adbd5cbc38ae08e5d85285956c2bb4352c0
Felix Lapierre <felix.lapierre1@gmail.com>
2021-02-15 18:50:32 -0500

    Split login page tests into 3

--- frontend/src/login/__tests__/LoginPage.test.jsx
+++ frontend/src/login/__tests__/LoginPage.test.jsx
@@ -13,12 +13,18 @@ describe("LoginPage", () => {
     wrapper = Enzyme.mount(<LoginPage />);
   });
 
-  it("Renders with the correct elements", () => {
+  it("Renders one title", () => {
     expect(wrapper.find(".title")).toHaveLength(1);
+  });
+
+  it("Renders with a username input and a password input", () => {
     expect(wrapper.find("input")).toHaveLength(2);
     expect(wrapper.find("label")).toHaveLength(2);
     expect(wrapper.find("label").at(0).text()).toEqual("Username *");
     expect(wrapper.find("label").at(1).text()).toEqual("Password *");
+  });
+
+  it("Renders a log in button", () => {
     expect(wrapper.find("button")).toHaveLength(1);
     expect(wrapper.find("button span.MuiButton-label").text()).toEqual(
       "Log In"
