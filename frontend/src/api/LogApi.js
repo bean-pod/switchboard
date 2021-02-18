1b7e3a759cfdd9ed95fe796c04020079a4511ca1
Sharon Ho <sharon.cy.ho@gmail.com>
2021-02-18 05:45:23 -0500

    Eliminate code smell

--- frontend/src/api/LogApi.js
+++ frontend/src/api/LogApi.js
@@ -2,15 +2,19 @@ import axios from "axios";
 import LogInfo from "../model/LogInfo";
 import * as SampleData from "./SampleData";
 
+function getLogs(response) {
+  return Promise.resolve(
+    response.data.map((log) => {
+      return new LogInfo(log.id, log.dateTime, log.level, log.message);
+    })
+  );
+}
+
 export async function getAllLogs() {
   return axios
     .get(process.env.REACT_APP_LOGS)
     .then((response) => {
-      return Promise.resolve(
-        response.data.map((log) => {
-          return new LogInfo(log.id, log.dateTime, log.level, log.message);
-        })
-      );
+      getLogs(response);
     })
     .catch(() => {
       return new Promise((resolve) => {
@@ -23,11 +27,7 @@ export async function getDeviceLogs(deviceSerialNumber) {
   return axios
     .get(`${process.env.REACT_APP_LOGS}/${deviceSerialNumber}`)
     .then((response) => {
-      return Promise.resolve(
-        response.data.map((log) => {
-          return new LogInfo(log.id, log.dateTime, log.level, log.message);
-        })
-      );
+      getLogs(response);
     })
     .catch(() => {
       return new Promise((resolve) => {
