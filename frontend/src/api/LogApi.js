af8a4938f20846f980be29b047bfea0207ab4864
Sharon Ho <sharon.cy.ho@gmail.com>
2021-02-18 06:14:19 -0500

    Change back LogApi

--- frontend/src/api/LogApi.js
+++ frontend/src/api/LogApi.js
@@ -2,28 +2,20 @@ import axios from "axios";
 import LogInfo from "../model/LogInfo";
 import * as SampleData from "./SampleData";
 
-function getLogs(response) {
-  return Promise.resolve(
-    response.data.map((log) => {
-      return new LogInfo(log.id, log.dateTime, log.level, log.message);
-    })
-  );
-}
-
-function getSampleLogs() {
-  return new Promise((resolve) => {
-    SampleData.getAllLogs(resolve);
-  });
-}
-
 export async function getAllLogs() {
   return axios
     .get(process.env.REACT_APP_LOGS)
     .then((response) => {
-      getLogs(response);
+      return Promise.resolve(
+        response.data.map((log) => {
+          return new LogInfo(log.id, log.dateTime, log.level, log.message);
+        })
+      );
     })
     .catch(() => {
-      getSampleLogs();
+      return new Promise((resolve) => {
+        SampleData.getAllLogs(resolve);
+      });
     });
 }
 
@@ -31,9 +23,15 @@ export async function getDeviceLogs(deviceSerialNumber) {
   return axios
     .get(`${process.env.REACT_APP_LOGS}/${deviceSerialNumber}`)
     .then((response) => {
-      getLogs(response);
+      return Promise.resolve(
+        response.data.map((log) => {
+          return new LogInfo(log.id, log.dateTime, log.level, log.message);
+        })
+      );
     })
     .catch(() => {
-      getSampleLogs();
+      return new Promise((resolve) => {
+        SampleData.getAllLogs(resolve);
+      });
     });
 }
