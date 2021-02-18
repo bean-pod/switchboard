7632281f10fd7c936448702b4bdc491b075f8981
Sharon Ho <sharon.cy.ho@gmail.com>
2021-02-17 22:23:36 -0500

    Add test for logs table

---
+++ frontend/src/logslist/__tests__/LogsTable.test.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { describe, expect, jest, it } from "@jest/globals";
+
+import { Box, TableContainer } from "@material-ui/core";
+import MaterialTable from "material-table";
+import LogsTable from "../../logslist/LogsTable";
+import * as SampleData from "../../api/SampleData";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.spyOn(global.console, "error");
+
+describe("LogsTable", () => {
+  let wrapper;
+  let dummyLogs = SampleData.getSampleLogs();
+  wrapper = Enzyme.shallow(<LogsTable logs={dummyLogs} />);
+  it("contains one Box component", () => {
+    expect(wrapper.find(Box)).toHaveLength(1);
+  });
+  it("contains one TableContainer component", () => {
+    expect(wrapper.find(TableContainer)).toHaveLength(1);
+  });
+  it("contains one MaterialTable component", () => {
+    expect(wrapper.find(MaterialTable)).toHaveLength(1);
+  });
+  it("contains one Time zone indicator text box", () => {
+    expect(wrapper.text().includes("Time Zone: ".concat(Intl.DateTimeFormat().resolvedOptions().timeZone))).toBe(true);
+  });
+});
