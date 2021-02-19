03fe0022bc7de6d3448cd0ea33686fa0c4c8dcfd
Sharon Ho <sharon.cy.ho@gmail.com>
2021-02-18 22:39:57 -0500

    Fix code smell on duplication in LogApi

--- frontend/src/api/LogApi.js
+++ frontend/src/api/LogApi.js
@@ -3,8 +3,16 @@ import LogInfo from "../model/LogInfo";
 import * as SampleData from "./SampleData";
 
 export async function getAllLogs() {
+  return getLogs(process.env.REACT_APP_LOGS);
+}
+
+export async function getDeviceLogs(deviceSerialNumber) {
+  return getLogs(`${process.env.REACT_APP_LOGS}/${deviceSerialNumber}`);
+}
+
+async function getLogs(endpoint) {
   return axios
-    .get(process.env.REACT_APP_LOGS)
+    .get(endpoint)
     .then((response) => {
       return Promise.resolve(
         response.data.map((log) => {
@@ -19,19 +27,3 @@ export async function getAllLogs() {
     });
 }
 
-export async function getDeviceLogs(deviceSerialNumber) {
-  return axios
-    .get(`${process.env.REACT_APP_LOGS}/${deviceSerialNumber}`)
-    .then((response) => {
-      return Promise.resolve(
-        response.data.map((log) => {
-          return new LogInfo(log.id, log.dateTime, log.level, log.message);
-        })
-      );
-    })
-    .catch(() => {
-      return new Promise((resolve) => {
-        SampleData.getAllLogs(resolve);
-      });
-    });
-}
