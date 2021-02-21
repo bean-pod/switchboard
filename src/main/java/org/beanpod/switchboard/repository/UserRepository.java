86c4c74fdc05c2486c7394eb0fb740c42cf5e9f9
François LaBerge <fgrclaberge@gmail.com>
2021-02-21 14:08:28 -0500

    lint backend

--- src/main/java/org/beanpod/switchboard/repository/UserRepository.java
+++ src/main/java/org/beanpod/switchboard/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package org.beanpod.switchboard.repository;
 
+import java.util.Optional;
 import org.beanpod.switchboard.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
