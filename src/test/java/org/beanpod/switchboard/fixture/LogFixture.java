d635354b38f12c6f862ae8fdba65f07eeca652a7
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-21 06:43:35 -0500

    Update Log fixtures to have a CreateLogRequest object and proper serial number for LogModel. Also, made the zone offset equal to +00:00 which corresponds to UTC timezone

--- src/test/java/org/beanpod/switchboard/fixture/LogFixture.java
+++ src/test/java/org/beanpod/switchboard/fixture/LogFixture.java
@@ -7,18 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 import org.beanpod.switchboard.dto.LogDto;
 import org.beanpod.switchboard.entity.LogEntity;
+import org.openapitools.model.CreateLogRequest;
 import org.openapitools.model.LogModel;
 
 public class LogFixture {
 
   public static final Long id = (long) 1;
   public static final OffsetDateTime dateTime =
-      OffsetDateTime.of(LocalDateTime.of(2017, 05, 12, 05, 45), ZoneOffset.ofHoursMinutes(6, 30));
+      OffsetDateTime.of(LocalDateTime.of(2017, 05, 12, 05, 45), ZoneOffset.ofHoursMinutes(0, 0));
   public static final String message = "data inserted";
   public static final String level = "info";
 
   public static LogModel getLogModel() {
-    return new LogModel().id(id).dateTime(dateTime).message(message).level(level);
+    return new LogModel().id(id).dateTime(dateTime).message(message).level(level).serialNumber(DeviceFixture.SERIAL_NUMBER);
   }
 
   public static LogEntity getLogEntity() {
@@ -39,6 +40,14 @@ public class LogFixture {
         .level(level)
         .serialNumber(DeviceFixture.SERIAL_NUMBER)
         .build();
+  }
+
+  public static CreateLogRequest getCreateLogRequest(){
+    return new CreateLogRequest()
+        .serialNumber(DeviceFixture.SERIAL_NUMBER)
+        .level(level)
+        .message(message)
+        .dateTime("2017-05-12T05:45:00+00:00");
   }
 
   public static List<LogModel> getListOfLogs() {
