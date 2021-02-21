4b68e87b709b2ef146d249e86546ad483f5887c8
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-21 06:47:49 -0500

    Test CreateLog controller

--- src/test/java/org/beanpod/switchboard/controller/LogControllerTest.java
+++ src/test/java/org/beanpod/switchboard/controller/LogControllerTest.java
@@ -2,16 +2,21 @@ package org.beanpod.switchboard.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import org.beanpod.switchboard.dao.LogDaoImpl;
+import org.beanpod.switchboard.dto.LogDto;
+import org.beanpod.switchboard.dto.mapper.LogMapper;
+import org.beanpod.switchboard.entity.LogEntity;
 import org.beanpod.switchboard.fixture.LogFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.openapitools.model.CreateLogRequest;
 import org.openapitools.model.LogModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,12 +24,20 @@ import org.springframework.http.ResponseEntity;
 class LogControllerTest {
 
   private static List<LogModel> logModels;
+  private static LogModel logModel;
+  private static LogDto logDto;
+  private static LogEntity logEntity;
   @InjectMocks private LogController logController;
   @Mock private LogDaoImpl logDao;
+  @Mock private LogMapper logMapper;
 
   @BeforeEach
   void setupLogFixture() {
     logModels = LogFixture.getListOfLogs();
+    logModel = LogFixture.getLogModel();
+    logDto = LogFixture.getLogDto();
+    logEntity = LogFixture.getLogEntity();
+
   }
 
   @BeforeEach
@@ -46,5 +59,19 @@ class LogControllerTest {
     ResponseEntity<List<LogModel>> response = logController.retrieveDeviceLogs("1");
     assertEquals(HttpStatus.OK, response.getStatusCode());
     assertIterableEquals(logModels, response.getBody());
+  }
+
+  @Test
+  final void testCreateLog(){
+    when(logDao.createLog(logDto)).thenReturn(logDto);
+    when(logMapper.toLogDto(any())).thenReturn(logDto);
+    when(logMapper.toLogModel(any())).thenReturn(logModel);
+    when(logMapper.toLogEntity(any())).thenReturn(logEntity);
+
+    ResponseEntity<LogModel> responseEntity = logController.createLog(LogFixture.getCreateLogRequest());
+    System.out.println(responseEntity.getBody());
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertEquals(logModel,responseEntity.getBody());
   }
 }
