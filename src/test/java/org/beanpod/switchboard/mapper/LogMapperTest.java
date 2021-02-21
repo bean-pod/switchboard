59b2a94da142671334429005ae36d0c7822226fb
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-21 07:59:48 -0500

    Reformat code

--- src/test/java/org/beanpod/switchboard/mapper/LogMapperTest.java
+++ src/test/java/org/beanpod/switchboard/mapper/LogMapperTest.java
@@ -13,32 +13,34 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LogMapperTest {
+
   private static LogMapper logMapper;
   private static OffsetDateTime dateTime;
 
   @BeforeEach
-  void setup(){
+  void setup() {
     logMapper = spy(LogMapper.class);
     dateTime = LogFixture.dateTime;
   }
 
   @Test
-  void mapTestValidDateString(){
-    String validDateTime  = "2017-05-12T05:45+00:00";
+  void mapTestValidDateString() {
+    String validDateTime = "2017-05-12T05:45+00:00";
     OffsetDateTime offsetDateTime = logMapper.map(validDateTime);
 
     assertEquals(dateTime, offsetDateTime);
   }
 
   @Test
-  void mapTestInvalidDateString(){
-    String validDateTime  = "2017-05-12 05:45+00:00";
+  void mapTestInvalidDateString() {
+    String validDateTime = "2017-05-12 05:45+00:00";
 
     try {
       OffsetDateTime offsetDateTime = logMapper.map(validDateTime);
       Assertions.fail("Expected exception to be thrown");
-    }catch(DateTimeParseException e){
-      assertThat(e).isInstanceOf(DateTimeParseException.class).hasMessage("Text '2017-05-12 05:45+00:00' could not be parsed at index 10");
+    } catch (DateTimeParseException e) {
+      assertThat(e).isInstanceOf(DateTimeParseException.class)
+          .hasMessage("Text '2017-05-12 05:45+00:00' could not be parsed at index 10");
     }
   }
 }
