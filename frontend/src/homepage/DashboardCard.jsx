5047ef73cd3eb482ba5cc986d6e08539cc1f3e9b
Jun <rebecca.j.loke@gmail.com>
2021-02-22 17:41:53 -0500

    Add title to dashboard card

--- frontend/src/homepage/DashboardCard.jsx
+++ frontend/src/homepage/DashboardCard.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import "./homepage.css";
-import {
-  Paper,
-} from "@material-ui/core";
+import { Typography, Paper } from "@material-ui/core";
 
-export default function DashCard(props){
-    return (
-        <Paper className="dashboardCard">{props.children}</Paper>
-    );
+export default function DashCard(props) {
+  const { title, children } = props;
+  return (
+    <>
+      <Paper className="dashboardCard">
+        <Typography variant="h5" gutterBottom>
+          {title}
+        </Typography>
+        {children}
+      </Paper>
+    </>
+  );
 }
+
+DashCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+};
--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./homepage.css";
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Paper,
-  Typography
-} from "@material-ui/core";
+import { Box, Button, Container, Grid } from "@material-ui/core";
 import DashboardCard from "./DashboardCard";
 import DynamicBreadcrumb from "../general/DynamicBreadcrumb";
+import ActiveStreamsCard from "./ActiveStreamsCard";
 
 export default function HomePage() {
   return (
@@ -20,37 +13,19 @@ export default function HomePage() {
       <Box className="flexContents headerAreaUnderline">
         <div className="title">Dashboard</div>
       </Box>
-      <Grid container justify="center" spacing={3}>
+      <Grid container justify="center" alignItems="stretch" spacing={3}>
         <Grid item xs={6}>
           <Grid style={{ height: "100%" }}>
-            <DashboardCard style={{ height: "100%"}}>
-              <Typography variant="h4" gutterBottom>
-                Active Streams
-              </Typography>
-              <Grid container justify="center" spacing={3}>
-                <Grid item xs={12}>
-                  TABLE GOES HERE
-                </Grid>
-                <Grid container alignItems = "flex-end" alignContent="center" >
-                <Grid item xs={6}>
-                  <Button variant="contained" color="primary" >See more</Button>
-                </Grid>
-                <Grid item xs={6}>
-                  <Button variant="contained" color="primary">Start Stream</Button>
-                </Grid>                  
-                </Grid>
-              </Grid>
-            </DashboardCard>
           </Grid>
         </Grid>
         <Grid item xs={6}>
-          <DashboardCard>My Devices</DashboardCard>
+          <DashboardCard title="My Devices" />
         </Grid>
         <Grid item xs={6}>
-        <DashboardCard>Activity Log</DashboardCard>
+          <DashboardCard title="Activiy Logs" />
         </Grid>
         <Grid item xs={6}>
-        <DashboardCard>Admin Panel</DashboardCard>
+          <DashboardCard title="Admin Panel" />
         </Grid>
       </Grid>
     </Container>
