2ce142ef32868f2ee1902c8f9ec1412593564e0c
Jun <rebecca.j.loke@gmail.com>
2021-02-22 16:37:05 -0500

    Add basic structure of page

--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import {
+  Box,
+  Button,
   Container,
+  Grid,
+  Paper,
+  Typography
 } from "@material-ui/core";
 import DynamicBreadcrumb from "../general/DynamicBreadcrumb";
 
@@ -8,6 +14,38 @@ export default function HomePage() {
   return (
     <Container>
       <DynamicBreadcrumb breadcrumbs={[["Home", ""]]} />
+
+      <Box className="flexContents headerAreaUnderline">
+        <div className="title">Dashboard</div>
+      </Box>
+      <Grid container justify="center" spacing={3}>
+        <Grid item xs={6}>
+          <Grid style={{ height: "100%" }}>
+              <Typography variant="h4" gutterBottom>
+                Active Streams
+              </Typography>
+              <Grid container justify="center" spacing={3}>
+                <Grid item xs={12}>
+                  TABLE GOES HERE
+                </Grid>
+                <Grid container alignItems = "flex-end" alignContent="center" >
+                <Grid item xs={6}>
+                  <Button variant="contained" color="primary" >See more</Button>
+                </Grid>
+                <Grid item xs={6}>
+                  <Button variant="contained" color="primary">Start Stream</Button>
+                </Grid>                  
+                </Grid>
+              </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
