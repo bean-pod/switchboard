6a659faa82438e1751028e7b8e168d29001958c6
Jun <rebecca.j.loke@gmail.com>
2021-02-22 19:49:29 -0500

    Create StreamListPage.test.jsx

---
+++ frontend/src/streamlist/__tests__/StreamListPage.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { beforeEach, describe, expect, it } from "@jest/globals";
+import { Box, Container } from "@material-ui/core";
+import StreamListPage from "../StreamList";
+import StreamTablePopulator from "../StreamTablePopulator";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<StreamListPage/> Component", () => {
+  let wrapper;
+  const dummySource = () => {};
+  beforeEach(() => {
+    wrapper = Enzyme.shallow(<StreamListPage dataSource={dummySource} />);
+  });
+  describe("Should contain the following components", () => {
+    it("Contains 1 <Container/> component", () => {
+      expect(wrapper.find(Container)).toHaveLength(1);
+    });
+    it("Contains 1 <Box/> component", () => {
+      expect(wrapper.find(Box)).toHaveLength(1);
+    });
+    it("Contains 1 <StreamTablePopulator/> component", () => {
+      expect(wrapper.find(StreamTablePopulator)).toHaveLength(1);
+    });
+  });
+});
