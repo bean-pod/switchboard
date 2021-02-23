14946eac1ae13ddbd4edeb058ae7d3226c69fcb2
Jun <rebecca.j.loke@gmail.com>
2021-02-22 19:18:47 -0500

    Create StreamTablePopulator.jsx
    intermediate step that handles the event changes without being in the table

---
+++ frontend/src/streamlist/StreamTablePopulator.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StreamsTable from "./StreamsTable";
+
+export default class StreamTablePopulator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      streams: []
+    };
+    this.dataSource = props.dataSource;
+    this.handleStreamsChange = this.handleStreamsChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.dataSource.getAllStreams(this.handleStreamsChange);
+  }
+
+  handleStreamsChange(streams) {
+    this.setState({
+      streams
+    });
+  }
+
+  render() {
+    const { streams } = this.state;
+    return <StreamsTable streams={streams} />;
+  }
+}
+
+StreamTablePopulator.propTypes = {
+  dataSource: PropTypes.objectOf(PropTypes.func).isRequired
+};
