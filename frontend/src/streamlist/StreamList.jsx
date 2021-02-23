c13d8bf7a7222175c33f69492b522f47fe9e2652
Jun <rebecca.j.loke@gmail.com>
2021-02-22 19:34:31 -0500

    Refactor StreamList to use StreamTablePopulator

--- frontend/src/streamlist/StreamList.jsx
+++ frontend/src/streamlist/StreamList.jsx
@@ -1,41 +1,20 @@
 import React from "react";
 import { Box, Container } from "@material-ui/core";
 import PropTypes from "prop-types";
-import StreamsTable from "./StreamsTable";
+import StreamTablePopulator from "./StreamTablePopulator";
 
-export default class StreamList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      streams: []
-    };
-    this.dataSource = props.dataSource;
-    this.handleStreamsChange = this.handleStreamsChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.dataSource.getAllStreams(this.handleStreamsChange);
-  }
-
-  handleStreamsChange(streams) {
-    this.setState({
-      streams
-    });
-  }
-
-  render() {
-    const { streams } = this.state;
-    return (
-      <>
-        <Container>
-          <Box className="headerAreaUnderline">
-            <div className="title">Current Streams</div>
-          </Box>
-          <StreamsTable streams={streams} />
-        </Container>
-      </>
-    );
-  }
+export default function StreamList(props) {
+  const { dataSource } = props;
+  return (
+    <>
+      <Container>
+        <Box className="headerAreaUnderline">
+          <div className="title">Current Streams</div>
+        </Box>
+        <StreamTablePopulator dataSource={dataSource} />
+      </Container>
+    </>
+  );
 }
 StreamList.propTypes = {
   dataSource: PropTypes.objectOf(PropTypes.func).isRequired
