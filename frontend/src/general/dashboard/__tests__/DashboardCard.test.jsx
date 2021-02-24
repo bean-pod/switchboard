8f8f22b33ab4b643891c24f3e0c32ad356166c5a
Jun <rebecca.j.loke@gmail.com>
2021-02-24 14:15:12 -0500

    Cleanup CashCar & gridColumn tests

--- frontend/src/general/dashboard/__tests__/DashboardCard.test.jsx
+++ frontend/src/general/dashboard/__tests__/DashboardCard.test.jsx
@@ -1,39 +1,35 @@
 import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { beforeEach, describe, expect, it } from "@jest/globals";
-import DashboardCard from "../DashboardCard"
+import { describe, expect, it } from "@jest/globals";
 import { Typography, Paper } from "@material-ui/core";
-
+import DashboardCard from "../DashboardCard";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe.only("<DashboardCard/> Component", () => {
-    let wrapper;
-    const dummyTitle = "Title";
-    const dummyBody = "Body";
+  let wrapper;
+  const dummyTitle = "Title";
+  const dummyBody = "Body";
 
-    describe(`render() with props title:${dummyTitle} children:${dummyBody}`, () => {
+  describe(`render() with props title:${dummyTitle} children:${dummyBody}`, () => {
+    wrapper = Enzyme.shallow(
+      <DashboardCard title={dummyTitle}>{dummyBody}</DashboardCard>
+    );
 
-        wrapper = Enzyme.shallow(
-            <DashboardCard title={dummyTitle}>
-                {dummyBody}
-            </DashboardCard>
-        );
-
-        describe("Contains 1 <Paper/> component", () => {
-            expect(wrapper.find(Paper)).toHaveLength(1);
-            it(`That has text "${dummyBody}"`, () => {
-                const bodyBox = wrapper.find(Paper)
-                expect(bodyBox.text())
-            })
-        });
-        describe("Contains 1 <Typography/> component", () => {
-            expect(wrapper.find(Typography)).toHaveLength(1);
-            it(`That has title "${dummyTitle}"`, () => {
-                const bodyBox = wrapper.find(Paper)
-                expect(bodyBox.text())
-            })
-        });
+    describe("Contains 1 <Paper/> component", () => {
+      expect(wrapper.find(Paper)).toHaveLength(1);
+      it(`That has text "${dummyBody}"`, () => {
+        const bodyBox = wrapper.find(Paper);
+        expect(bodyBox.text());
+      });
     });
+    describe("Contains 1 <Typography/> component", () => {
+      expect(wrapper.find(Typography)).toHaveLength(1);
+      it(`That has title "${dummyTitle}"`, () => {
+        const bodyBox = wrapper.find(Paper);
+        expect(bodyBox.text());
+      });
+    });
+  });
 });
--- frontend/src/general/dashboard/__tests__/GridColumn.test.jsx
+++ frontend/src/general/dashboard/__tests__/GridColumn.test.jsx
@@ -2,54 +2,51 @@ import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { describe, expect, it } from "@jest/globals";
-import GridColumn from "../GridColumn";
 import { Grid } from "@material-ui/core";
-
+import GridColumn from "../GridColumn";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe.only("<GridColumn/> Component", () => {
-    let wrapper;
-    const dummyWidth = 4;
-    const dummySpacing = 5;
-    const dummyBody = "Body";
+  let wrapper;
+  const dummyWidth = 4;
+  const dummySpacing = 5;
+  const dummyBody = "Body";
 
-    describe(`render() with props width:${dummyWidth} children:${dummyBody}`, () => {
+  describe(`render() with props width:${dummyWidth} children:${dummyBody}`, () => {
+    wrapper = Enzyme.shallow(
+      <GridColumn width={dummyWidth} spacing={dummySpacing}>
+        {dummyBody}
+      </GridColumn>
+    );
 
-        wrapper = Enzyme.shallow(
-            <GridColumn width={dummyWidth} spacing={dummySpacing}>
-                {dummyBody}
-            </GridColumn>
-        );
-
-        describe("Contains 2 <Grid/> components", () => {
-            expect(wrapper.find(Grid)).toHaveLength(2);
-            describe("First <Grid/> has props", () => {
-                const outerGrid = wrapper.find(Grid).first();
-                it(`"item" with value true`, () => {
-                    expect(outerGrid.props().item).toBe(true);
-                })
-                it(`"xs" with value ${dummyWidth}`, () => {
-                    expect(outerGrid.props().xs).toBe(dummyWidth);
-                })
-            })
-            describe("First <Grid/> has props", () => {
-                const innerGrid = wrapper.find(Grid).last();
-                it(`"container" with value true`, () => {
-                    expect(innerGrid.props().container).toBe(true);
-                })
-                it(`"spacing" with value ${dummySpacing}`, () => {
-                    expect(innerGrid.props().spacing).toBe(dummySpacing);
-                })
-            })
-
+    describe("Contains 2 <Grid/> components", () => {
+      expect(wrapper.find(Grid)).toHaveLength(2);
+      describe("First <Grid/> has props", () => {
+        const outerGrid = wrapper.find(Grid).first();
+        it(`"item" with value true`, () => {
+          expect(outerGrid.props().item).toBe(true);
         });
-        // describe("Contains 1 <Typography/> component", () => {
-        //     expect(wrapper.find(Typography)).toHaveLength(1);
-        //     it(`That has title "${dummyTitle}"`, () => {
-        //         const bodyBox = wrapper.find(Paper)
-        //         expect(bodyBox.text())
-        //     })
-        // });
+        it(`"xs" with value ${dummyWidth}`, () => {
+          expect(outerGrid.props().xs).toBe(dummyWidth);
+        });
+      });
+      describe("First <Grid/> has props", () => {
+        const innerGrid = wrapper.find(Grid).last();
+        it(`"container" with value true`, () => {
+          expect(innerGrid.props().container).toBe(true);
+        });
+        it(`"spacing" with value ${dummySpacing}`, () => {
+          expect(innerGrid.props().spacing).toBe(dummySpacing);
+        });
+      });
     });
+    // describe("Contains 1 <Typography/> component", () => {
+    //     expect(wrapper.find(Typography)).toHaveLength(1);
+    //     it(`That has title "${dummyTitle}"`, () => {
+    //         const bodyBox = wrapper.find(Paper)
+    //         expect(bodyBox.text())
+    //     })
+    // });
+  });
 });
