2e61e04db1d2672be9806244592d5bca7f238351
Jun <rebecca.j.loke@gmail.com>
2021-02-24 15:34:40 -0500

    Create logsTableWrapper

--- frontend/src/loglist/LogListPage.jsx
+++ frontend/src/loglist/LogListPage.jsx
@@ -1,47 +1,26 @@
 import React from "react";
 import { Box, Container } from "@material-ui/core";
 import DynamicBreadcrumb from "../general/DynamicBreadcrumb";
-import LogsTable from "./LogsTable";
-import * as LogApi from "../api/LogApi";
+import LogsTableWrapper from "./LogsTableWrapper";
 
-export default class LogListPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logs: []
-    };
-    this.handleLogsChange = this.handleLogsChange.bind(this);
-  }
-
-  componentDidMount() {
-    LogApi.getAllLogs().then(this.handleLogsChange);
-  }
-
-  handleLogsChange(logs) {
-    this.setState({
-      logs
-    });
-  }
-
-  render() {
-    const { logs } = this.state;
-    return (
-      <>
-        <Container>
-          <DynamicBreadcrumb
-            breadcrumbs={[
-              ["Home", "/"],
-              ["Logs", "/Logs"]
-            ]}
-          />
-          <Box className="areaUnderBreadcrumbs">
-            <Box className="headerAreaUnderline">
-              <div className="title">Logs</div>
-            </Box>
-            <LogsTable logs={logs} />
+export default function LogListPage(props) {
+  const { logsDataSource } = props;
+  return (
+    <>
+      <Container>
+        <DynamicBreadcrumb
+          breadcrumbs={[
+            ["Home", "/"],
+            ["Logs", "/Logs"]
+          ]}
+        />
+        <Box className="areaUnderBreadcrumbs">
+          <Box className="headerAreaUnderline">
+            <div className="title">Logs</div>
           </Box>
-        </Container>
-      </>
-    );
-  }
+          <LogsTableWrapper logsDataSource={logsDataSource} />
+        </Box>
+      </Container>
+    </>
+  );
 }
