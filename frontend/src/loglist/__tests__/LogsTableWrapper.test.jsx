c942ab41f8258821b3e20f1be9f5bccb58b6eaaa
Jun <rebecca.j.loke@gmail.com>
2021-02-24 17:00:02 -0500

    lint

--- frontend/src/loglist/__tests__/LogsTableWrapper.test.jsx
+++ frontend/src/loglist/__tests__/LogsTableWrapper.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { beforeEach, describe, expect, it } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import LogsTableWrapper from "../LogsTableWrapper";
 import LogsTable from "../LogsTable";
 import LogInfo from "../../model/LogInfo";
@@ -9,50 +9,45 @@ import LogInfo from "../../model/LogInfo";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<LogsTableWrapper/> Class Component", () => {
-    let wrapper;
-    const dummyValue = [new LogInfo(1, null, "Info", "Log 1 info")]
-    const dummySource = {
-        getAllLogs() {
-            return new Promise((resolve) => resolve(dummyValue));
-        }
-    };
-    describe("render() function", () => {
-        wrapper = Enzyme.shallow(
-            <LogsTableWrapper logsDataSource={dummySource} />
-        );
-        describe("returns a component that", () => {
-            it("Contains 1 <LogsTable/> component", () => {
-                expect(wrapper.find(LogsTable)).toHaveLength(1);
-            });
-        });
+  let wrapper;
+  const dummyValue = [new LogInfo(1, null, "Info", "Log 1 info")];
+  const dummySource = {
+    getAllLogs() {
+      return new Promise((resolve) => resolve(dummyValue));
+    }
+  };
+  describe("render() function", () => {
+    wrapper = Enzyme.shallow(<LogsTableWrapper logsDataSource={dummySource} />);
+    describe("returns a component that", () => {
+      it("Contains 1 <LogsTable/> component", () => {
+        expect(wrapper.find(LogsTable)).toHaveLength(1);
+      });
     });
+  });
 
-    describe("handleLogsChange()", () => {
-        wrapper = Enzyme.shallow(
-            <LogsTableWrapper logsDataSource={dummySource} />
-        );
+  describe("handleLogsChange()", () => {
+    wrapper = Enzyme.shallow(<LogsTableWrapper logsDataSource={dummySource} />);
 
-        //     const dummySource = {
-        //       getAllLogs() {
-        //         return new Promise((resolve) => resolve([]));
-        //       }
-        //     };
+    //     const dummySource = {
+    //       getAllLogs() {
+    //         return new Promise((resolve) => resolve([]));
+    //       }
+    //     };
 
-        //     wrapper = Enzyme.shallow(<LogsTableWrapper logsDataSource={dummySource} />);
+    //     wrapper = Enzyme.shallow(<LogsTableWrapper logsDataSource={dummySource} />);
 
-        it("should set the state", () => {
-                  const startingState = {
-                    logs: dummyValue
-                  };
-                  const expectedValue = [new LogInfo(1, null, "Info", "Log 1 info")];
-                  const expectedState = {
-                    logs: expectedValue
-                  };
+    it("should set the state", () => {
+      const startingState = {
+        logs: dummyValue
+      };
+      const expectedValue = [new LogInfo(1, null, "Info", "Log 1 info")];
+      const expectedState = {
+        logs: expectedValue
+      };
 
-                  expect(wrapper.state()).toStrictEqual(startingState);
-                  wrapper.instance().handleLogsChange(expectedValue);
-                  expect(wrapper.state()).toStrictEqual(expectedState);
-               
-        });
+      expect(wrapper.state()).toStrictEqual(startingState);
+      wrapper.instance().handleLogsChange(expectedValue);
+      expect(wrapper.state()).toStrictEqual(expectedState);
     });
+  });
 });
