c2c495b420a8a8777041e90582f6ba1c3d6ae321
Jun <rebecca.j.loke@gmail.com>
2021-02-24 12:58:30 -0500

    Move Add Stream table!! And add href to buttons

--- frontend/src/homepage/ActiveStreamsCard.jsx
+++ frontend/src/homepage/ActiveStreamsCard.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import DashboardCard from "./DashboardCard";
-import DashBoardButton from "./DashboardButton";
+
+import DashboardCard from "../general/dashboard/DashboardCard";
+import DashBoardButton from "../general/dashboard/DashboardButton";
+import StreamsTableWrapper from "../streamlist/StreamsTableWrapper";
+
+import * as StreamApi from "../api/StreamApi";
 
 export default function ActiveStreamsCard() {
+  const dataSource = StreamApi;
+
   return (
     <DashboardCard title="Active Streams" style={{ height: "100%" }}>
       <Grid container justify="center" direction="row" spacing={3}>
         <Grid item xs={12}>
-          TABLE GOES HERE
+          <StreamsTableWrapper dataSource={dataSource} />
         </Grid>
         <Grid item xs={4}>
-          <DashBoardButton>See more</DashBoardButton>
+          <DashBoardButton href="/Streaming">See more</DashBoardButton>
         </Grid>
         <Grid item xs={4}>
-          <DashBoardButton>Start Stream</DashBoardButton>
+          {
+            // TODO make it create stream
+          }
+          <DashBoardButton href="/Streaming">Start Stream</DashBoardButton>
         </Grid>
       </Grid>
     </DashboardCard>
