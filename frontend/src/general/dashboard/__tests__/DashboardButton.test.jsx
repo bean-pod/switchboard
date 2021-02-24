62af08252ed29bd704d8dae69fd68009005e1181
Jun <rebecca.j.loke@gmail.com>
2021-02-24 15:13:54 -0500

    Linter

--- frontend/src/general/dashboard/__tests__/DashboardButton.test.jsx
+++ frontend/src/general/dashboard/__tests__/DashboardButton.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { describe, expect, it } from "@jest/globals";
-import { Link, Button } from "@material-ui/core";
+import { Link } from "@material-ui/core";
 
 import DashboardButton from "../DashboardButton";
 
