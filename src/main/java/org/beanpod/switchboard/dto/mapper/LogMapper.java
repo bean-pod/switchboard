6fafa0e29fbca6d686dcf8f58fcd5614e7eb4d09
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-02-25 11:28:31 -0500

    [#220] Implement an endpoint to create logs (#319)
    * Update the contacts to have an endpoint to create logs
    
    * Update the LogMapper interface to have the needed mappings
    
    * Implement createLog method which create a log
    
    * Implement the logs endpoint controller
    
    * Add LogDto object fixture
    
    * Update Log fixtures to have a CreateLogRequest object and proper serial number for LogModel. Also, made the zone offset equal to +00:00 which corresponds to UTC timezone
    
    * Test CreateLog controller
    
    * Test CreateLog method in LogDaoImpl class
    
    * Reformat the code using Google-Java-Format plugin
    
    * Remove necessary print statement
    
    * Test map method in the LogMapper interface
    
    * Reformat code
    
    * Reformat code attempt
    
    * Update method name to be more descriptive
    
    * Add createLog method that gets LogModel object and return LogDto object in the service layer
    
    * Update mappers to remove middle mappings
    
    * Update mapper name due to a typo
    
    * Update the controller logic
    
    * Update LogController and LogDaoImpl tests
    
    * Test createLog method in the service layer
    
    * Reformat code

--- src/main/java/org/beanpod/switchboard/dto/mapper/LogMapper.java
+++ src/main/java/org/beanpod/switchboard/dto/mapper/LogMapper.java
@@ -1,8 +1,11 @@
 package org.beanpod.switchboard.dto.mapper;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import org.beanpod.switchboard.dto.LogDto;
 import org.beanpod.switchboard.entity.LogEntity;
 import org.mapstruct.Mapper;
+import org.openapitools.model.CreateLogRequest;
 import org.openapitools.model.LogModel;
 
 @Mapper(componentModel = "spring")
@@ -11,4 +14,21 @@ public interface LogMapper {
   List<LogModel> toLogModels(List<LogEntity> logEntityList);
 
   LogModel toLogModel(LogEntity logEntity);
+
+  LogEntity toLogEntity(LogDto logDto);
+
+  LogModel logDtoToLogModel(LogDto logDto);
+
+  LogDto logModelToLogDto(LogModel logModel);
+
+  LogModel createLogRequestToLogModel(CreateLogRequest createLogRequest);
+
+  LogDto logEntityToLogDto(LogEntity logEntity);
+
+  /* to convert string to OffsetDateTime
+   * value must be of YYYY-MM-DDTHH:mm:ss+00:00
+   */
+  default OffsetDateTime map(String value) {
+    return OffsetDateTime.parse(value);
+  }
 }
--- src/test/java/org/beanpod/switchboard/service/LogServiceTest.java
+++ src/test/java/org/beanpod/switchboard/service/LogServiceTest.java
@@ -1,10 +1,13 @@
 package org.beanpod.switchboard.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.beanpod.switchboard.dao.LogDaoImpl;
+import org.beanpod.switchboard.dto.LogDto;
 import org.beanpod.switchboard.dto.mapper.LogMapper;
 import org.beanpod.switchboard.entity.LogEntity;
 import org.beanpod.switchboard.fixture.DeviceFixture;
@@ -15,18 +18,24 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.openapitools.model.LogModel;
 
 class LogServiceTest {
 
   public static LogEntity logEntity;
+  public static LogDto logDto;
+  public static LogModel logModel;
 
   @InjectMocks LogService logService;
   @Mock LogRepository logRepository;
   @Mock LogMapper logMapper;
+  @Mock LogDaoImpl logDao;
 
   @BeforeEach
   void setupLogFixture() {
     logEntity = LogFixture.getLogEntity();
+    logDto = LogFixture.getLogDto();
+    logModel = LogFixture.getLogModel();
   }
 
   @BeforeEach
@@ -39,5 +48,16 @@ class LogServiceTest {
     when(logRepository.save(any())).thenReturn(logEntity);
     logService.createLog("im a log", "info", DeviceFixture.SERIAL_NUMBER);
     verify(logRepository, times(1)).save(any());
+  }
+
+  @Test
+  final void testCreateLog() {
+    when(logRepository.save(any())).thenReturn(logEntity);
+    when(logMapper.logModelToLogDto(any())).thenReturn(logDto);
+    when(logService.createLog(logModel)).thenReturn(logDto);
+
+    LogDto actualLodModel = logService.createLog(logModel);
+
+    assertEquals(logDto, actualLodModel);
   }
 }
