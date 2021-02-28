1b72f6366a9d3b0083d5d107b0bea9a505755123
Jun <rebecca.j.loke@gmail.com>
2021-02-28 16:37:39 -0500

    Create top level App authenticator

---
+++ frontend/src/app/AppAuthenticator.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default class Authenticator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false,
+      admin: false
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogin() {
+    // API call.then(response =>
+    // const {isAdmin} = response;
+    const isAdmin = true;
+    this.setState({
+      authenticated: true,
+      admin: isAdmin
+    });
+  }
+
+  handleLogout() {
+    // API call.then(response =>
+    this.setState({
+      authenticated: false,
+      admin: false
+    });
+  }
+
+  render() {
+    const { authenticated, admin } = this.state;
+    return (
+      <AppRouter
+        handleLogin={this.handleLogin}
+        handleLogout={this.handleLogout}
+        authenticated={authenticated}
+        admin={admin}
+      />
+    );
+  }
+}
