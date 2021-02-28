3d66c37b52a524c817ca9fb027686594c574d76b
Jun <rebecca.j.loke@gmail.com>
2021-02-28 17:00:09 -0500

    Use logsTableWrapper now that it exists

--- frontend/src/homepage/ActivityLogsCard.jsx
+++ frontend/src/homepage/ActivityLogsCard.jsx
@@ -3,17 +3,18 @@ import { Grid } from "@material-ui/core";
 
 import DashboardCard from "../general/dashboard/DashboardCard";
 import DashBoardButton from "../general/dashboard/DashboardButton";
+import LogsTableWrapper from "../loglist/LogsTableWrapper";
 
-// import * as LogApi from "../api/LogApi";
+import * as LogApi from "../api/LogApi";
 
 export default function ActivityLogsCard() {
-  //   const dataSource = LogApi;
+  const dataSource = LogApi;
 
   return (
     <DashboardCard title="Activity Logs">
       <Grid container justify="center" direction="row" spacing={3}>
         <Grid item xs={12}>
-          LogsTableWrapper
+        <LogsTableWrapper logsDataSource={dataSource} />
         </Grid>
         <Grid item xs={4}>
           <DashBoardButton href="/Logs">View all</DashBoardButton>
