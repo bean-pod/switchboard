b0260b1b9f36489afba7852bfcee1fa8c69aed45
Jun <rebecca.j.loke@gmail.com>
2021-02-28 22:06:33 -0500

    Update ProtectedRoute.jsx

---
+++ frontend/src/app/ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Redirect, Route } from "react-router-dom";
+
+export default class ProtectedRoute extends React.Component {
+  constructor(props) {
+    super(props);
+    this.component = this.component.bind(this);
+  }
+
+  component() {
+    const { authenticated, render } = this.props;
+    if (authenticated) {
+      return render();
+    }
+    return <Redirect to={{ pathname: "/login" }} />;
+  }
+
+  render() {
+    const { path } = this.props;
+    return <Route exact path={path} render={() => this.component()} />;
+  }
+}
+
+ProtectedRoute.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+    render:PropTypes.func.isRequired
+}
