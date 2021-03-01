6c24eee3385964b49a07f726dc4aab26d20adf59
Jun <rebecca.j.loke@gmail.com>
2021-03-01 14:11:20 -0500

    Refactor test

--- frontend/src/homepage/__tests__/HomePageContents.test.jsx
+++ frontend/src/homepage/__tests__/HomePageContents.test.jsx
@@ -15,88 +15,77 @@ import DevicesCard from "../DevicesCard";
 Enzyme.configure({ adapter: new Adapter() });
 describe("<HomePage/> functional component", () => {
   const wrapper = Enzyme.shallow(<HomePage />);
-  describe("Contains 6 <Grid/> components", () => {
+  it("Contains 6 <Grid/> components", () => {
     expect(wrapper.find(Grid)).toHaveLength(6);
-    describe("First <Grid/>", () => {
-      const outerGrid = wrapper.find(Grid).first();
-      const expectedJustify = "center";
-      const expectedAlign = "stretch";
-      const expectedDirection = "row";
-      const expectedSpacing = 3;
-      it(`with expected props`, () => {
-        expect(outerGrid.props().container).toBe(true);
-        expect(outerGrid.props().justify).toBe(expectedJustify);
-        expect(outerGrid.props().alignItems).toBe(expectedAlign);
-        expect(outerGrid.props().direction).toBe(expectedDirection);
-        expect(outerGrid.props().spacing).toBe(expectedSpacing);
-      });
-    });
-    describe("Second <Grid/>", () => {
-      const secondGrid = wrapper.find(Grid).at(1);
-      const expectedXs = 6;
-      it("with expected props", () => {
-        expect(secondGrid.props().item).toBe(true);
-        expect(secondGrid.props().xs).toBe(expectedXs);
-      });
-    });
-    describe("Third <Grid/>", () => {
-      const thirdGrid = wrapper.find(Grid).at(2);
-      it("with expected props", () => {
-        expect(typeof thirdGrid.props().style).toBe("object");
-        const styleProperty = thirdGrid.props().style;
-        expect(styleProperty.height).not.toBe(undefined);
-        expect(styleProperty.height).toBe("100%");
-        expect(thirdGrid.props().children.type.name).toBe("ActiveStreamCard");
-      });
-    });
-    describe("Fourth <Grid/>", () => {
-      const fourthGrid = wrapper.find(Grid).at(3);
-      const expectedXs = 12;
-      describe("with expected props", () => {
-        expect(fourthGrid.props().item).toBe(true);
-        expect(fourthGrid.props().xs).toBe(expectedXs);
-        expect(fourthGrid.props().children.type.name).toBe("DevicesCard");
-      });
-    });
-    describe("Fifth <Grid/>", () => {
-      const fifthGrid = wrapper.find(Grid).at(4);
-      const expectedXs = 12;
-      it("with expected props", () => {
-        expect(fifthGrid.props().item).toBe(true);
-        expect(fifthGrid.props().xs).toBe(expectedXs);
-        expect(fifthGrid.props().children.type.name).toBe("ActivityLogCard");
-      });
-    });
-    describe("Sixth <Grid/>", () => {
-      const sixthGrid = wrapper.find(Grid).at(5);
-      const expectedXs = 12;
-      it("with expected props", () => {
-        expect(sixthGrid.props().item).toBe(true);
-        expect(sixthGrid.props().xs).toBe(expectedXs);
-        expect(sixthGrid.props().children.type.name).toBe("DashboardCard");
-      });
-    });
   });
-  describe("Contains 1 <GridColumn/> Component", () => {
-    expect(wrapper.find(GridColumn)).toHaveLength(1);
-    it("with expected props", () => {
-      const gridColumn = wrapper.find(GridColumn).first();
-      const expectedWidth = 6;
+  it("First <Grid/> has expected props", () => {
+    const outerGrid = wrapper.find(Grid).first();
+    const expectedJustify = "center";
+    const expectedAlign = "stretch";
+    const expectedDirection = "row";
+    const expectedSpacing = 3;
 
-      expect(gridColumn.props().width).toBe(expectedWidth);
-    });
+    expect(outerGrid.props().container).toBe(true);
+    expect(outerGrid.props().justify).toBe(expectedJustify);
+    expect(outerGrid.props().alignItems).toBe(expectedAlign);
+    expect(outerGrid.props().direction).toBe(expectedDirection);
+    expect(outerGrid.props().spacing).toBe(expectedSpacing);
   });
-  describe("Contains 1 <DynamicBreadcrumb/> Component", () => {
+  it("Second <Grid/> has expected props", () => {
+    const secondGrid = wrapper.find(Grid).at(1);
+    const expectedXs = 6;
+    expect(secondGrid.props().item).toBe(true);
+    expect(secondGrid.props().xs).toBe(expectedXs);
+  });
+  it("Third <Grid/> has expected props", () => {
+    const thirdGrid = wrapper.find(Grid).at(2);
+
+    expect(typeof thirdGrid.props().style).toBe("object");
+    const styleProperty = thirdGrid.props().style;
+    expect(styleProperty.height).not.toBe(undefined);
+    expect(styleProperty.height).toBe("100%");
+    expect(thirdGrid.props().children.type.name).toBe("ActiveStreamCard");
+  });
+  it("Fourth <Grid/> has expected props", () => {
+    const fourthGrid = wrapper.find(Grid).at(3);
+    const expectedXs = 12;
+
+    expect(fourthGrid.props().item).toBe(true);
+    expect(fourthGrid.props().xs).toBe(expectedXs);
+    expect(fourthGrid.props().children.type.name).toBe("DevicesCard");
+  });
+  it("Fifth <Grid/> has expected props", () => {
+    const fifthGrid = wrapper.find(Grid).at(4);
+    const expectedXs = 12;
+
+    expect(fifthGrid.props().item).toBe(true);
+    expect(fifthGrid.props().xs).toBe(expectedXs);
+    expect(fifthGrid.props().children.type.name).toBe("ActivityLogCard");
+  });
+  it("Sixth <Grid/> has expected props", () => {
+    const sixthGrid = wrapper.find(Grid).at(5);
+    const expectedXs = 12;
+
+    expect(sixthGrid.props().item).toBe(true);
+    expect(sixthGrid.props().xs).toBe(expectedXs);
+    expect(sixthGrid.props().children.type.name).toBe("DashboardCard");
+  });
+  it("Contains 1 <GridColumn/> Component that has expected props", () => {
+    expect(wrapper.find(GridColumn)).toHaveLength(1);
+    const gridColumn = wrapper.find(GridColumn).first();
+    const expectedWidth = 6;
+
+    expect(gridColumn.props().width).toBe(expectedWidth);
+  });
+  it("Contains 1 <DynamicBreadcrumb/> Component that has expected props", () => {
     expect(wrapper.find(DynamicBreadcrumb)).toHaveLength(1);
-    it("with expected props", () => {
-      const breadcrumbs = wrapper.find(DynamicBreadcrumb).first();
-      const expectedCrumb = [["Home", ""]];
-      expect(breadcrumbs.props().breadcrumbs).toHaveLength(1);
-      const innerCrumb = breadcrumbs.props().breadcrumbs[0];
-      expect(innerCrumb).toHaveLength(2);
-      expect(innerCrumb[0]).toBe(expectedCrumb[0][0]);
-      expect(innerCrumb[1]).toBe(expectedCrumb[0][1]);
-    });
+    const breadcrumbs = wrapper.find(DynamicBreadcrumb).first();
+    const expectedCrumb = [["Home", ""]];
+    expect(breadcrumbs.props().breadcrumbs).toHaveLength(1);
+    const innerCrumb = breadcrumbs.props().breadcrumbs[0];
+    expect(innerCrumb).toHaveLength(2);
+    expect(innerCrumb[0]).toBe(expectedCrumb[0][0]);
+    expect(innerCrumb[1]).toBe(expectedCrumb[0][1]);
   });
   it("Contains 1 <Container/>", () => {
     expect(wrapper.find(Container)).toHaveLength(1);
