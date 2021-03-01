d8df1981c052274f426be1ba43491c2c9ef3bd65
Jun <rebecca.j.loke@gmail.com>
2021-03-01 14:02:24 -0500

    Refactor test to use standard

--- frontend/src/homepage/__tests__/ActivityLogCard.test.jsx
+++ frontend/src/homepage/__tests__/ActivityLogCard.test.jsx
@@ -15,87 +15,52 @@ describe("<DevicesCard/> functional component", () => {
   const wrapper = Enzyme.shallow(<ActivityLogCard />);
 
   describe("returns a component that", () => {
-    describe("Contains 1 <DashboardCard/> component", () => {
+    it("Contains 1 <DashboardCard/> component that has expected props", () => {
+      const expectedTitle = "Activity Logs";
+      
       expect(wrapper.find(DashboardCard)).toHaveLength(1);
-
       const dashboardCard = wrapper.find(DashboardCard).first();
-      describe("that has props", () => {
-        const expectedTitle = "Activity Logs";
-        it(`"title" with value "${expectedTitle}"`, () => {
-          expect(dashboardCard.props().title).toBe(expectedTitle);
-        });
-      });
+      expect(dashboardCard.props().title).toBe(expectedTitle);
     });
-    describe("Contains 3 <Grid/> components", () => {
+    it("Contains 3 <Grid/> components", () => {
       expect(wrapper.find(Grid)).toHaveLength(3);
-      describe("First <Grid/> has props", () => {
-        const outerGrid = wrapper.find(Grid).first();
-        const expectedJustify = "center";
-        const expectedDirection = "row";
-        const expectedSpacing = 3;
+    });
+    it("First <Grid/> has expected props", () => {
+      const outerGrid = wrapper.find(Grid).first();
+      const expectedJustify = "center";
+      const expectedDirection = "row";
+      const expectedSpacing = 3;
 
-        it(`"container" with value true`, () => {
-          expect(outerGrid.props().container).toBe(true);
-        });
-        it(`"justify" with value ${expectedJustify}`, () => {
-          expect(outerGrid.props().justify).toBe(expectedJustify);
-        });
-        it(`"direction" with value ${expectedDirection}`, () => {
-          expect(outerGrid.props().direction).toBe(expectedDirection);
-        });
-        it(`"spacing" with value ${expectedSpacing}`, () => {
-          expect(outerGrid.props().spacing).toBe(expectedSpacing);
-        });
-      });
-      describe("Second <Grid/>", () => {
-        const secondGrid = wrapper.find(Grid).at(1);
-        const expectedXs = 12;
-        describe("has props", () => {
-          it(`"item" with value true`, () => {
-            expect(secondGrid.props().item).toBe(true);
-          });
-          it(`"xs" with value ${expectedXs}`, () => {
-            expect(secondGrid.props().xs).toBe(expectedXs);
-          });
-        });
-        it(`has "children" with type LogsTableWrapper`, () => {
-          expect(secondGrid.props().children.type.name).toBe(
-            "LogsTableWrapper"
-          );
-        });
-      });
-      describe("Third <Grid/>", () => {
-        const thirdGrid = wrapper.find(Grid).at(2);
-        const expectedXs = 4;
-        describe("has props", () => {
-          it(`"item" with value true`, () => {
-            expect(thirdGrid.props().item).toBe(true);
-          });
-          it(`"xs" with value ${expectedXs}`, () => {
-            expect(thirdGrid.props().xs).toBe(expectedXs);
-          });
-        });
-        it(`has "children" with type DashboardButton`, () => {
-          expect(thirdGrid.props().children.type.name).toBe("DashboardButton");
-        });
-      });
+      expect(outerGrid.props().container).toBe(true);
+      expect(outerGrid.props().justify).toBe(expectedJustify);
+      expect(outerGrid.props().direction).toBe(expectedDirection);
+      expect(outerGrid.props().spacing).toBe(expectedSpacing);
+    });
+    it("Second <Grid/> has expected props", () => {
+      const secondGrid = wrapper.find(Grid).at(1);
+      const expectedXs = 12;
+
+      expect(secondGrid.props().item).toBe(true);
+      expect(secondGrid.props().xs).toBe(expectedXs);
+      expect(secondGrid.props().children.type.name).toBe("LogsTableWrapper");
+    });
+    it("Third <Grid/> has expected props", () => {
+      const thirdGrid = wrapper.find(Grid).at(2);
+      const expectedXs = 4;
+
+      expect(thirdGrid.props().item).toBe(true);
+      expect(thirdGrid.props().xs).toBe(expectedXs);
+      expect(thirdGrid.props().children.type.name).toBe("DashboardButton");
     });
     it("Contains 1 <LogsTableWrapper/> component", () => {
       expect(wrapper.find(LogsTableWrapper)).toHaveLength(1);
     });
-    describe("Contains 1 <DashboardButton/> component", () => {
+    describe("Contains 1 <DashboardButton/> component that  has expected props", () => {
       expect(wrapper.find(DashboardButton)).toHaveLength(1);
-      describe("First <DashboardButton/>", () => {
-        const firstButton = wrapper.find(DashboardButton).first();
-        describe("has props", () => {
-          it(`"href" with value "/Logs"`, () => {
-            expect(firstButton.props().href).toBe("/Logs");
-          });
-          it(`"children" with value "View All"`, () => {
-            expect(firstButton.props().children).toBe("View All");
-          });
-        });
-      });
+
+      const firstButton = wrapper.find(DashboardButton).first();
+      expect(firstButton.props().href).toBe("/Logs");
+      expect(firstButton.props().children).toBe("View All");
     });
   });
 });
