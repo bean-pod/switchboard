e4e65590d755eacdafa5154823706a397aa1013b
Sharon Ho <sharon.cy.ho@gmail.com>
2021-03-03 18:42:46 -0500

    Align homepage title with breadcrumbs

--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -12,33 +12,35 @@ export default function HomePage() {
   return (
     <Container>
       <DynamicBreadcrumb breadcrumbs={[["Home", ""]]} />
-      <Box className="flexContents headerAreaUnderline">
-        <div className="title">Dashboard</div>
-      </Box>
-      <Grid
-        container
-        justify="center"
-        alignItems="stretch"
-        direction="row"
-        spacing={3}
-      >
-        <Grid item xs={6}>
-          <Grid style={{ height: "100%" }}>
-            <ActiveStreamCard />
+      <Box className="areaUnderBreadcrumbs">
+        <Box className="flexContents headerAreaUnderline">
+          <div className="title">Dashboard</div>
+        </Box>
+        <Grid
+          container
+          justify="center"
+          alignItems="stretch"
+          direction="row"
+          spacing={3}
+        >
+          <Grid item xs={6}>
+            <Grid style={{ height: "100%" }}>
+              <ActiveStreamCard />
+            </Grid>
           </Grid>
+          <GridColumn width={6}>
+            <Grid item xs={12}>
+              <DevicesCard />
+            </Grid>
+            <Grid item xs={12}>
+              <ActivityLogCard />
+            </Grid>
+            <Grid item xs={12}>
+              <AdminPanelCard />
+            </Grid>
+          </GridColumn>
         </Grid>
-        <GridColumn width={6}>
-          <Grid item xs={12}>
-            <DevicesCard />
-          </Grid>
-          <Grid item xs={12}>
-            <ActivityLogCard />
-          </Grid>
-          <Grid item xs={12}>
-            <AdminPanelCard />
-          </Grid>
-        </GridColumn>
-      </Grid>
+      </Box>
     </Container>
   );
 }
