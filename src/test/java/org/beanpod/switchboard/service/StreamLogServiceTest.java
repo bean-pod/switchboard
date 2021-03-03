0fa6554d8352d7c8b9f3e0e66b4a239b3892e50d
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-03-03 11:52:02 -0500

    Test StreamLogService class

---
+++ src/test/java/org/beanpod/switchboard/service/StreamLogServiceTest.java
@@ -0,0 +1,52 @@
+package org.beanpod.switchboard.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.beanpod.switchboard.dao.StreamLogDaoImpl;
+import org.beanpod.switchboard.dto.StreamLogDto;
+import org.beanpod.switchboard.dto.mapper.LogStreamMapper;
+import org.beanpod.switchboard.entity.LogEntity;
+import org.beanpod.switchboard.entity.StreamLog;
+import org.beanpod.switchboard.fixture.DeviceFixture;
+import org.beanpod.switchboard.fixture.LogFixture;
+import org.beanpod.switchboard.fixture.StreamFixture;
+import org.beanpod.switchboard.fixture.StreamLogFixture;
+import org.beanpod.switchboard.repository.LogStreamRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class StreamLogServiceTest {
+  @InjectMocks
+  StreamLogService streamLogService;
+  @Mock
+  StreamLogDaoImpl streamLogDao;
+  @Mock
+  LogStreamMapper logStreamMapper;
+
+  private StreamLogDto streamLogDto;
+  private LogEntity logEntity;
+
+  @BeforeEach
+  void setup() {
+    MockitoAnnotations.initMocks(this);
+
+    streamLogDto = StreamLogFixture.getStreamLogDto();
+    logEntity = LogFixture.getLogEntity();
+  }
+
+  @Test
+  final void createStreamLogTest(){
+    when(logStreamMapper.toLogStreamDto(any())).thenReturn(streamLogDto);
+    when(streamLogDao.createStreamLog(any())).thenReturn(streamLogDto);
+
+    StreamLogDto actualStreamLogDto = streamLogService.createStreamLog(LogFixture.id, DeviceFixture.SERIAL_NUMBER2.toString(),
+        String.valueOf(StreamFixture.ID),logEntity);
+
+    assertEquals(streamLogDto, actualStreamLogDto);
+  }
+}
