2f50f826aa63f3b116930edce4d4259ed477d342
Felix Lapierre <felix.lapierre1@gmail.com>
2021-03-03 14:50:45 -0500

    Use StreamFixture for creating test data

--- frontend/src/api/tests/StreamApi.test.js
+++ frontend/src/api/tests/StreamApi.test.js
@@ -5,108 +5,26 @@ import { afterEach, describe, expect, jest, it } from "@jest/globals";
 import * as StreamApi from "../StreamApi";
 import StreamInfo from "../../model/StreamInfo";
 import { convertToDataObject } from "../../model/ConvertDataFormat";
+import * as StreamFixture from "./StreamFixture";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock("axios");
 
-const axiosAllStreamsResponse = [1, 2];
-const axiosGetFirstStreamResponse = {
-  id: 1,
-  inputChannel: {
-    id: 1,
-    channel: {
-      id: 1,
-      name: "Sample Receiver Channel 1",
-      port: 20002
-    },
-    decoder: {
-      serialNumber: "z7VBn9aK",
-      device: {
-        serialNumber: "z7VBn9aK",
-        publicIpAddress: "0:0:0:0:0:0:0:1",
-        privateIpAddress: "127.0.0.1",
-        displayName: "sample_receiver",
-        status: "online"
-      },
-      input: null
-    }
-  },
-  outputChannel: {
-    id: 2,
-    channel: {
-      id: 4,
-      name: "Sample Sender Channel 2",
-      port: 20001
-    },
-    encoder: {
-      serialNumber: "d7TxFn7o",
-      device: {
-        serialNumber: "d7TxFn7o",
-        publicIpAddress: "0:0:0:0:0:0:0:1",
-        privateIpAddress: "127.0.0.1",
-        displayName: "sample_sender",
-        status: "online"
-      },
-      output: null
-    }
-  },
-  isRendezvous: false
-};
-
-const axiosGetSecondStreamResponse = {
-  id: 2,
-  inputChannel: {
-    id: 2,
-    channel: {
-      id: 2,
-      name: "Sample Receiver Channel 2",
-      port: 20003
-    },
-    decoder: {
-      serialNumber: "z7VBn9aK",
-      device: {
-        serialNumber: "z7VBn9aK",
-        publicIpAddress: "0:0:0:0:0:0:0:1",
-        privateIpAddress: "127.0.0.1",
-        displayName: "sample_receiver",
-        status: "online"
-      },
-      input: null
-    }
-  },
-  outputChannel: {
-    id: 1,
-    channel: {
-      id: 3,
-      name: "Sample Sender Channel 1",
-      port: 20000
-    },
-    encoder: {
-      serialNumber: "d7TxFn7o",
-      device: {
-        serialNumber: "d7TxFn7o",
-        publicIpAddress: "0:0:0:0:0:0:0:1",
-        privateIpAddress: "127.0.0.1",
-        displayName: "sample_sender",
-        status: "online"
-      },
-      output: null
-    }
-  },
-  isRendezvous: false
-};
+const firstStreamResponse = StreamFixture.firstStreamResponse;
+const secondStreamResponse = StreamFixture.secondStreamResponse;
+const allStreamsResponse = StreamFixture.allStreamsResponse;
 
 const expectedFirstStreamRespone = new StreamInfo(
   1,
-  convertToDataObject(axiosGetFirstStreamResponse.outputChannel.encoder),
-  convertToDataObject(axiosGetFirstStreamResponse.inputChannel.decoder),
+  convertToDataObject(firstStreamResponse.outputChannel.encoder),
+  convertToDataObject(firstStreamResponse.inputChannel.decoder),
   ["Additional stream info goes here."]
 );
 const expectedSecondStreamRespone = new StreamInfo(
   2,
-  convertToDataObject(axiosGetSecondStreamResponse.outputChannel.encoder),
-  convertToDataObject(axiosGetSecondStreamResponse.inputChannel.decoder),
+  convertToDataObject(secondStreamResponse.outputChannel.encoder),
+  convertToDataObject(secondStreamResponse.inputChannel.decoder),
   ["Additional stream info goes here."]
 );
 
@@ -122,7 +40,7 @@ describe("Stream Api", () => {
 
   describe("getStream", () => {
     it("should call axios.get and return a single stream's information", async () => {
-      axios.get.mockResolvedValue({ data: axiosGetFirstStreamResponse });
+      axios.get.mockResolvedValue({ data: firstStreamResponse });
       const response = await StreamApi.getStream(123);
 
       // check that callback was invoked with correct value
@@ -137,9 +55,9 @@ describe("Stream Api", () => {
   describe("getAllStreams", () => {
     it("should call axios.get and return an array of streams", async () => {
       axios.get
-        .mockResolvedValueOnce({ data: axiosAllStreamsResponse })
-        .mockResolvedValueOnce({ data: axiosGetFirstStreamResponse })
-        .mockResolvedValueOnce({ data: axiosGetSecondStreamResponse });
+        .mockResolvedValueOnce({ data: allStreamsResponse })
+        .mockResolvedValueOnce({ data: firstStreamResponse })
+        .mockResolvedValueOnce({ data: secondStreamResponse });
 
       const response = await StreamApi.getAllStreams();
       expect(response).toEqual(expectedAllStreamsResponse);
