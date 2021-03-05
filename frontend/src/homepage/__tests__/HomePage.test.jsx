2c8880c376982659de27835b573b4a9c105958fc
Jun <rebecca.j.loke@gmail.com>
2021-03-04 19:59:46 -0500

    lint

--- frontend/src/homepage/__tests__/HomePage.test.jsx
+++ frontend/src/homepage/__tests__/HomePage.test.jsx
@@ -8,28 +8,27 @@ import Page from "../../general/Page";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+describe("<HomePage/> functional component", () => {
+  let wrapper;
+  const expectedTitle = "Dashboard";
+  const expectedCrumb = [["Home", ""]];
+  describe("returns a component that", () => {
+    beforeEach(() => {
+      wrapper = Enzyme.shallow(<HomePage />);
+    });
+    it("contains 1 <Page/> component with the correct props", () => {
+      expect(wrapper.find(Page)).toHaveLength(1);
 
-describe("<HomePage/> functional component", ()=>{
-    let wrapper;
-    const expectedTitle = "Dashboard";
-    const expectedCrumb = [["Home", ""]];
-    describe("returns a component that", ()=>{
-        beforeEach(()=>{
-            wrapper = Enzyme.shallow(<HomePage/>);
-        })
-        it("contains 1 <Page/> component with the correct props", ()=>{
-            expect(wrapper.find(Page)).toHaveLength(1);
-            
-            const page= wrapper.find(Page).first();
-            expect(page.props().title).toBe(expectedTitle);
-            expect(page.props().breadcrumbs).toBeInstanceOf(Array);
-            expect(page.props().breadcrumbs[0]).toBeInstanceOf(Array);
-            expect(page.props().breadcrumbs[0]).toHaveLength(2);
-            expect(page.props().breadcrumbs[0][0]).toBe(expectedCrumb[0][0]);
-            expect(page.props().breadcrumbs[0][1]).toBe(expectedCrumb[0][1]);
-        })
-        it("Contains 1 <HomePageContents/> component", ()=>{
-            expect(wrapper.find(HomePageContents)).toHaveLength(1);
-        })
-    })
-})
+      const page = wrapper.find(Page).first();
+      expect(page.props().title).toBe(expectedTitle);
+      expect(page.props().breadcrumbs).toBeInstanceOf(Array);
+      expect(page.props().breadcrumbs[0]).toBeInstanceOf(Array);
+      expect(page.props().breadcrumbs[0]).toHaveLength(2);
+      expect(page.props().breadcrumbs[0][0]).toBe(expectedCrumb[0][0]);
+      expect(page.props().breadcrumbs[0][1]).toBe(expectedCrumb[0][1]);
+    });
+    it("Contains 1 <HomePageContents/> component", () => {
+      expect(wrapper.find(HomePageContents)).toHaveLength(1);
+    });
+  });
+});
