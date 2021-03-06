e24838c19d913049f8212c37cf25b175b973c3a3
Jun <rebecca.j.loke@gmail.com>
2021-03-06 17:45:29 -0500

    Make app router a funcitonal copmonent

--- frontend/src/app/AppRouter.jsx
+++ frontend/src/app/AppRouter.jsx
@@ -1,41 +1,36 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import ProtectedRoute from "./ProtectedRoute";
+import UnprotectedRoute from "./UnprotectedRoute";
+
 import LoginPage from "../login/LoginPage";
 import HomePage from "../homepage/HomePage";
 import PathNotFoundPage from "../general/PathNotFoundPage";
-import UnprotectedRoute from "./UnprotectedRoute";
+
 import * as AuthenticationApi from "../api/AuthenticationApi";
 
-export default class AppRouter extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { isAuthenticated } = AuthenticationApi;
-    return (
-      <BrowserRouter>
-        <Switch>
-          <UnprotectedRoute
-            path="/Login"
-            isAuthenticated={isAuthenticated}
-            render={() => <LoginPage />}
-          />
-          <ProtectedRoute
-            path="/Home"
-            isAuthenticated={isAuthenticated}
-            render={() => {
-              return <HomePage />;
-            }}
-          />
-          <Route path="/">
-            <PathNotFoundPage />;
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+export default function AppRouter() {
+  const { isAuthenticated } = AuthenticationApi;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <UnprotectedRoute
+          path="/Login"
+          isAuthenticated={isAuthenticated}
+          render={() => <LoginPage />}
+        />
+        <ProtectedRoute
+          path="/Home"
+          isAuthenticated={isAuthenticated}
+          render={() => {
+            return <HomePage />;
+          }}
+        />
+        <Route path="/">
+          <PathNotFoundPage />;
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
