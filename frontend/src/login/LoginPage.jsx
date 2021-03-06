306f61aa0815161191441d61267dfe590c9f9314
Jun Loke <rebecca.j.loke@gmail.com>
2021-03-06 15:57:04 -0500

    [#347] Refactor Login to use Page (#362)
    * Make loginPage a class component and extract other components
    
    * rename login page to loginpage contents
    
    * Fix typo
    
    * lint
    
    * Add loginPage & LoginPage tests
    
    * Fix tests

--- frontend/src/login/LoginPage.jsx
+++ frontend/src/login/LoginPage.jsx
@@ -1,31 +1,11 @@
-import React, { useState } from "react";
-import { Box, Container } from "@material-ui/core";
-import * as AuthenticationApi from "../api/AuthenticationApi";
-import LoginFailedDialog from "./LoginFailedDialog";
-import LoginConsole from "./LoginConsole";
+import React from "react";
+import Page from "../general/Page";
+import LoginPageContents from "./LoginPageContents";
 
 export default function LoginPage() {
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [dialogMessage, setDialogMessage] = useState("");
-
-  const handleSubmit = (username, password) => {
-    AuthenticationApi.logIn({ username, password }).catch((error) => {
-      setDialogMessage(error.message);
-      setDialogOpen(true);
-    });
-  };
-
   return (
-    <Container>
-      <Box className="flexContents headerAreaUnderline">
-        <div className="title">Login</div>
-      </Box>
-      <LoginConsole handleSubmit={handleSubmit} />
-      <LoginFailedDialog
-        open={dialogOpen}
-        setOpen={setDialogOpen}
-        message={dialogMessage}
-      />
-    </Container>
+    <Page title="Login" breadcrumbs={[]} login>
+      <LoginPageContents />
+    </Page>
   );
 }
