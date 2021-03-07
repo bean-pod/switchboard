8067d504d0593cc6c9010388d132a8648ad18e63
Jun <rebecca.j.loke@gmail.com>
2021-03-07 00:27:07 -0500

    Cleanup unused props

--- frontend/src/login/LoginPage.jsx
+++ frontend/src/login/LoginPage.jsx
@@ -5,24 +5,12 @@ import { useHistory } from "react-router-dom";
 import Page from "../general/Page";
 import LoginPageContents from "./LoginPageContents";
 
-export default function LoginPage(props) {
+export default function LoginPage() {
   const history = useHistory();
-  const { isAuthenticated, handleLogout, handleLogin } = props;
 
   return (
-    <Page
-      isAuthenticated={isAuthenticated}
-      handleLogout={handleLogout}
-      title="Login"
-      breadcrumbs={[]}
-    >
-      <LoginPageContents history={history} handleLogin={handleLogin} />
+    <Page title="Login" breadcrumbs={[]}>
+      <LoginPageContents history={history} />
     </Page>
   );
 }
-
-LoginPage.propTypes = {
-  isAuthenticated: PropTypes.func.isRequired,
-  handleLogout: PropTypes.func.isRequired,
-  handleLogin: PropTypes.func.isRequired
-};
