9746431cd94a6af3be5e914281f7e0c5d942f495
Jun Loke <rebecca.j.loke@gmail.com>
2021-03-06 21:42:03 -0500

    [#345] Refactor DevicelistPage use Page (#364)
    * Add conditional formatting for TitleButtons
    
    * Move DeviceListPage to use Page
    
    * Cleanup imports
    
    * Remove incorrect DeviceList Test
    
    * Rename Contents table to DeviceListTabTable
    
    * Add test for DeviceListPage
    
    * lint
    
    * Cleanup to follow convention
    
    * Add conditional rendering test
    
    * Make deviceList optional at the Page level
    
    * Add props test for optional prop
    
    * add DeviceList to test for DeviceListPage
    
    * rename Contents table to DeviceListTabTable to accurately reflect what it is
    
    * lint
    
    * Add test to DeviceListTabTable
    
    * lint
    
    * remove useless AddDeviceButton
    
    * remove useless ID
    
    * Create TitleButtonsTest
    
    * Make id not required
    
    * lint

--- frontend/src/general/__tests__/Page.test.jsx
+++ frontend/src/general/__tests__/Page.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { describe, expect, it } from "@jest/globals";
+import { beforeAll, describe, expect, it } from "@jest/globals";
 import { Container } from "@material-ui/core";
 import Page from "../Page";
 
@@ -15,25 +15,52 @@ describe("<Title/> functional Component", () => {
   const dummyCrumb = [["bread", "crumb"]];
   const dummyChild = <div className="someDummyChild" />;
 
-  const wrapper = Enzyme.shallow(
-    <Page title={dummyTitle} breadcrumbs={dummyCrumb}>
-      {dummyChild}
-    </Page>
-  );
+  let wrapper;
 
-  it("returns a component with the correct composition", () => {
-    expect(wrapper.find(HeaderBar)).toHaveLength(1);
-    expect(wrapper.find(Container)).toHaveLength(1);
-
-    expect(wrapper.find(DynamicBreadcrumb)).toHaveLength(1);
-    const crumb = wrapper.find(DynamicBreadcrumb).first();
-    expect(crumb.props().breadcrumbs[0][0]).toBe(dummyCrumb[0][0]);
-    expect(crumb.props().breadcrumbs[0][1]).toBe(dummyCrumb[0][1]);
-
-    expect(wrapper.find(Title)).toHaveLength(1);
-    const title = wrapper.find(Title).first();
-    expect(title.props().title).toBe(dummyTitle);
-
-    expect(wrapper.find(".someDummyChild").length).toBeGreaterThan(0);
+  describe("returns a component that", () => {
+    describe("if props do not contain deviceList", () => {
+      beforeAll(() => {
+        wrapper = Enzyme.shallow(
+          <Page title={dummyTitle} breadcrumbs={dummyCrumb}>
+            {dummyChild}
+          </Page>
+        );
+      });
+      const expectedDeviceList = false;
+      it("Contains 1 <HeaderBar/> component with expected props", () => {
+        expect(wrapper.find(HeaderBar)).toHaveLength(1);
+      });
+      it("Contains 1 <Container/> component", () => {
+        expect(wrapper.find(Container)).toHaveLength(1);
+      });
+      it("Contains 1 <DynamicBreadcrumb/> component with expected props", () => {
+        expect(wrapper.find(DynamicBreadcrumb)).toHaveLength(1);
+        const crumb = wrapper.find(DynamicBreadcrumb).first();
+        expect(crumb.props().breadcrumbs[0][0]).toBe(dummyCrumb[0][0]);
+        expect(crumb.props().breadcrumbs[0][1]).toBe(dummyCrumb[0][1]);
+      });
+      it("Contains 1 <Title/> component with expected props", () => {
+        expect(wrapper.find(Title)).toHaveLength(1);
+        const title = wrapper.find(Title).first();
+        expect(title.props().title).toBe(dummyTitle);
+        expect(title.props().deviceList).toBe(expectedDeviceList);
+      });
+    });
+    describe("if props contain deviceList", () => {
+      beforeAll(() => {
+        wrapper = Enzyme.shallow(
+          <Page title={dummyTitle} breadcrumbs={dummyCrumb} deviceList>
+            {dummyChild}
+          </Page>
+        );
+      });
+      const expectedDeviceList = true;
+      it("Contains 1 <Title/> component with expected props", () => {
+        expect(wrapper.find(Title)).toHaveLength(1);
+        const title = wrapper.find(Title).first();
+        expect(title.props().title).toBe(dummyTitle);
+        expect(title.props().deviceList).toBe(expectedDeviceList);
+      });
+    });
   });
 });
