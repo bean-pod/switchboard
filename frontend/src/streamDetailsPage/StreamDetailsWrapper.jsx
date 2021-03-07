f598ad19ac28148c652c5631071acc5092a2accb
gdzooks <issa4044@gmail.com>
2021-03-06 21:40:31 -0500

    Fill in cards with info & TODOs

--- frontend/src/streamDetailsPage/StreamDetailsWrapper.jsx
+++ frontend/src/streamDetailsPage/StreamDetailsWrapper.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Container, Grid } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 
 import StreamInfo from "../model/StreamInfo";
 import DashboardCard from "../general/dashboard/DashboardCard";
+import StreamDeviceDetails from "./StreamDeviceDetails";
 // import { getSampleStream } from "../api/SampleStream";
 
 export default function StreamDetailsWrapper(props) {
   const { streamDetailSource } = props;
   return (
     <Container>
-      <Grid container spacing={3} >
+      <Grid container spacing={3}>
         <Grid item xs={6}>
-          <DashboardCard title="Sender Details" />
+          <DashboardCard title="Sender Details">
+            <StreamDeviceDetails device={streamDetailSource.sender} />
+          </DashboardCard>
         </Grid>
         <Grid item xs={6}>
-          <DashboardCard title="Receiver Details" />
+          <DashboardCard title="Receiver Details">
+            <StreamDeviceDetails device={streamDetailSource.receiver} />
+          </DashboardCard>
         </Grid>
         <Grid item xs={7}>
-          <DashboardCard title="Logs" />
+          <DashboardCard title="Logs">
+            {/* TODO: fill in once stream logs are up */}
+          </DashboardCard>
         </Grid>
         <Grid item xs={5}>
-          <DashboardCard title="Statistics" />
+          <DashboardCard title="Statistics">
+            {/* TODO: fill in once stream stats are up */}
+          </DashboardCard>
         </Grid>
       </Grid>
     </Container>
