802ec15b8c05b0d6e0b4f2a2fde3b8408ba4a140
Jun <rebecca.j.loke@gmail.com>
2021-03-07 13:52:00 -0500

    cleanup

--- frontend/src/app/AppRouter.jsx
+++ frontend/src/app/AppRouter.jsx
@@ -52,7 +52,7 @@ export default function AppRouter() {
           }}
         />
         <Route path="/">
-          <PathNotFoundPage />;
+          <PathNotFoundPage />
         </Route>
       </Switch>
     </BrowserRouter>
