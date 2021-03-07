646e29eb221c0927b7db0f162ff5bd9611110b19
gdzooks <issa4044@gmail.com>
2021-03-06 21:40:50 -0500

    prep logs for once stream logs are in backend

---
+++ frontend/src/streamDetailsPage/StreamLogs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LogsTableWrapper from "../loglist/LogsTableWrapper";
+
+export default function StreamLogs(props) {
+    const { streamLogs } = props;
+
+    return (
+        <LogsTableWrapper logsDataSource={streamLogs} />
+    );
+}
+
+StreamLogs.propTypes = {
+    streamLogs: PropTypes.objectOf(PropTypes.func).isRequired
+}
