64ba5d2bd2387e89ee80d1ed10e61e5a55bed7c0
Jun Loke <rebecca.j.loke@gmail.com>
2021-03-08 16:02:06 -0500

    [#343] Refactor DeviceDetailsPage to use Page (#366)
    * RenameDeviceDetailsPage to DeviceDetailsPageContents
    
    * Cleanup DeviceDetailsPageContents
    
    * Create DeviceDetailsPage.jsx
    
    * Make link use serial number
    
    * Make DeviceDetailsConciseTable take Device prop and renderDeviceNameDetail
    
    * remove title styling
    
    * use icon buttons to accommodate space
    
    * remove extra padding and remove label
    
    * lint
    
    * Make Contents test correspond
    
    * Make Editable & static name their own components
    
    * Add tests
    
    * lint & clenaup
    
    * lint
    
    * Align right
    
    * make DeviceNameDetail use new components
    
    * Add tests for device details page refactor (#368)
    
    * Add tests for DeviceNameDetail. Also fixed an issue in DeviceNameDetail where the name does not change immediately after a rename
    
    * Apply feedback
    
    * rename deviceNameDetail to deviceName
    
    * test cleanup
    
    * Add test for DeviceDetailsPage
    
    * Lint
    
    * lint
    
    * Add conciseRow test for name case
    
    * Improved tests
    
    Co-authored-by: Felix Lapierre <32281260+felixlapierre@users.noreply.github.com>
    Co-authored-by: Felix Lapierre <felix.lapierre1@gmail.com>

---
+++ frontend/src/deviceDetailsPage/__tests__/DeviceName.test.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
+import {
+  describe,
+  afterEach,
+  beforeEach,
+  expect,
+  jest,
+  it
+} from "@jest/globals";
+import DeviceName from "../DeviceName";
+import EditableName from "../EditableName";
+import StaticName from "../StaticName";
+import * as DeviceApi from "../../api/DeviceApi";
+
+Enzyme.configure({ adapter: new Adapter() });
+jest.mock("../../api/DeviceApi");
+jest.spyOn(DeviceApi, "updateDeviceName");
+
+describe("<DeviceName/> component", () => {
+  let wrapper;
+  const mockDevice = {
+    name: "Mock Device",
+    id: "Serial Number"
+  };
+  const defaultState = {
+    name: mockDevice.name,
+    editing: false
+  };
+  const editingState = {
+    name: mockDevice.name,
+    editing: true
+  };
+
+  beforeEach(() => {
+    wrapper = Enzyme.shallow(
+      <DeviceName deviceName={mockDevice.name} deviceId={mockDevice.id} />
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("render() function returns a component that ", () => {
+    describe("When state.editing is false", () => {
+      it("Contains one <StaticName/> component with expected props", () => {
+        expect(wrapper.state()).toEqual(defaultState);
+
+        const staticName = wrapper.find(StaticName);
+        expect(staticName).toHaveLength(1);
+
+        const props = staticName.first().props();
+        expect(props.deviceName).toEqual(mockDevice.name);
+        expect(props.startEditing).toEqual(wrapper.instance().startEdit);
+      });
+    });
+
+    describe("When state.editing is true", () => {
+      it("Contains one <EditableName/> component with expected props", () => {
+        wrapper.setState(editingState);
+
+        const editableName = wrapper.find(EditableName);
+        expect(editableName).toHaveLength(1);
+
+        const props = editableName.first().props();
+        expect(props.deviceName).toEqual(mockDevice.name);
+        expect(props.confirmEditing).toEqual(wrapper.instance().confirmEditing);
+        expect(props.setName).toEqual(wrapper.instance().setName);
+        expect(props.cancelEditing).toEqual(wrapper.instance().cancelEditing);
+      });
+    });
+  });
+
+  it("startEdit() function sets the state to editing: true", () => {
+    wrapper.instance().startEdit();
+    expect(wrapper.state()).toEqual(editingState);
+  });
+
+  describe("cancelEditing() function", () => {
+    beforeEach(() => {
+      wrapper.setState(editingState);
+    });
+
+    it("sets the state to editing: false", () => {
+      wrapper.instance().cancelEditing();
+      expect(wrapper.state()).toEqual(defaultState);
+    });
+  });
+
+  it("setName() function sets the state.name to passed value", () => {
+    const newName = "New Name";
+    wrapper.instance().setName(newName);
+    expect(wrapper.state()).toEqual({
+      name: newName,
+      editing: false
+    });
+  });
+
+  describe("confirmEditing() function", () => {
+    beforeEach(() => {
+      wrapper.setState(editingState);
+    });
+
+    describe("When updating name succeeds", () => {
+      it("Calls updateDeviceName and contains <StaticName/> with the new name", () => {
+        const newName = "New Name";
+        wrapper.instance().setName(newName);
+
+        DeviceApi.updateDeviceName.mockResolvedValue();
+
+        const editEvent = {
+          preventDefault: jest.fn()
+        };
+        wrapper.instance().confirmEditing(editEvent);
+
+        expect(DeviceApi.updateDeviceName).toHaveBeenCalledWith(
+          mockDevice.id,
+          newName
+        );
+
+        const staticName = wrapper.find(StaticName);
+        expect(staticName).toHaveLength(1);
+        const props = staticName.first().props();
+        expect(props.deviceName).toEqual(newName);
+      });
+    });
+
+    describe("When updating name fails", () => {
+      it("Calls updateDeviceName and contains <StaticName/> with the old name", async () => {
+        const newName = "New Name";
+        wrapper.instance().setName(newName);
+
+        DeviceApi.updateDeviceName.mockRejectedValue();
+
+        const editEvent = {
+          preventDefault: jest.fn()
+        };
+        wrapper.instance().confirmEditing(editEvent);
+        const flushPromises = () => new Promise(setImmediate);
+        await flushPromises();
+
+        expect(DeviceApi.updateDeviceName).toHaveBeenCalledWith(
+          mockDevice.id,
+          newName
+        );
+
+        const staticName = wrapper.find(StaticName);
+        expect(staticName).toHaveLength(1);
+        const props = staticName.first().props();
+        expect(props.deviceName).toEqual(mockDevice.name);
+      });
+    });
+  });
+});
