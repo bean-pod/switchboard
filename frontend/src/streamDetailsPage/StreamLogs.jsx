31cbf63a5326f31a8a75fb202436b64cecd0cde8
gdzooks <issa4044@gmail.com>
2021-03-08 21:22:29 -0500

    lint'd

--- frontend/src/streamDetailsPage/StreamLogs.jsx
+++ frontend/src/streamDetailsPage/StreamLogs.jsx
@@ -3,13 +3,11 @@ import PropTypes from "prop-types";
 import LogsTableWrapper from "../loglist/LogsTableWrapper";
 
 export default function StreamLogs(props) {
-    const { streamLogs } = props;
+  const { streamLogs } = props;
 
-    return (
-        <LogsTableWrapper logsDataSource={streamLogs} />
-    );
+  return <LogsTableWrapper logsDataSource={streamLogs} />;
 }
 
 StreamLogs.propTypes = {
-    streamLogs: PropTypes.objectOf(PropTypes.func).isRequired
-}
+  streamLogs: PropTypes.objectOf(PropTypes.func).isRequired
+};
