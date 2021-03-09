77310ab310389f644de57da1ab46f2cc0fb83ba2
Jun <rebecca.j.loke@gmail.com>
2021-03-09 00:02:43 -0500

    use toISOString() instead of toString

--- frontend/src/model/LogInfo.js
+++ frontend/src/model/LogInfo.js
@@ -1,11 +1,7 @@
 export default class LogInfo {
   constructor(id, dateTime, level, message) {
     this.id = id;
-    this.dateTime = new Date(dateTime)
-      .toString()
-      .split(" ")
-      .slice(0, 5)
-      .join(" ");
+    this.dateTime = new Date(dateTime).toISOString();
     this.level = level;
     this.message = message;
   }
