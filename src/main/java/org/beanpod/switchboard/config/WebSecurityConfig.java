535d8d15f8a902dbe1347e50595b8704727ac70d
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2021-03-10 20:23:22 -0500

    Rebase the auth branch with master (#377)
    * [#272] Add endpoint for authentication (#314)
    
    * Log stream id when deleting a stream
    
    * Log the encoder and decoder details when deleting a stream
    
    * add user entity and security in pom
    
    * Add confirmation token functionality and config file
    
    * add mail dependency and functionality for tokens
    
    * refactor code to remove dead unused confimration functionality
    
    * code for thymeleaf testing
    
    * Reformat code to comply with linter
    
    * delete unused entity
    
    * remove confirmation token related classes/refactor dead code
    
    * Add endpoint to get an auth token and secure endpoints
    
    * Rebase with maste
    
    * Add authorization header to the frontend
    
    * Add aurhorization file
    
    * Change FE authorization to use basic auth
    
    * Properly connect FE and BE
    
    * Cleanup unused code
    
    * Reformat code
    
    * Reformat code
    
    * Fix the authentication API tests
    
    * Fix more FE tests
    
    * Remove one security hotspot and lint frontend.
    
    * Increase test coverage and try to make sonar cloud happier
    
    * Lint FE
    
    * Add unit test for JWTAuthenticationFilter
    
    * Handle some code smells
    
    * reformat code
    
    * lint backend
    
    * lint frontend
    
    * More FE linting
    
    * readd spring.profiles.active=production
    
    * readd spring.profiles.active=production
    
    Co-authored-by: Mohamed Hefny <mohhef@rocketmail.com>
    
    * Auth create stream bug (#352)
    
    * Update the api-spec for upcoming refactor in the controllers
    
    * Revert "Update the api-spec for upcoming refactor in the controllers"
    
    This reverts commit bf12d43cd0cdb0b6c3b73e1e8d36c0ed2c33b431.
    
    * Fix the create stream endpoint not using authentication
    
    * Linting
    
    * Readd the test
    
    * *lint rolling*
    
    * Fix more linting errors
    
    Co-authored-by: Felix Lapierre <felix.lapierre1@gmail.com>
    
    * Small FE test merge fixes and delte StreamList.test.jsx
    
    * remove typo
    
    * Run linter
    
    Co-authored-by: Mohamed Hefny <mohhef@rocketmail.com>
    Co-authored-by: Felix Lapierre <felix.lapierre1@gmail.com>

--- src/main/java/org/beanpod/switchboard/config/WebSecurityConfig.java
+++ src/main/java/org/beanpod/switchboard/config/WebSecurityConfig.java
@@ -1,45 +1,78 @@
 package org.beanpod.switchboard.config;
 
+import javax.validation.ValidationException;
 import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.beanpod.switchboard.service.UserService;
+import org.openapitools.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @AllArgsConstructor
+@Log
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final UserService userService;
 
   private final BCryptPasswordEncoder bcryptPasswordEncoder;
 
-  // TODO remove this method when sign in functionality is implemented
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/*/**");
-  }
+  private final SecurityProperties securityProperties;
 
-  // TODO uncomment this block when sign in functionality is enabled, this secures the whole website
-  //  @Override
-  //  protected void configure(HttpSecurity http) throws Exception {
-  //
-  //    http.authorizeRequests()
-  //        .antMatchers("/sign-up/**", "/sign-in/**")
-  //        .permitAll()
-  //        .anyRequest()
-  //        .authenticated()
-  //        .and()
-  //        .formLogin()
-  //        .loginPage("/sign-in")
-  //        .permitAll();
-  //  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors()
+        .and()
+        .csrf()
+        .disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers("/user/sign-up")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .addFilter(new JwtAuthenticationFilter(authenticationManagerBean(), securityProperties))
+        .addFilter(new JwtAuthorizationFilter(authenticationManagerBean(), securityProperties));
+  }
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    UserModel superuser =
+        new UserModel()
+            .username(securityProperties.getSuperuserUsername())
+            .password(securityProperties.getSuperuserPassword());
+    try {
+      userService.signUpUser(superuser);
+    } catch (ValidationException validationException) {
+      log.info(validationException.getMessage());
+    }
+
     auth.userDetailsService(userService).passwordEncoder(bcryptPasswordEncoder);
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
   }
 }
