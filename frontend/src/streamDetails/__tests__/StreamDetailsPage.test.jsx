1a31527433a160410c7ff101e20df7e5194ad6f0
gdzooks <issa4044@gmail.com>
2021-03-11 21:19:36 -0500

    Tests for StreamDetailsPage

---
+++ frontend/src/streamDetails/__tests__/StreamDetailsPage.test.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { beforeEach, describe, expect, it } from "@jest/globals";
+
+import Page from "../../general/Page";
+import StreamDetailsPage from "../StreamDetailsPage";
+import StreamDetailsWrapper from "../StreamDetailsWrapper";
+import DeviceInfo from "../../model/DeviceInfo";
+import StreamInfo from "../../model/StreamInfo";
+import { getSampleStream } from "../../api/SampleData";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<StreamDetailsPage/> functional component", () => {
+  let wrapper;
+  const dummySender = new DeviceInfo(1, 1, 1, 1, 1, 1, [1, 2]);
+  const dummyReceiver = new DeviceInfo(2, 2, 2, 2, 2, 2, [3, 4]);
+  const dummyStream = new StreamInfo(1, dummySender, dummyReceiver, 2, 3);
+
+  const dummyStreamLocation = {
+    state: {
+      stream: dummyStream
+    }
+  };
+
+  describe("when passed a location prop", () => {
+    describe("returns a component that", () => {
+      beforeEach(() => {
+        wrapper = Enzyme.shallow(
+          <StreamDetailsPage location={dummyStreamLocation} />
+        );
+      });
+      it("contains one <Page/> component with expected props", () => {
+        const expectedTitle = "Stream Details";
+        const expectedBreadcrumbs = [
+          ["Home", "/"],
+          ["Active Streams", "/Streams"],
+          ["Stream Details", `/Streams/Details/${dummyStream.id}`]
+        ];
+
+        expect(wrapper.find(Page)).toHaveLength(1);
+
+        const props = wrapper.find(Page).first().props();
+        expect(props.title).toBe(expectedTitle);
+        expect(props.breadcrumbs).toStrictEqual(expectedBreadcrumbs);
+      });
+      it("contains one <StreamDetailsWrapper component with expected props", () => {
+        expect(wrapper.find(StreamDetailsWrapper)).toHaveLength(1);
+
+        const props = wrapper.find(StreamDetailsWrapper).first().props();
+        expect(props.stream).toBeInstanceOf(StreamInfo);
+        expect(props.stream).toStrictEqual(dummyStream);
+      });
+    });
+  });
+  describe("when not passed a location prop", () => {
+    const expectedDefaultStream = getSampleStream();
+    beforeEach(() => {
+      wrapper = Enzyme.shallow(<StreamDetailsPage />);
+    });
+    describe("returns a component that", () => {
+      it("contains one <Page/> component with expected props", () => {
+        const expectedTitle = "Stream Details";
+        const expectedBreadcrumbs = [
+          ["Home", "/"],
+          ["Active Streams", "/Streams"],
+          ["Stream Details", `/Streams/Details/${expectedDefaultStream.id}`]
+        ];
+
+        expect(wrapper.find(Page)).toHaveLength(1);
+
+        const props = wrapper.find(Page).first().props();
+        expect(props.title).toBe(expectedTitle);
+        expect(props.breadcrumbs).toStrictEqual(expectedBreadcrumbs);
+      });
+      it("contains one <StreamDetailsWrapper/> component with expected props", () => {
+        expect(wrapper.find(StreamDetailsWrapper)).toHaveLength(1);
+        const props = wrapper.find(StreamDetailsWrapper).first().props();
+        expect(props.stream).toBeInstanceOf(StreamInfo);
+        expect(props.stream).toStrictEqual(expectedDefaultStream);
+      });
+    });
+  });
+});
