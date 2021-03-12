b19d2f6c81f7fe5b1d4348a65976d88ed529d65e
gdzooks <issa4044@gmail.com>
2021-03-12 01:25:23 -0500

    Update test to no longer get a console error idrc

--- frontend/src/streamDetails/__tests__/StreamLogs.test.jsx
+++ frontend/src/streamDetails/__tests__/StreamLogs.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { describe } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 
 import StreamLogs from "../StreamLogs";
 import LogsTableWrapper from "../../loglist/LogsTableWrapper";
@@ -11,7 +11,11 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("<StreamLogs/> functional component", () => {
     let wrapper;
 
-    const dummyLogSource = jest.fn();
+    const dummyLogSource = {
+        getStreamLogs() {
+            return jest.fn()
+        }
+    };
 
     describe("it returns a component that", () => {
         beforeEach(() => {
