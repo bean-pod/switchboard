c86e7bff670e34f36350d4966ae39215f409966f
Sharon Ho <sharon.cy.ho@gmail.com>
2021-03-15 18:10:01 -0400

    Refactor form console and failed dialog

---
+++ frontend/src/general/userForm/FormConsole.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { Button, Container, TextField } from "@material-ui/core";
+import PropTypes from "prop-types";
+
+export default class FormConsole extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.setUsername = this.setUsername.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+  }
+
+  onSubmit(event) {
+    const { handleSubmit } = this.props;
+    const { username, password } = this.state;
+    event.preventDefault();
+    handleSubmit(username, password);
+  }
+
+  setUsername(username) {
+    this.setState({
+      username
+    });
+  }
+
+  setPassword(password) {
+    this.setState({
+      password
+    });
+  }
+
+  render() {
+    const { buttonName, isValidate, isCreate } = this.props;
+    const { password } = this.state;
+    return (
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <form
+            className={classes.form}
+            noValidate={!isValidate}
+            onSubmit={this.onSubmit}
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={(event) => this.setUsername(event.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              error={
+                isCreate
+                  ? password.length < 5 && password.length > 0
+                  : undefined
+              }
+              inputProps={
+                isCreate ? { maxLength: 20, minLength: 5 } : undefined
+              }
+              helperText={
+                isCreate
+                  ? "Password must be between 5 to 20 characters"
+                  : undefined
+              }
+              autoComplete="current-password"
+              onChange={(event) => this.setPassword(event.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {buttonName}
+            </Button>
+          </form>
+        </div>
+      </Container>
+    );
+  }
+}
+
+FormConsole.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  buttonName: PropTypes.string.isRequired,
+  isValidate: PropTypes.bool,
+  isCreate: PropTypes.bool
+};
+
+FormConsole.defaultProps = {
+  isValidate: false,
+  isCreate: false
+};
---
+++ frontend/src/general/userForm/FormFailedDialog.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Dialog from "../dialog/Dialog";
+
+export default class FormFailedDialog extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.dialogElement = React.createRef();
+    this.openDialog = this.openDialog.bind(this);
+  }
+
+  openDialog() {
+    return this.dialogElement.current.openDialog();
+  }
+
+  render() {
+    const { title, message } = this.props;
+
+    const actionButton = {
+      name: "OK",
+      onClick: this.dialogElement.current.closeDialog()
+    };
+
+    return (
+      <Dialog
+        ref={this.dialogElement}
+        title={title}
+        actionButton={actionButton}
+        noCancel
+      >
+        {message}
+      </Dialog>
+    );
+  }
+}
+
+FormFailedDialog.propTypes = {
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired
+};
