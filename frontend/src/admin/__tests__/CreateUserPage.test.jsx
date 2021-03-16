4d3429c47fad4729fa4e004a2c7822c7f5465321
Sharon Ho <sharon.cy.ho@gmail.com>
2021-03-15 21:38:38 -0400

    Test for CreateUserPage

---
+++ frontend/src/admin/__tests__/CreateUserPage.test.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { beforeEach, describe, expect, it } from "@jest/globals";
+
+import CreateUserPage from "../createUser/CreateUserPage";
+import Page from "../../general/Page";
+import CreateUserPageContents from "../createUser/CreateUserPageContents";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<CreateUserPage/> class component", () => {
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = Enzyme.shallow(<CreateUserPage />);
+  });
+
+  it("Contains one <Page/> component with correct props", () => {
+    const expectedTitle = "Create a User";
+    const expectedBreadcrumb = [
+        ["Home", "/Home"],
+        ["Admin", "/Admin"],
+        ["Create a User", "/Admin/New"]
+    ];
+
+    expect(wrapper.find(Page)).toHaveLength(1);
+
+    const page = wrapper.find(Page).first();
+    expect(page.props().title).toBe(expectedTitle);
+    expect(page.props().breadcrumbs).toStrictEqual(expectedBreadcrumb);
+  });
+
+  it("Contains one <CreateUserPageContents/> component", () => {
+    expect(wrapper.find(CreateUserPageContents)).toHaveLength(1);
+  });
+});
