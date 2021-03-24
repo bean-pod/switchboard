684cb113fd986a190fad240e9d8f6e24fe156901
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-03-24 07:30:35 -0400

    Add the createLog function that creates stream-related logs

--- src/main/java/org/beanpod/switchboard/service/StreamLogService.java
+++ src/main/java/org/beanpod/switchboard/service/StreamLogService.java
@@ -1,5 +1,6 @@
 package org.beanpod.switchboard.service;
 
+import java.time.OffsetDateTime;
 import lombok.RequiredArgsConstructor;
 import org.beanpod.switchboard.dao.StreamLogDaoImpl;
 import org.beanpod.switchboard.dto.StreamLogDto;
@@ -11,8 +12,8 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class StreamLogService {
-  LogStreamMapper logStreamMapper;
-  StreamLogDaoImpl streamLogDao;
+  private final LogStreamMapper logStreamMapper;
+  private final StreamLogDaoImpl streamLogDao;
 
   public StreamLogDto createStreamLog(
       Long id, String serialNumber, String streamID, LogEntity logEntity) {
@@ -25,5 +26,18 @@ public class StreamLogService {
             .build();
 
     return streamLogDao.createStreamLog(logStreamMapper.toLogStreamDto(streamLog));
+  }
+
+  //used in StreamAspect class to create stream-related logs
+  public StreamLogDto createLog(OffsetDateTime dateTime, String message, String decoderSerial, String encoderSerial, String streamId){
+    LogEntity logEntity = new LogEntity(OffsetDateTime.now(), message, "info", decoderSerial);
+    StreamLog streamLog = new StreamLog(encoderSerial, streamId);
+
+    //logEntity.setStreamLog(streamLog); // will cause infinite loop
+    streamLog.setLogEntity(logEntity);
+
+    StreamLogDto streamLogDto = logStreamMapper.toLogStreamDto(streamLog);
+
+    return streamLogDao.createStreamLog(streamLogDto);
   }
 }
