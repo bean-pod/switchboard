4dd806f115bde6babba1219a83cb6857820500d6
gdzooks <issa4044@gmail.com>
2021-03-25 11:44:56 -0400

    Create SimpleTable component
    *Note: will NOT work with the deviceDetails table because that is not a *simple* table

---
+++ frontend/src/general/simpleTable/SimpleTable.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { TableBody, Table, TableContainer } from "@material-ui/core";
+import SimpleTableRow from "./SimpleTableRow";
+
+export default function SimpleTable(props) {
+  const { propertyPairs } = props;
+
+  return (
+    <TableContainer>
+      <Table>
+        <TableBody>
+          {propertyPairs.map((propertyPair) => (
+            <SimpleTableRow
+              name={propertyPair[0]}
+              value={propertyPair[1]}
+              key={`component_${propertyPair[0]}`}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+SimpleTable.propTypes = {
+  propertyPairs: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string))
+    .isRequired
+};
---
+++ frontend/src/general/simpleTable/SimpleTableRow.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { TableCell, TableRow } from "@material-ui/core";
+
+export default function SimpleTableRow(props) {
+  const { name, value } = props;
+
+  return (
+    <>
+      <TableRow>
+        <TableCell>{name}</TableCell>
+        <TableCell align="center">{value}</TableCell>
+      </TableRow>
+    </>
+  );
+}
+
+SimpleTableRow.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired
+};
