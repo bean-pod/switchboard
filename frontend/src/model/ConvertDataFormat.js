7c257453a6c0cef4a83371187e54664784dc3e14
gdzooks <issa4044@gmail.com>
2021-03-25 14:40:45 -0400

    Add convert method for stream stats

--- frontend/src/model/ConvertDataFormat.js
+++ frontend/src/model/ConvertDataFormat.js
@@ -1,4 +1,8 @@
 import DeviceInfo from "./DeviceInfo";
+import StreamStatisticsInfo from "./StreamStatistics/StreamStatisticsInfo";
+import StreamStatsDeviceInfo from "./StreamStatistics/StreamStatsDeviceInfo";
+import StreamStatsLinkInfo from "./StreamStatistics/StreamStatsLinkInfo";
+import StreamStatsWindowInfo from "./StreamStatistics/StreamStatsWindowInfo";
 
 export function convertDeviceToDataObject(databaseDevice) {
   return new DeviceInfo(
@@ -10,6 +14,44 @@ export function convertDeviceToDataObject(databaseDevice) {
     databaseDevice.device.status,
     databaseDevice.inputs || databaseDevice.outputs,
     databaseDevice.inputs ? "receiver" : "sender"
+  );
+}
+
+export function convertStatsToDataObject(databaseStats) {
+  return new StreamStatisticsInfo(
+    databaseStats.id,
+    databaseStats.time,
+    new StreamStatsWindowInfo(
+      databaseStats.window.flow,
+      databaseStats.window.congestion,
+      databaseStats.window.flight
+    ),
+    new StreamStatsLinkInfo(
+      databaseStats.link.rtt,
+      databaseStats.link.bandwidth,
+      databaseStats.link.maxBandwidth
+    ),
+    new StreamStatsDeviceInfo(
+      "send",
+      databaseStates.send.packets,
+      databaseStates.send.packetsLost,
+      databaseStates.send.packetsDropped,
+      databaseStates.send.packetsRetransmitted,
+      databaseStates.send.bytes,
+      databaseStates.send.bytesDropped,
+      databaseStates.send.mbitRate
+    ),
+    new StreamStatsDeviceInfo(
+      "receive",
+      databaseStates.send.packets,
+      databaseStates.send.packetsLost,
+      databaseStates.send.packetsDropped,
+      databaseStates.send.packetsRetransmitted,
+      databaseStates.send.packetsBelated,
+      databaseStates.send.bytes,
+      databaseStates.send.bytesDropped,
+      databaseStates.send.mbitRate
+    )
   );
 }
 
