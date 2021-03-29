dd184da18e2f0588a5b8a2f1e1dee4ea69f52c99
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-03-29 15:04:19 -0400

    Implement a controller to get all logs of a specific stream

--- src/main/java/org/beanpod/switchboard/dao/StreamLogDaoImpl.java
+++ src/main/java/org/beanpod/switchboard/dao/StreamLogDaoImpl.java
@@ -1,19 +1,27 @@
 package org.beanpod.switchboard.dao;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.beanpod.switchboard.dto.StreamLogDto;
 import org.beanpod.switchboard.dto.mapper.LogStreamMapper;
 import org.beanpod.switchboard.repository.LogStreamRepository;
+import org.openapitools.model.StreamLogModel;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class StreamLogDaoImpl {
+
   private final LogStreamRepository logStreamRepository;
   private final LogStreamMapper logStreamMapper;
 
   public StreamLogDto createStreamLog(StreamLogDto streamLogDto) {
     return logStreamMapper.toLogStreamDto(
         logStreamRepository.save(logStreamMapper.toStreamLog(streamLogDto)));
+  }
+
+  public List<StreamLogModel> getStreamLogs(Long serialNumber) {
+    return logStreamMapper.toStreamLogModels(
+        logStreamRepository.findByStreamId(serialNumber.toString()));
   }
 }
