f7eb17850b4c52c800d1e1cb571748e1db9a41f8
gdzooks <issa4044@gmail.com>
2021-03-29 02:45:10 -0400

    Test for stream stats page

---
+++ frontend/src/streamDetails/DetailedStreamStatistics/__tests__/StreamStatisticsPage.test.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { beforeEach, describe, expect, it } from "@jest/globals";
+
+import Page from "../../../general/Page";
+import StreamStatisticsPage from "../StreamStatisticsPage";
+import StreamStatisticsPageContents from "../StreamStatisticsPageContents";
+import StreamStatisticsInfo from "../../../model/StreamStatistics/StreamStatisticsInfo";
+import StreamStatsDeviceInfo from "../../../model/StreamStatistics/StreamStatsDeviceInfo";
+import StreamStatsLinkInfo from "../../../model/StreamStatistics/StreamStatsLinkInfo";
+import StreamStatsWindowInfo from "../../../model/StreamStatistics/StreamStatsWindowInfo";
+
+import { getSampleStreamStats } from "../../../api/SampleData";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<StreamStatisticsPage/> functional component", () => {
+  let wrapper;
+  const dummyStreamStats = new StreamStatisticsInfo(
+    10,
+    10,
+    new StreamStatsWindowInfo(10, 10, 10),
+    new StreamStatsLinkInfo(10, 10, 10),
+    new StreamStatsDeviceInfo("send", 10, 10, 10, 10, 10, 10, 10),
+    new StreamStatsDeviceInfo("receive", 10, 10, 10, 10, 10, 10, 10, 10, 10)
+  );
+  const dummyStatsLocation = {
+    state: {
+      dummyStreamStats
+    }
+  };
+
+  describe("when passed a location prop", () => {
+    describe("returns a component that", () => {
+      beforeEach(() => {
+        wrapper = Enzyme.shallow(
+          <StreamStatisticsPage location={dummyStatsLocation} />
+        );
+      });
+      afterEach(() => {
+        wrapper.unmount();
+      });
+      it("contains 1 Page component with expected props", () => {
+        const expectedTitle = `Stream ${dummyStreamStats.id} Statistics`;
+        const expectedBreadcrumbs = [
+          ["Home", "/Home"],
+          ["Active Streams", "/Streams"],
+          ["Stream Details"],
+          ["Statistics", `/Streams/Details/${dummyStreamStats.id}/Statistics`]
+        ];
+
+        const page = wrapper.find(Page);
+        expect(page).toHaveLength(1);
+
+        const pageProps = page.props();
+        expect(pageProps.title).toBe(expectedTitle);
+        expect(pageProps.breadcrumbs).toStrictEqual(expectedBreadcrumbs);
+      });
+      it("contains 1 StreamStatisticsPageContents component with expected props", () => {
+        const contents = wrapper.find(StreamStatisticsPageContents);
+        expect(contents).toHaveLength(1);
+
+        const contentsProps = contents.props();
+        expect(contentsProps.statistics).toStrictEqual(dummyStreamStats);
+      });
+    });
+  });
+  describe("when not passed a location prop", () => {
+    describe("returns a component that", () => {
+      const expectedDefaultStats = getSampleStreamStats();
+      beforeEach(() => {
+        wrapper = Enzyme.shallow(<StreamStatisticsPage />);
+      });
+      afterEach(() => {
+        wrapper.unmount();
+      });
+      it("contains 1 Page component with expected props", () => {
+        const expectedTitle = `Stream ${expectedDefaultStats.id} Statistics`;
+        const expectedBreadcrumbs = [
+          ["Home", "/Home"],
+          ["Active Streams", "/Streams"],
+          ["Stream Details"],
+          [
+            "Statistics",
+            `/Streams/Details/${expectedDefaultStats.id}/Statistics`
+          ]
+        ];
+
+        const page = wrapper.find(Page);
+        expect(page).toHaveLength(1);
+
+        const pageProps = page.props();
+        expect(pageProps.title).toBe(expectedTitle);
+        expect(pageProps.breadcrumbs).toStrictEqual(expectedBreadcrumbs);
+      });
+      it("contains 1 StreamStatisticsPageContents component with expected props", () => {
+        const contents = wrapper.find(StreamStatisticsPageContents);
+        expect(contents).toHaveLength(1);
+
+        const contentsProps = contents.props();
+        expect(contentsProps.statistics).toStrictEqual(expectedDefaultStats);
+      });
+    });
+  });
+});
