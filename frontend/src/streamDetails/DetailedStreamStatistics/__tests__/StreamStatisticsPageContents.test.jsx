af0fa343e95d3169303f9e1c5de87a1c54a50b6c
gdzooks <issa4044@gmail.com>
2021-03-29 02:45:49 -0400

    linter my love

--- frontend/src/streamDetails/DetailedStreamStatistics/__tests__/StreamStatisticsPageContents.test.jsx
+++ frontend/src/streamDetails/DetailedStreamStatistics/__tests__/StreamStatisticsPageContents.test.jsx
@@ -15,52 +15,54 @@ import { getSampleStreamStats } from "../../../api/SampleData";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<StreamStatisticsPageContents/> functional component", () => {
-    let wrapper;
-    const dummyStatistics = getSampleStreamStats();
-    describe("returns a component that", () => {
-        beforeEach(() => {
-            wrapper = Enzyme.shallow(<StreamStatisticsPageContents statistics={dummyStatistics} />);
-        });
-        afterEach(() => {
-          wrapper.unmount();
-        });
-        it("has 4 Grid components with expected props", () => {
-            const grids = wrapper.find(Grid);
-            expect(grids).toHaveLength(4);
+  let wrapper;
+  const dummyStatistics = getSampleStreamStats();
+  describe("returns a component that", () => {
+    beforeEach(() => {
+      wrapper = Enzyme.shallow(
+        <StreamStatisticsPageContents statistics={dummyStatistics} />
+      );
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    it("has 4 Grid components with expected props", () => {
+      const grids = wrapper.find(Grid);
+      expect(grids).toHaveLength(4);
 
-            const gridOneProps = grids.at(0).props();
-            expect(gridOneProps.container).toBe(true);
-            expect(gridOneProps.spacing).toBe(3);
+      const gridOneProps = grids.at(0).props();
+      expect(gridOneProps.container).toBe(true);
+      expect(gridOneProps.spacing).toBe(3);
 
-            const gridTwoProps = grids.at(1).props();
-            expect(gridTwoProps.item).toBe(true);
-            expect(gridTwoProps.xs).toBe(4);
+      const gridTwoProps = grids.at(1).props();
+      expect(gridTwoProps.item).toBe(true);
+      expect(gridTwoProps.xs).toBe(4);
 
-            const gridThreeProps = grids.at(2).props();
-            expect(gridThreeProps.item).toBe(true);
-            expect(gridThreeProps.xs).toBe(4);
+      const gridThreeProps = grids.at(2).props();
+      expect(gridThreeProps.item).toBe(true);
+      expect(gridThreeProps.xs).toBe(4);
 
-            const gridFourProps = grids.at(3).props();
-            expect(gridFourProps.item).toBe(true);
-            expect(gridFourProps.xs).toBe(4);
-        });
-        it("has 1 StatisticsOverviewCard component with expected props", () => {
-            const card = wrapper.find(StatisticsOverviewCard);
+      const gridFourProps = grids.at(3).props();
+      expect(gridFourProps.item).toBe(true);
+      expect(gridFourProps.xs).toBe(4);
+    });
+    it("has 1 StatisticsOverviewCard component with expected props", () => {
+      const card = wrapper.find(StatisticsOverviewCard);
 
-            const cardProps = card.props();
-            expect(cardProps.stats).toStrictEqual(dummyStatistics);
-        });
-        it("has 1 StatisticsSendingCard component with expected props", () => {
-            const card = wrapper.find(StatisticsSendingCard);
+      const cardProps = card.props();
+      expect(cardProps.stats).toStrictEqual(dummyStatistics);
+    });
+    it("has 1 StatisticsSendingCard component with expected props", () => {
+      const card = wrapper.find(StatisticsSendingCard);
 
-            const cardProps = card.props();
-            expect(cardProps.stats).toStrictEqual(dummyStatistics);
-        });
-        it("has 1 StatisticsReceivingCard component with expected props", () => {
-            const card = wrapper.find(StatisticsReceivingCard);
+      const cardProps = card.props();
+      expect(cardProps.stats).toStrictEqual(dummyStatistics);
+    });
+    it("has 1 StatisticsReceivingCard component with expected props", () => {
+      const card = wrapper.find(StatisticsReceivingCard);
 
-            const cardProps = card.props();
-            expect(cardProps.stats).toStrictEqual(dummyStatistics);
-        });
-    })
+      const cardProps = card.props();
+      expect(cardProps.stats).toStrictEqual(dummyStatistics);
+    });
+  });
 });
