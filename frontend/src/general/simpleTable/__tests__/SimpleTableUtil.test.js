c113619e060459d0c04ee435d75952886804eaa5
nix <issa4044@gmail.com>
2021-03-30 16:59:51 -0400

    fix test

--- frontend/src/general/simpleTable/__tests__/SimpleTableUtil.test.js
+++ frontend/src/general/simpleTable/__tests__/SimpleTableUtil.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "@jest/globals";
 
-import zipProperties from "../SimpleTableUtil";
+import { zipProperties } from "../SimpleTableUtil";
 
 describe("zipProperties() method", () => {
   describe("when given two arrays", () => {
