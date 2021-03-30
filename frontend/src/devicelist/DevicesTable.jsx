260c900c516526a72db404677f1a43d70f3b207b
Sharon Ho <sharon.cy.ho@gmail.com>
2021-03-29 20:52:21 -0400

    Remove device details changes to another PR

--- frontend/src/devicelist/DevicesTable.jsx
+++ frontend/src/devicelist/DevicesTable.jsx
@@ -20,8 +20,8 @@ import MaterialTable, { MTableToolbar } from "material-table";
 
 import ChannelDetailsTable from "./ChannelDetailsTable";
 import StatusIndicator from "../general/StatusIndicator";
+import ActionMenu from "./ActionMenu";
 import DeviceInfo from "../model/DeviceInfo";
-import DeviceDetailsButton from "./DeviceDetailsButton";
 
 function getComponents() {
   return {
@@ -73,7 +73,7 @@ function getColumnInfo() {
       filtering: false,
       sorting: false,
       render: function Actions(rowData) {
-        return <DeviceDetailsButton device={rowData} />;
+        return <ActionMenu device={rowData} />;
       },
       align: "center",
       export: false
@@ -86,7 +86,7 @@ function getDetailPanel() {
     {
       icon: ExpandMore,
       openIcon: ExpandLess,
-      tooltip: "Show Channels",
+      tooltip: "Show Device Details",
       render: function DetailPanel(rowData) {
         return (
           <div className="lightestGrey" style={{ padding: "1.5em" }}>
