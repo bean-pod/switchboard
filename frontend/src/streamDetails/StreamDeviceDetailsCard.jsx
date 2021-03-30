ab85bb11574c6674c5349a69ca1e90603f9d2c3b
nix <issa4044@gmail.com>
2021-03-30 17:14:08 -0400

    LINT

--- frontend/src/streamDetails/StreamDeviceDetailsCard.jsx
+++ frontend/src/streamDetails/StreamDeviceDetailsCard.jsx
@@ -16,10 +16,7 @@ export default function StreamDeviceDetailsCard(props) {
     <DashboardCard title={cardTitle}>
       <Grid container>
         <Grid item xs={12}>
-          <SimpleTable
-            propertyNames={propertyNames}
-            properties={properties}
-          />
+          <SimpleTable propertyNames={propertyNames} properties={properties} />
         </Grid>
       </Grid>
     </DashboardCard>
