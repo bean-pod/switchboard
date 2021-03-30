05389cd1e30b078cccb4af0b7fb40dae7f72014a
Jun <rebecca.j.loke@gmail.com>
2021-03-29 20:50:41 -0400

    lint

--- frontend/src/api/tests/DeviceApi.test.js
+++ frontend/src/api/tests/DeviceApi.test.js
@@ -93,11 +93,11 @@ describe("DeviceApi", () => {
       );
     });
   });
-  describe("uploadConfiguration() function",  ()=>{
-    it("Should return a promise that resolves to true", async ()=>{
+  describe("uploadConfiguration() function", () => {
+    it("Should return a promise that resolves to true", async () => {
       const result = await DeviceApi.uploadConfiguration();
 
       expect(result).toBe(true);
-    })
-  })
+    });
+  });
 });
