ed22509e77fcd70adf035b7826d101eb911c7d2b
Joshua Butler <butler.joshua355@gmail.com>
2021-03-31 12:57:37 -0400

    removed unused general access methods that were replaced by restricted access methods

--- src/main/java/org/beanpod/switchboard/repository/StreamRepository.java
+++ src/main/java/org/beanpod/switchboard/repository/StreamRepository.java
@@ -11,12 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StreamRepository extends JpaRepository<StreamEntity, Long> {
 
-  @Query("SELECT s FROM Stream s WHERE s.outputChannel.encoder.serialNumber = :encoderSerial")
-  List<StreamEntity> getEncoderStreams(@Param(value = "encoderSerial") String encoderSerial);
-
-  @Query("SELECT s FROM Stream s WHERE s.inputChannel.decoder.serialNumber = :decoderSerial")
-  List<StreamEntity> getDecoderStreams(@Param(value = "decoderSerial") String decoderSerial);
-
   List<StreamEntity> findAllByOutputChannelEncoderDeviceUserAndOutputChannelEncoderSerialNumber(
       UserEntity user, String encoderSerial);
 
