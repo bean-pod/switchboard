3813b6042914cb66a50454dc8c05518ea219a064
Jun <rebecca.j.loke@gmail.com>
2021-03-31 22:47:44 -0400

    Create DeviceListPageContents.jsx

---
+++ frontend/src/devicelist/DeviceListPageContents.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { getSenders, getReceivers } from "../api/DeviceApi";
+import DeviceTableTitle from "./DeviceTableTitle";
+import DevicesTable from "./DevicesTable";
+
+export default class DeviceListPageContents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 0,
+      senders: [],
+      receivers: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSendersChange = this.handleSendersChange.bind(this);
+    this.handleReceiversChange = this.handleReceiversChange.bind(this);
+  }
+
+  componentDidMount() {
+    getSenders(this.handleSendersChange);
+    getReceivers(this.handleReceiversChange);
+  }
+
+  handleChange(value) {
+    this.setState({
+      selected: value
+    });
+  }
+
+  handleSendersChange(senders) {
+    this.setState({
+      senders
+    });
+  }
+
+  handleReceiversChange(receivers) {
+    this.setState({
+      receivers
+    });
+  }
+
+  getDevices() {
+    const { receivers, senders, selected } = this.state;
+    switch (selected) {
+      case 1:
+        return senders;
+      case 2:
+        return receivers;
+      default:
+        return senders.concat(receivers);
+    }
+  }
+
+  getTitle() {
+    const { selected } = this.state;
+    return (
+      <DeviceTableTitle index={selected} handleChange={this.handleChange} />
+    );
+  }
+
+  render() {
+    return <DevicesTable devices={this.getDevices()} title={this.getTitle()} />;
+  }
+}
