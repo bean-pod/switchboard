facd2ff3556b28af0e2fdf0380ab28ccf5a39f00
Jun <rebecca.j.loke@gmail.com>
2021-04-03 19:05:22 -0400

    Create SelectTable.jsx
    Select table as base to select an index of a list of items

---
+++ frontend/src/createStream/SelectDeviceDialog/SelectTable.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { List, ListItem, ListItemText } from "@material-ui/core";
+
+export default class SelectTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleListItemClick = this.handleListItemClick.bind(this);
+  }
+
+  handleListItemClick(index) {
+    const { setIndex } = this.props;
+    setIndex(index);
+  }
+
+  render() {
+    const { selectedIndex, items } = this.props;
+    return (
+      <List>
+        {items.map((item, index) => {
+          return (
+            <ListItem
+              button
+              selected={selectedIndex === index}
+              onClick={() => this.handleListItemClick(index)}
+            >
+              <ListItemText primary={item} />
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  }
+}
+
+SelectDeviceSwipeableSteps.propTypes = {
+  selectedIndex: PropTypes.number.isRequired,
+  setIndex: PropTypes.func.isRequired,
+  items: PropTypes.arrayOf(PropTypes.string).isRequired
+};
