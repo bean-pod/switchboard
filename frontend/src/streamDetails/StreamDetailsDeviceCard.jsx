547f1a4b727945d3a277cbad94823c77546511d2
Jun <rebecca.j.loke@gmail.com>
2021-04-03 15:08:55 -0400

    simplify StreamDetails DeviceCard to use StreamDeviceCard

--- frontend/src/streamDetails/StreamDetailsDeviceCard.jsx
+++ frontend/src/streamDetails/StreamDetailsDeviceCard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid } from "@material-ui/core";
 
 import DeviceInfo from "../model/DeviceInfo";
-import DashboardCard from "../general/dashboard/DashboardCard";
-import SimpleTable from "../general/simpleTable/SimpleTable";
 import ButtonInfo from "../general/dashboard/ButtonInfo";
+import StreamDeviceCard from "../general/StreamDeviceCard";
 
 export default function StreamDetailsDeviceCard(props) {
   const { cardTitle, device, channel } = props;
@@ -14,20 +12,14 @@ export default function StreamDetailsDeviceCard(props) {
     { device },
     "View Device"
   );
-  const properties = {
-    Name: device.name,
-    "Serial Number": device.serialNumber,
-    Channel: channel
-  };
 
   return (
-    <DashboardCard title={cardTitle} button={button}>
-      <Grid container>
-        <Grid item xs={12}>
-          <SimpleTable properties={properties} />
-        </Grid>
-      </Grid>
-    </DashboardCard>
+    <StreamDeviceCard
+      cardTitle={cardTitle}
+      button={button}
+      device={device}
+      channel={channel}
+    />
   );
 }
 
