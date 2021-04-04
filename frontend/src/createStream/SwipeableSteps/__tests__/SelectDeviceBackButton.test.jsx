c0b74efd9bde4dea52ae36721ebff17c883eff17
Jun <rebecca.j.loke@gmail.com>
2021-04-04 13:01:16 -0400

    Create SelectDeviceBackButton.test.jsx

---
+++ frontend/src/createStream/SwipeableSteps/__tests__/SelectDeviceBackButton.test.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { afterEach, beforeEach, describe, expect } from "@jest/globals";
+import { Button } from "@material-ui/core";
+import { Close, KeyboardArrowLeft } from "@material-ui/icons";
+
+import StepperBackButton from "../StepperBackButton";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<StepperBackButton/> functional component", () => {
+  let wrapper;
+  const mockBack = jest.fn();
+  const mockClose= jest.fn();
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+    describe("when prop isFirst is true", ()=>{
+        beforeEach(()=>{
+            wrapper = Enzyme.shallow(
+            <StepperBackButton 
+            isFirst
+            handleBack={mockBack}
+            handleClose={mockClose}
+            />)
+        })
+        it("returns a <Button/> component with expected props & children", ()=>{
+            const buttons = wrapper.find(Button);
+            expect(buttons).toHaveLength(1);
+
+            const props = buttons.at(0).props();
+            const expected={
+                size: "small",
+                onClick: mockClose,
+                children: [
+                    <Close/>,
+                    "Cancel"
+                ]
+            }
+            expect(props.size).toBe(expected.size);
+            expect(props.onClick).toStrictEqual(expected.onClick);
+            expect(props.children).toStrictEqual(expected.children);
+
+        })  
+    })
+    describe("when prop isFirst is false", ()=>{
+        beforeEach(()=>{
+            const isNotFirst = false;
+
+            wrapper = Enzyme.shallow(
+            <StepperBackButton 
+            isFirst={isNotFirst}
+            handleBack={mockBack}
+            handleClose={mockClose}
+            />)
+        })
+        it("returns a <Button/> component with expected props & children", ()=>{
+            const buttons = wrapper.find(Button);
+            expect(buttons).toHaveLength(1);
+
+            const props = buttons.at(0).props();
+            const expected={
+                size: "small",
+                onClick: mockBack,
+                children: [
+                    <KeyboardArrowLeft/>,
+                    "Back"
+                ]
+            }
+            expect(props.size).toBe(expected.size);
+            expect(props.onClick).toStrictEqual(expected.onClick);
+            expect(props.children).toStrictEqual(expected.children);
+
+        })  
+    })
+
+
+});
