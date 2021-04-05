6078c5511b97a418c150b2ad79153ee5a2814b46
Jun <rebecca.j.loke@gmail.com>
2021-04-05 13:57:23 -0400

    mkae breadcrumbs use navLink instead and pass 3rd crumb value to state

--- frontend/src/general/DynamicBreadcrumb.jsx
+++ frontend/src/general/DynamicBreadcrumb.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { Box, Breadcrumbs, Link, Typography } from "@material-ui/core";
+import { Box, Breadcrumbs, Typography } from "@material-ui/core";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+import DeviceInfo from "../model/DeviceInfo";
 
 export default function DynamicBreadcrumb(props) {
   const { breadcrumbs } = props;
   return (
     <Box padding="2em 0em 0em 1em">
       <Breadcrumbs aria-label="breadcrumb" id="breadcrumbParent">
-        {breadcrumbs.map((crumb, i) => {
+        {breadcrumbs.map((crumb) => {
           return (
-            <Link
-              color="inherit"
-              href={crumb[1]}
-              id={`breadcrumb${i}`}
+            <NavLink
+              to={{
+                pathname: crumb[1],
+                state: crumb[2]
+              }}
               key={`breadcrumb ${crumb[0]}`}
             >
               <Typography color="textPrimary">{crumb[0]}</Typography>
-            </Link>
+            </NavLink>
           );
         })}
       </Breadcrumbs>
@@ -24,5 +27,10 @@ export default function DynamicBreadcrumb(props) {
   );
 }
 DynamicBreadcrumb.propTypes = {
-  breadcrumbs: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired
+  breadcrumbs: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.string),
+      PropTypes.instanceOf(DeviceInfo)
+    ])
+  ).isRequired
 };
