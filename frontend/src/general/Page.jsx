346ed316cc53aab7b90f91a85468b3b9c34ebb9d
Jun <rebecca.j.loke@gmail.com>
2021-04-05 14:45:46 -0400

    props validation update for Page

--- frontend/src/general/Page.jsx
+++ frontend/src/general/Page.jsx
@@ -4,6 +4,8 @@ import { Box, Container } from "@material-ui/core";
 import DynamicBreadcrumb from "./DynamicBreadcrumb";
 import Title from "./Title";
 import HeaderBar from "./HeaderBar";
+import DeviceInfo from "../model/DeviceInfo";
+import StreamInfo from "../model/StreamInfo";
 
 export default function Page(props) {
   const { breadcrumbs, title, deviceList, children } = props;
@@ -22,8 +24,19 @@ export default function Page(props) {
 }
 
 Page.propTypes = {
-  breadcrumbs: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string))
-    .isRequired,
+  breadcrumbs: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.objectOf(
+          PropTypes.oneOfType([
+            PropTypes.instanceOf(DeviceInfo),
+            PropTypes.instanceOf(StreamInfo)
+          ])
+        )
+      ])
+    )
+  ).isRequired,
   title: PropTypes.string.isRequired,
   deviceList: PropTypes.bool,
   children: PropTypes.oneOfType([
