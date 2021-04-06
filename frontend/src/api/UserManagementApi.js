91b1bc7fae494498489906d2c69a7156ae02d94f
Sharon Ho <sharon.cy.ho@gmail.com>
2021-04-06 14:43:50 -0400

    Add ignore comment for default export

--- frontend/src/api/UserManagementApi.js
+++ frontend/src/api/UserManagementApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { getAuthorizationHeader } from "./AuthenticationUtil";
 
+/* eslint-disable import/prefer-default-export */
 export async function createUser(credentials) {
   return axios.post(
     process.env.REACT_APP_SIGNUP,
