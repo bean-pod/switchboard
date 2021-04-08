d0b2f2c32bd4b150a35e7a7a1ed39cd7bf0812c2
Sharon Ho <sharon.cy.ho@gmail.com>
2021-04-08 01:06:58 -0400

    Replace error dialog with snackbar

--- frontend/src/admin/__tests__/CreateUserPageContents.test.jsx
+++ frontend/src/admin/__tests__/CreateUserPageContents.test.jsx
@@ -12,7 +12,6 @@ import {
 
 import CreateUserPageContents from "../createUser/CreateUserPageContents";
 import CreateUserFormConsole from "../createUser/CreateUserFormConsole";
-import FormFailedDialog from "../../general/userForm/FormFailedDialog";
 import * as UserManagementApi from "../../api/UserManagementApi";
 import * as SnackbarMessage from "../../general/SnackbarMessage";
 
@@ -24,17 +23,8 @@ const snackbarSpy = jest.spyOn(SnackbarMessage, "snackbar");
 
 describe("<CreateUserPageContents/> class component", () => {
   let wrapper;
-  const mockOpenDialog = jest.fn();
 
   beforeEach(() => {
-    const mockRefElement = {
-      current: {
-        openDialog: mockOpenDialog
-      }
-    };
-    jest.spyOn(React, "createRef").mockImplementation(() => {
-      return mockRefElement;
-    });
     wrapper = Enzyme.shallow(<CreateUserPageContents />);
   });
 
@@ -52,36 +42,6 @@ describe("<CreateUserPageContents/> class component", () => {
       expect(formConsoleProps.handleSubmit).toBe(
         wrapper.instance().handleSubmit
       );
-    });
-
-    it("contains one <FormFailedDialog/> component with the expected props", () => {
-      expect(wrapper.find(FormFailedDialog)).toHaveLength(1);
-
-      const FormFailedDialogProps = wrapper.find(FormFailedDialog).props();
-
-      expect(FormFailedDialogProps.title).toEqual("Failed to create user");
-      expect(FormFailedDialogProps.errorMessage).toBe(
-        wrapper.state().dialogMessage
-      );
-    });
-  });
-
-  describe("setDialogMessage() function", () => {
-    it("sets the state of dialogMessage", () => {
-      const startState = "initialMessage";
-      wrapper.setState({ dialogMessage: startState });
-
-      const changedState = "test";
-      wrapper.instance().setDialogMessage(changedState);
-
-      expect(wrapper.state().dialogMessage).toEqual(changedState);
-    });
-  });
-
-  describe("openDialog() function", () => {
-    it("calls dialogElement.current.openDialog()", () => {
-      wrapper.instance().openDialog();
-      expect(mockOpenDialog).toBeCalledTimes(1);
     });
   });
 
@@ -112,7 +72,7 @@ describe("<CreateUserPageContents/> class component", () => {
     });
 
     describe("when createUser() rejects", () => {
-      it("Calls openDialog() and changes the state dialogMessage to the error message", async () => {
+      it("Displays an error snackbar with the error message", async () => {
         const someErrorMessage = "errorMessage";
         UserManagementApi.createUser.mockRejectedValue({
           message: someErrorMessage
@@ -123,11 +83,11 @@ describe("<CreateUserPageContents/> class component", () => {
         const flushPromises = () => new Promise(setImmediate);
         await flushPromises();
 
-        expect(mockOpenDialog).toHaveBeenCalled();
-
-        expect(wrapper.state()).toEqual({
-          dialogMessage: someErrorMessage
-        });
+        expect(snackbarSpy).toHaveBeenCalledTimes(1);
+        expect(snackbarSpy).toHaveBeenCalledWith(
+          "error",
+          `Failed to create user: ${someErrorMessage}`
+        );
       });
     });
   });
--- frontend/src/admin/createUser/CreateUserPageContents.jsx
+++ frontend/src/admin/createUser/CreateUserPageContents.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import FormFailedDialog from "../../general/userForm/FormFailedDialog";
 import { createUser } from "../../api/UserManagementApi";
 import CreateUserFormConsole from "./CreateUserFormConsole";
 import { snackbar } from "../../general/SnackbarMessage";
@@ -8,13 +7,7 @@ import { snackbar } from "../../general/SnackbarMessage";
 export default class CreateUserPageContents extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dialogMessage: ""
-    };
-    this.dialogElement = React.createRef();
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.openDialog = this.openDialog.bind(this);
-    this.setDialogMessage = this.setDialogMessage.bind(this);
   }
 
   handleSubmit(username, password) {
@@ -23,32 +16,11 @@ export default class CreateUserPageContents extends React.Component {
         snackbar("success", `User ${username} successfully created!`);
       })
       .catch((error) => {
-        this.openDialog();
-        this.setDialogMessage(error.message);
+        snackbar("error", `Failed to create user: ${error.message}`);
       });
   }
 
-  setDialogMessage(message) {
-    this.setState({
-      dialogMessage: message
-    });
-  }
-
-  openDialog() {
-    this.dialogElement.current.openDialog();
-  }
-
   render() {
-    const { dialogMessage } = this.state;
-    return (
-      <>
-        <CreateUserFormConsole handleSubmit={this.handleSubmit} />
-        <FormFailedDialog
-          ref={this.dialogElement}
-          title="Failed to create user"
-          errorMessage={dialogMessage}
-        />
-      </>
-    );
+    return <CreateUserFormConsole handleSubmit={this.handleSubmit} />;
   }
 }
