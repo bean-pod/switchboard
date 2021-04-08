76e1aa464f2c78b2382e6bf7331c0c78b61c7499
nix <issa4044@gmail.com>
2021-04-07 20:59:43 -0400

    Update detailed stats page breadcrumbs
    Add StreamInfo object to stream details breadcrumb
    Add stream + stats object to stats breadcrumb so clicking it won't break the page
    
    Co-Authored-By: Jun Loke <35614138+arejayelle@users.noreply.github.com>

--- frontend/src/streamDetails/DetailedStreamStatistics/StreamStatisticsPage.jsx
+++ frontend/src/streamDetails/DetailedStreamStatistics/StreamStatisticsPage.jsx
@@ -9,14 +9,18 @@ import { getSampleStreamStats } from "../../api/SampleData";
 export default function StreamStatisticsPage(props) {
   const {
     location: {
-      state: { statistics }
+      state: { statistics, stream }
     }
   } = props;
   const breadcrumbs = [
     ["Home", "/Home"],
     ["Active Streams", "/Streams"],
-    ["Stream Details"],
-    ["Statistics", `/Streams/Details/${statistics.id}/Statistics`]
+    ["Stream Details", `/Streams/Details/${stream.id}`, { stream }],
+    [
+      "Statistics",
+      `/Streams/Details/${stream.id}/Statistics`,
+      { statistics, stream }
+    ]
   ];
 
   return (
@@ -26,14 +30,10 @@ export default function StreamStatisticsPage(props) {
   );
 }
 
-StreamStatisticsPage.defaultProps = {
-  location: { state: { statistics: getSampleStreamStats() } }
-};
-
 StreamStatisticsPage.propTypes = {
   location: PropTypes.shape({
     state: PropTypes.shape({
       statistics: PropTypes.instanceOf(StreamStatisticsInfo)
     })
-  })
+  }).isRequired
 };
