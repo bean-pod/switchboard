dc791c3e6baca5f4277cde38d679b4378486e644
François LaBerge <fgrclaberge@gmail.com>
2021-04-09 12:00:57 -0400

    Add tests for the dateMapper

--- src/test/java/org/beanpod/switchboard/fixture/DecoderFixture.java
+++ src/test/java/org/beanpod/switchboard/fixture/DecoderFixture.java
@@ -2,6 +2,7 @@ package org.beanpod.switchboard.fixture;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import lombok.SneakyThrows;
@@ -15,8 +16,6 @@ public class DecoderFixture {
 
   public static final String SERIAL_NUMBER = "1";
   public static final String SERIAL_NUMBER2 = "2";
-  private static final String pattern = "yyyy-MM-dd HH:mm:ss";
-  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
   private static final Set<InputChannelEntity> setOfChannels =
       ChannelFixture.getInputChannelEntities();
   private static final Set<InputChannelDto> setOfDtoChannels = ChannelFixture.getInputChannelDtos();
@@ -25,7 +24,7 @@ public class DecoderFixture {
   public static DecoderEntity getDecoderEntity1() {
     return DecoderEntity.builder()
         .serialNumber(SERIAL_NUMBER)
-        .lastCommunication(simpleDateFormat.parse("2020-10-31 05:05:05"))
+        .lastCommunication(DateFixture.getDate())
         .device(DeviceFixture.getDevice1())
         .input(setOfChannels)
         .build();
@@ -35,7 +34,7 @@ public class DecoderFixture {
   public static DecoderEntity getDecoderEntity2() {
     return DecoderEntity.builder()
         .serialNumber(SERIAL_NUMBER2)
-        .lastCommunication(simpleDateFormat.parse("2020-10-31 05:05:05"))
+        .lastCommunication(DateFixture.getDate())
         .device(DeviceFixture.getDevice2())
         .input(setOfChannels)
         .build();
@@ -56,7 +55,7 @@ public class DecoderFixture {
   public static DecoderDto getDecoderDto() {
     return DecoderDto.builder()
         .serialNumber(SERIAL_NUMBER)
-        .lastCommunication(simpleDateFormat.parse("2020-10-31 05:05:05"))
+        .lastCommunication(DateFixture.getDate())
         .device(DeviceFixture.getDeviceDto())
         .input(setOfDtoChannels)
         .build();
--- src/test/java/org/beanpod/switchboard/fixture/EncoderFixture.java
+++ src/test/java/org/beanpod/switchboard/fixture/EncoderFixture.java
@@ -3,6 +3,7 @@ package org.beanpod.switchboard.fixture;
 import java.text.SimpleDateFormat;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import lombok.SneakyThrows;
@@ -16,8 +17,6 @@ public class EncoderFixture {
 
   public static final String SERIAL_NUMBER = "1";
   public static final String SERIAL_NUMBER2 = "2";
-  private static final String pattern = "yyyy-MM-dd HH:mm:ss";
-  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
   private static final Set<OutputChannelEntity> setOfChannels =
       ChannelFixture.getOutputChannelEntities();
   private static final Set<OutputChannelDto> setOfDtoChannels =
@@ -27,7 +26,7 @@ public class EncoderFixture {
   public static EncoderEntity getEncoderEntity1() {
     return EncoderEntity.builder()
         .serialNumber(SERIAL_NUMBER)
-        .lastCommunication(simpleDateFormat.parse("2020-10-31 05:05:05"))
+        .lastCommunication(DateFixture.getDate())
         .device(DeviceFixture.getDevice1())
         .output(setOfChannels)
         .build();
@@ -37,7 +36,7 @@ public class EncoderFixture {
   public static EncoderEntity getEncoderEntity2() {
     return EncoderEntity.builder()
         .serialNumber(SERIAL_NUMBER2)
-        .lastCommunication(simpleDateFormat.parse("2020-10-31 05:05:05"))
+        .lastCommunication(DateFixture.getDate())
         .device(DeviceFixture.getDevice2())
         .output(setOfChannels)
         .build();
@@ -58,7 +57,7 @@ public class EncoderFixture {
   public static EncoderDto getEncoderDto() {
     return EncoderDto.builder()
         .serialNumber(SERIAL_NUMBER)
-        .lastCommunication(simpleDateFormat.parse("2020-10-31 05:05:05"))
+        .lastCommunication(DateFixture.getDate())
         .device(DeviceFixture.getDeviceDto())
         .output(setOfDtoChannels)
         .build();
@@ -67,14 +66,14 @@ public class EncoderFixture {
   public static EncoderModel getEncoderModel() {
     return new EncoderModel()
         .serialNumber(SERIAL_NUMBER)
-        .lastCommunication(OffsetDateTime.parse("2020-10-31T05:05:05Z"))
+        .lastCommunication(DateFixture.getOffsetDateTime())
         .device(DeviceFixture.getDeviceModel());
   }
 
   public static EncoderModel getEncoderModelWithOutputChannel() {
     return new EncoderModel()
         .serialNumber(SERIAL_NUMBER)
-        .lastCommunication(OffsetDateTime.parse("2020-10-31T05:05:05Z"))
+        .lastCommunication(DateFixture.getOffsetDateTime())
         .device(DeviceFixture.getDeviceModel())
         .output(List.of(ChannelFixture.getOutputChannelModel()));
   }
