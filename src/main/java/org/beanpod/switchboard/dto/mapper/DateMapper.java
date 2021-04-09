c5123c920af2f1baeb333a89ea682401bf3ac357
François LaBerge <fgrclaberge@gmail.com>
2021-04-08 22:35:52 -0400

    Fix some bugs with the mapper and the paths

--- src/main/java/org/beanpod/switchboard/dto/mapper/DateMapper.java
+++ src/main/java/org/beanpod/switchboard/dto/mapper/DateMapper.java
@@ -1,16 +1,21 @@
 package org.beanpod.switchboard.dto.mapper;
 
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface DateMapper {
   default Date toDate(OffsetDateTime offsetDateTime) {
+    if(offsetDateTime == null){
+      return null;
+    }
+
     return new Date(offsetDateTime.toInstant().toEpochMilli());
   }
 
   default OffsetDateTime toOffsetDateTime(Date date) {
-    return OffsetDateTime.from(date.toInstant());
+    return date.toInstant().atOffset(ZoneOffset.UTC);
   }
 }
