294fc184b207e9cc06b710d976611d695599d477
Jun <rebecca.j.loke@gmail.com>
2021-04-09 11:21:19 -0400

    lint

--- frontend/src/general/LogoutMenu/__tests__/LogoutMenuOpener.test.jsx
+++ frontend/src/general/LogoutMenu/__tests__/LogoutMenuOpener.test.jsx
@@ -11,79 +11,84 @@ import LogoutMenu from "../LogoutMenu";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("<LogoutMenuOpener/> class component", ()=>{
-    let wrapper;
-    const dummyDisabled = false;
-    const mockHandleLogout = jest.fn();
-    const mockRefElement = {
-        current: {thing:"thing"}
+describe("<LogoutMenuOpener/> class component", () => {
+  let wrapper;
+  const dummyDisabled = false;
+  const mockHandleLogout = jest.fn();
+  const mockRefElement = {
+    current: { thing: "thing" }
+  };
+
+  beforeEach(() => {
+    jest.spyOn(React, "createRef").mockImplementation(() => {
+      return mockRefElement;
+    });
+    wrapper = Enzyme.shallow(
+      <LogoutMenuOpener
+        disabled={dummyDisabled}
+        handleLogout={mockHandleLogout}
+      />
+    );
+  });
+  describe("render() function returns a component that", () => {
+    it("Contains 1 <IconButton/> component with expected props", () => {
+      const components = wrapper.find(IconButton);
+      expect(components).toHaveLength(1);
+
+      const props = components.at(0).props();
+
+      const expected = {
+        id: "acctBtn",
+        color: "inherit",
+        disabled: dummyDisabled,
+        onClick: wrapper.instance().handleOpen,
+        children: <AccountCircle />
       };
 
-    beforeEach(()=>{
-          jest.spyOn(React, "createRef").mockImplementation(() => {
-            return mockRefElement;
-          });
-        wrapper = Enzyme.shallow(<LogoutMenuOpener disabled={dummyDisabled} handleLogout={mockHandleLogout}/>)
-    })
-    describe("render() function returns a component that", ()=>{
-        it("Contains 1 <IconButton/> component with expected props", ()=>{
-            const components = wrapper.find(IconButton);
-            expect(components).toHaveLength(1);
+      expect(props).toMatchObject(expected);
+    });
+    it("Contains 1 <AccountCircle/> component with expected props", () => {
+      const components = wrapper.find(AccountCircle);
+      expect(components).toHaveLength(1);
 
-            const props = components.at(0).props();
+      const props = components.at(0).props();
 
-            const expected = {
-                id: "acctBtn",
-                color: "inherit",
-                disabled: dummyDisabled,
-                onClick: wrapper.instance().handleOpen,
-                children: <AccountCircle/>
-            }
+      const expected = {};
 
-            expect(props).toMatchObject(expected);
-        })
-        it("Contains 1 <AccountCircle/> component with expected props", ()=>{
-            const components = wrapper.find(AccountCircle);
-            expect(components).toHaveLength(1);
+      expect(props).toStrictEqual(expected);
+    });
+    it("Contains 1 <LogoutMenu/> component with expected props", () => {
+      const components = wrapper.find(LogoutMenu);
+      expect(components).toHaveLength(1);
 
-            const props = components.at(0).props();
+      const props = components.at(0).props();
 
-            const expected = {}
+      const expected = {
+        anchor: mockRefElement.current,
+        open: wrapper.state().open,
+        handleLogout: mockHandleLogout,
+        handleClose: wrapper.instance().handleClose
+      };
 
-            expect(props).toStrictEqual(expected);
-        })
-        it("Contains 1 <LogoutMenu/> component with expected props", ()=>{
-            const components = wrapper.find(LogoutMenu);
-            expect(components).toHaveLength(1);
+      expect(props).toMatchObject(expected);
+    });
+  });
+  it("handleClose() function should set the state to false", () => {
+    wrapper.setState({ open: true });
 
-            const props = components.at(0).props();
+    const expected = {
+      open: false
+    };
+    wrapper.instance().handleClose();
+    expect(wrapper.state()).toStrictEqual(expected);
+  });
+  it("handleOpen() function should set the state to true", () => {
+    wrapper.setState({ open: false });
 
-            const expected = {
-                anchor: mockRefElement.current,
-                open: wrapper.state().open,
-                handleLogout: mockHandleLogout,
-                handleClose: wrapper.instance().handleClose
-            }
-
-            expect(props).toMatchObject(expected);
-        })
-    })
-    it("handleClose() function should set the state to false", ()=>{
-        wrapper.setState({open:true});
-
-        const expected ={
-            open: false
-        };
-        wrapper.instance().handleClose();
-        expect(wrapper.state()).toStrictEqual(expected)
-    })
-    it("handleOpen() function should set the state to true", ()=>{
-        wrapper.setState({open:false});
-
-        const expected ={
-            open: true
-        };
-        wrapper.instance().handleOpen();
-        expect(wrapper.state()).toStrictEqual(expected)
-    })
-})
+    const expected = {
+      open: true
+    };
+    wrapper.instance().handleOpen();
+    expect(wrapper.state()).toStrictEqual(expected);
+  });
+});
