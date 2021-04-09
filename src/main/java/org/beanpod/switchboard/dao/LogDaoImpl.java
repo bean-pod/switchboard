df2a2ad15808933a18e59dd47a7bbcc42cc61c45
Hani Sabsoob <39838955+sebhani@users.noreply.github.com>
2021-04-08 22:02:22 -0400

    Retrieve signed-in user logs only

--- src/main/java/org/beanpod/switchboard/dao/LogDaoImpl.java
+++ src/main/java/org/beanpod/switchboard/dao/LogDaoImpl.java
@@ -1,9 +1,11 @@
 package org.beanpod.switchboard.dao;
 
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.beanpod.switchboard.dto.LogDto;
 import org.beanpod.switchboard.dto.mapper.LogMapper;
+import org.beanpod.switchboard.entity.UserEntity;
 import org.beanpod.switchboard.repository.LogRepository;
 import org.openapitools.model.LogModel;
 import org.springframework.stereotype.Component;
@@ -14,9 +16,12 @@ public class LogDaoImpl {
 
   private final LogRepository logRepository;
   private final LogMapper logMapper;
+  private final HttpServletRequest request;
+  private final UserDaoImpl userDao;
 
   public List<LogModel> getLogs() {
-    return logMapper.toLogModels(logRepository.findAll());
+    UserEntity user = userDao.findUser(request.getUserPrincipal().getName());
+    return logMapper.toLogModels(logRepository.findAll(user.getId()));
   }
 
   public List<LogModel> getDeviceLogs(String serialNumber) {
--- src/main/java/org/beanpod/switchboard/repository/LogRepository.java
+++ src/main/java/org/beanpod/switchboard/repository/LogRepository.java
@@ -1,8 +1,11 @@
 package org.beanpod.switchboard.repository;
 
 import java.util.List;
+import org.beanpod.switchboard.dao.UserDaoImpl;
 import org.beanpod.switchboard.entity.LogEntity;
+import org.beanpod.switchboard.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,7 +13,12 @@ public interface LogRepository extends JpaRepository<LogEntity, Long> {
 
   LogEntity save(LogEntity logEntity);
 
-  List<LogEntity> findAll();
+  @Query(
+      value =
+          "select lo.id, lo.level, lo.message, lo.serial_number, lo.date_time "
+              + "from log_entity lo, user us, device de "
+              + "where us.id = de.user_id and us.id = ?1 and de.serial_number = lo.serial_number", nativeQuery = true)
+  List<LogEntity> findAll(Long user_id);
 
   List<LogEntity> findBySerialNumber(String serialNumber);
 }
