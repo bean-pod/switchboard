4847177f2a032221b7bfe59b5b452e0b4346ad17
nix <issa4044@gmail.com>
2021-04-10 11:38:40 -0400

    Update DeviceListPage.test.jsx

--- frontend/src/devicelist/__tests__/DeviceListPage.test.jsx
+++ frontend/src/devicelist/__tests__/DeviceListPage.test.jsx
@@ -12,34 +12,89 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("<DeviceListPage/> functional component", () => {
   let wrapper;
 
-  beforeEach(() => {
-    wrapper = Enzyme.shallow(<DeviceListPage />);
-  });
-  afterEach(() => {
-    wrapper.unmount();
-  });
-  describe("returns a component that", () => {
-    it("Contains 1 <Page/> component with the expected props", () => {
-      const expectedBreadcrumbs = [
-        ["Home", "/Home"],
-        ["My Devices", "/Devices"]
-      ];
-      const expected = {
-        title: "My Devices",
-        breadcrumbs: expectedBreadcrumbs,
-        hasStreamButton: true
-      };
-
-      const page = wrapper.find(Page);
-      expect(page).toHaveLength(1);
-
-      const pageProps = page.props();
-      expect(pageProps.title).toBe(expected.title);
-      expect(pageProps.breadcrumbs).toStrictEqual(expected.breadcrumbs);
-      expect(pageProps.hasStreamButton).toBe(expected.hasStreamButton);
+  describe("when not passed a location", () => {
+    beforeEach(() => {
+      wrapper = Enzyme.shallow(<DeviceListPage />);
     });
-    it("Contains 1 <DeviceListPageContents/> component", () => {
-      expect(wrapper.find(DeviceListPageContents)).toHaveLength(1);
+    afterEach(() => {
+      wrapper.unmount();
     });
-  });
+    describe("returns a component that", () => {
+      it("Contains 1 <Page/> component with the expected props", () => {
+        const expectedBreadcrumbs = [
+          ["Home", "/Home"],
+          ["My Devices", "/Devices"]
+        ];
+        const expected = {
+          title: "My Devices",
+          breadcrumbs: expectedBreadcrumbs,
+          hasStreamButton: true
+        };
+  
+        const page = wrapper.find(Page);
+        expect(page).toHaveLength(1);
+  
+        const pageProps = page.props();
+        expect(pageProps.title).toBe(expected.title);
+        expect(pageProps.breadcrumbs).toStrictEqual(expected.breadcrumbs);
+        expect(pageProps.hasStreamButton).toBe(expected.hasStreamButton);
+      });
+      it("Contains 1 <DeviceListPageContents/> component with expected props", () => {
+        const defaultSelectedState = {
+          selected: 0
+        }
+  
+        const deviceListContents = wrapper.find(DeviceListPageContents);
+        expect(deviceListContents).toHaveLength(1);
+  
+        const contentsProps = deviceListContents.props();
+        expect(contentsProps.selectedState).toStrictEqual(defaultSelectedState);
+  
+      });
+    });
+  })
+  describe("when passed a location component", () => {
+    const dummyLocation = {
+      state: { selected: 2 }
+    }
+    beforeEach(() => {
+      wrapper = Enzyme.shallow(<DeviceListPage location={dummyLocation} />);
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    describe("returns a component that", () => {
+      it("Contains 1 <Page/> component with the expected props", () => {
+        const expectedBreadcrumbs = [
+          ["Home", "/Home"],
+          ["My Devices", "/Devices"]
+        ];
+        const expected = {
+          title: "My Devices",
+          breadcrumbs: expectedBreadcrumbs,
+          hasStreamButton: true
+        };
+  
+        const page = wrapper.find(Page);
+        expect(page).toHaveLength(1);
+  
+        const pageProps = page.props();
+        expect(pageProps.title).toBe(expected.title);
+        expect(pageProps.breadcrumbs).toStrictEqual(expected.breadcrumbs);
+        expect(pageProps.hasStreamButton).toBe(expected.hasStreamButton);
+      });
+      it("Contains 1 <DeviceListPageContents/> component with expected props", () => {
+        const selectedState = {
+          selected: 2
+        }
+  
+        const deviceListContents = wrapper.find(DeviceListPageContents);
+        expect(deviceListContents).toHaveLength(1);
+  
+        const contentsProps = deviceListContents.props();
+        expect(contentsProps.selectedState).toStrictEqual(selectedState);
+  
+      });
+    });
+  })
 });
