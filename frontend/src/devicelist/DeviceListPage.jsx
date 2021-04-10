291e5645a3e63c8e59b68f060bda20a24fef483c
nix <issa4044@gmail.com>
2021-04-10 11:38:34 -0400

    Add location and selectedState props to pass device list select state around

--- frontend/src/devicelist/DeviceListPage.jsx
+++ frontend/src/devicelist/DeviceListPage.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Page from "../general/Page";
 import DeviceListPageContents from "./DeviceListPageContents";
 
-export default function DeviceListPage() {
+export default function DeviceListPage(props) {
+  const {
+    location: { state: selected }
+  } = props;
+
   const breadcrumb = [
     ["Home", "/Home"],
     ["My Devices", "/Devices"]
   ];
   return (
     <Page title="My Devices" breadcrumbs={breadcrumb} hasStreamButton>
-      <DeviceListPageContents />
+      <DeviceListPageContents selectedState={selected} />
     </Page>
   );
 }
+
+DeviceListPage.defaultProps = {
+  location: { state: { selected: 0 } }
+};
+
+DeviceListPage.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      selected: PropTypes.number
+    })
+  })
+};
--- frontend/src/devicelist/DeviceListPageContents.jsx
+++ frontend/src/devicelist/DeviceListPageContents.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { getSenders, getReceivers } from "../api/DeviceApi";
 import DeviceTableTitle from "./DeviceTableTitle";
@@ -21,6 +22,8 @@ export default class DeviceListPageContents extends React.Component {
   }
 
   componentDidMount() {
+    const { selectedState } = this.props;
+    this.handleChange(selectedState.selected);
     getSenders(this.handleSendersChange);
     getReceivers(this.handleReceiversChange);
   }
@@ -66,3 +69,13 @@ export default class DeviceListPageContents extends React.Component {
     return <DevicesTable devices={this.getDevices()} title={this.getTitle()} />;
   }
 }
+
+DeviceListPageContents.defaultProps = {
+  selectedState: { selected: 0 }
+};
+
+DeviceListPageContents.propTypes = {
+  selectedState: PropTypes.shape({
+    selected: PropTypes.number
+  })
+};
